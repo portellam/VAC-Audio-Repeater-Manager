59edad1196e479d311b67caae8b13803a66995ed
Alex Portell <amportell.13@gmail.com>
2023-12-14 15:46:24 -0500

    Added notes for RepeaterInfo.

--- VACARM/VACARM.Tests/RepeaterInfoTests.cs
+++ VACARM/VACARM.Tests/RepeaterInfoTests.cs
@@ -6,5 +6,40 @@ namespace VACARM.Tests
     [TestFixture]
     public class RepeaterInfoTests
     {
+        // Constructor???
+
+        // ContextClick
+        /*
+         *  TODO: validate sender and RoutedEventArgs?
+         * graphIsNull_DoSomething_ExpectSomething
+         * graphIsValid_DoSomething_ExpectSomething
+         */
+
+        // OnPropertyChanged
+        /*
+         * propertyNameIsNull_DoNothing
+         * propertyNameIsEmpty_DoNothing
+         * propertyNameIsValid_DoSomething_ExpectSomething
+         */
+
+        // SetData
+        /*
+         * infoListIsNull_PropertiesAreUnchanged_ReturnVoid
+         * infoListIsValid_PropertiesAreChanged_ReturnVoid
+         */
+
+        // ToCommand
+        /*
+         * ReturnString
+         * (make sure public strings populate?)
+         * 
+         */
+
+        // ToSaveData
+        /*
+         * ReturnString
+         * (make sure public strings populate?)
+         * 
+         */
     }
 }
