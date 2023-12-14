8c9313b941cbe14aafaf9607e37aed8c73ff314c
Alex Portell <amportell.13@gmail.com>
2023-12-14 14:02:51 -0500

    Finished notes for DefaultData.

--- VACARM/VACARM.Tests/DefaultDataTests.cs
+++ VACARM/VACARM.Tests/DefaultDataTests.cs
@@ -6,5 +6,29 @@ namespace VACARM.Tests
     [TestFixture]
     public class DefaultDataTests
     {
+
+        // CheckFile()
+        /*
+         * PathIsNull_WriteDefaultRepeaterToFile__
+         * PathDoesNotExist_WriteDefaultRepeaterToFile__
+         * PathDoesExist_DoNotWriteDefaultRepeaterToFile__ 
+         * _DefaultGraphIsNotNullAndNetworkCountIsGreaterThanOneAndSavePathExists_DefaultGraphIsUnchanged
+         * _DefaultGraphIsNull_DefaultGraphIsTwoBackSlashes
+         * _NetworkCountIsOne_DefaultGraphIsChanged
+         * _NetworkCountIsZero_DefaultGraphIsTwoBackSlashes
+         * _SavePathDoesNotExist_DefaultGraphIsTwoBackSlashes
+         */
+
+        // Refresh()
+        /*
+         * NOTE: this borrows one line from CheckFile.
+         * TODO: refactor?
+         */
+
+        // Save()
+        /*
+         * DataWriteAllLinesToPath_ConfirmDataIsSame ?
+         * TODO: try to test for every exception? NOTE: refactor?
+         */
     }
 }
