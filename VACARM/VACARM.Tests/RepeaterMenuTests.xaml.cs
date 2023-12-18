b7d93822a7fce9c16022dc8609fa40689eb5106c
Alex Portell <amportell.13@gmail.com>
2023-12-18 13:07:35 -0500

    Added notes.

--- VACARM/VACARM.Tests/RepeaterMenuTests.xaml.cs
+++ VACARM/VACARM.Tests/RepeaterMenuTests.xaml.cs
@@ -6,5 +6,21 @@ namespace VACARM.Tests
     [TestFixture]
     public class RepeaterMenuTests
     {
+        // Constructor()
+        /*
+         * _RepeaterInfoIsNull_ThrowArgumentNullException
+         * _BipartiteDeviceGraphIsNull_ThrowArgumentNullException
+         * _InputsAreValid_SetGridInfoAndUpdateMetadata
+         */
+
+        // DeleteButton_Click()
+        /*
+         * _RemoveFromGraphAndCloseWindow
+         */
+
+        // Okay_Click()
+        /*
+         * _CloseWindow
+         */
     }
 }
