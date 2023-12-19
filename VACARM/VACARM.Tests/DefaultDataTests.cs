2da16f8e6ac78d17b0f0d543e710c5a9a3a02788
Alex Portell <amportell.13@gmail.com>
2023-12-19 17:32:58 -0500

    Update DefaultDataTests.cs

--- VACARM/VACARM.Tests/DefaultDataTests.cs
+++ VACARM/VACARM.Tests/DefaultDataTests.cs
@@ -23,26 +23,32 @@ namespace VACARM.Tests
          * _NetworkCountIsOne_DefaultGraphIsChanged
          * _NetworkCountIsZero_DefaultGraphIsTwoBackSlashes
          * _SavePathDoesNotExist_DefaultGraphIsTwoBackSlashes
+         * 
+         * _FileIsNotNull_ReadAllLinesFails_ThrowIOException
+         * _FileIsNotNull_ReadAllLinesPasses_DoesSavePathExistReturnsFalse_ReturnVoid
+         * _FileIsNotNull_ReadAllLinesPasses_DoesSavePathExistReturnsTrue_NetworkArrayLengthIsOne_SetDefaultGraphToEmpty
+         * _FileIsNotNull_ReadAllLinesPasses_DoesSavePathExistReturnsTrue_NetworkArrayLengthIsZero_SetDefaultGraphToDefault
+         * _FileIsNotNull_ReadAllLinesPasses_DoesSavePathExistReturnsTrue_NetworkArrayLengthIs
          */
 
 		// DoesSavePathExist()
 		/*
-         * 
-         * 
+         * _SavePathExists_ReturnTrue
+         * _SavePathDoesNotExist_CreateDirectoryFails_CatchIOExceptionAndReturnFalse
+         * _SavePathDoesNotExist_CreateDirectoryPasses_ReturnTrue
          */
 
 		// Refresh()
 		/*
-         * NOTE: this borrows one line from CheckFile.
-         * TODO: refactor?
+         * _CheckFile_FileIsNotNull_ReadAllLinesFromPath
+         * _CheckFile_FileIsNull_CatchIOException
          */
 
 		// Save()
 		/*
-         * _DataWriteAllLinesToPath_ConfirmDataIsSame ???
-         * 
-         * 
-         * 
+         * _FileIsNotNullAndDataIsNotNull_DataWriteAllLinesToPath
+         * _FileIsNotNullAndDataIsNull_DataWriteAllLinesToPath
+         * _FileIsNull_CatchIOException
          */
 	}
 }
