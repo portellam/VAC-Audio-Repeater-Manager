6fc681bfb7fdefadec5e683fe7d8791abdb60807
Alex Portell <amportell.13@gmail.com>
2023-12-19 01:08:33 -0500

    Started notes.

--- VACARM/VACARM.Tests/DeviceControlTests.xaml.cs
+++ VACARM/VACARM.Tests/DeviceControlTests.xaml.cs
@@ -6,5 +6,60 @@ namespace VACARM.Tests
     [TestFixture]
     public class DeviceControlTests
     {
+        // Constructor
+        /*
+         * 
+         * 
+         */
+
+        // OnPropertyChanged()
+        /*
+         * 
+         * 
+         */
+
+        // SetBackgrounColor()
+        /*
+         * 
+         * 
+         */
+
+        // UserControl_MouseLeftButtonDown()
+        /*
+         * 
+         * 
+         */
+
+        // UserControl_MouseLeftButtonUp()
+        /*
+         * 
+         * 
+         */
+
+        // UserControl_PreviewMouseMove()
+        /*
+         * _IfSelectedToolIsNotHand_ReturnVoid
+         * _IfLeftButtonIsNotPressed_ReturnVoid
+
+         * _LeftIsLessThanZero_LeftIsGreaterThanActualWidth_SetTopToZero_TopIsGreaterThanActualHeight
+         * _LeftIsLessThanZero_LeftIsGreaterThanActualWidth_SetTopToZero_TopIsLessThanOrEqualToActualHeight
+         * _LeftIsLessThanZero_LeftIsGreaterThanActualWidth_TopIsLessThanZero_TopIsGreaterThanActualHeight
+         * _LeftIsLessThanZero_LeftIsGreaterThanActualWidth_TopIsLessThanZero_TopIsLessThanOrEqualToActualHeight
+
+         * _LeftIsLessThanZero_LeftIsLessThanOrEqualToActualWidth_SetTopToZero_TopIsGreaterThanActualHeight
+         * _LeftIsLessThanZero_LeftIsLessThanOrEqualToActualWidth_SetTopToZero_TopIsLessThanOrEqualToActualHeight
+         * _LeftIsLessThanZero_LeftIsLessThanOrEqualToActualWidth_TopIsLessThanZero_TopIsGreaterThanActualHeight
+         * _LeftIsLessThanZero_LeftIsLessThanOrEqualToActualWidth_TopIsLessThanZero_TopIsLessThanOrEqualToActualHeight
+         * 
+         * _SetLeftToZero_LeftIsGreaterThanActualWidth_SetTopToZero_TopIsGreaterThanActualHeight
+         * _SetLeftToZero_LeftIsGreaterThanActualWidth_SetTopToZero_TopIsLessThanOrEqualToActualHeight
+         * _SetLeftToZero_LeftIsGreaterThanActualWidth_TopIsLessThanZero_TopIsGreaterThanActualHeight
+         * _SetLeftToZero_LeftIsGreaterThanActualWidth_TopIsLessThanZero_TopIsLessThanOrEqualToActualHeight
+
+         * _SetLeftToZero_LeftIsLessThanOrEqualToActualWidth_SetTopToZero_TopIsGreaterThanActualHeight
+         * _SetLeftToZero_LeftIsLessThanOrEqualToActualWidth_SetTopToZero_TopIsLessThanOrEqualToActualHeight
+         * _SetLeftToZero_LeftIsLessThanOrEqualToActualWidth_TopIsLessThanZero_TopIsGreaterThanActualHeight
+         * _SetLeftToZero_LeftIsLessThanOrEqualToActualWidth_TopIsLessThanZero_TopIsLessThanOrEqualToActualHeight
+         */
     }
 }
