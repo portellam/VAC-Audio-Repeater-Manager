4cb77309f9604a3a7af4eff0a00c30d24299b69c
Alex Portell <amportell.13@gmail.com>
2023-12-19 22:26:59 -0500

    Finished pseudocode unit tests.

--- VACARM_GUI.Tests/AddDeviceDialogTests.xaml.cs
+++ VACARM_GUI.Tests/AddDeviceDialogTests.xaml.cs
@@ -7,32 +7,32 @@ namespace VACARM_GUI.Tests
     {
         // Constructor
         /*
-         * 
-         * 
+         * _SetMetadata
          */
 
         // CancelButton_Click()
         /*
-         * 
-         * 
+         * _Close_ConfirmClose
          */
 
         // OkButton_Click()
         /*
-         * 
-         * 
+         * _SelectedDeviceSelectedIndexIsNegativeOne_ReturnVoid
+         * _SelectedDeviceTypeSelectedIndexIsNegativeOne_ReturnVoid
+         * _SelectedIndexIsNotNegativeOne_SelectedDeviceTypeSelectedIndexIsNonZero_DevicesAreWaveOut
+         * _SelectedIndexIsNotNegativeOne_SelectedDeviceTypeSelectedIndexIsZero_DevicesAreWaveIn
          */
 
         // SelectDeviceType_SelectionChanged()
         /*
-         * 
-         * 
+         * _SelectedIndexIsZero_ItemsSourceIsWaveInNameList
+         * _SelectedIndexIsNonZero_ItemsSourceIsWaveOutNameList
          */
 
         // Window_MouseDown()
         /*
-         * 
-         * 
+         * _MouseButtonChangedButtonIsLeftButton_DragMove
+         * _MouseButtonChangedButtonIsNotLeftButton_ReturnVoid
          */
     }
 }
