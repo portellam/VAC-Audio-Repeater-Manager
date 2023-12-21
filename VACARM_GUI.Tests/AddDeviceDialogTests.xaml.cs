32b937e94de5d3562bbf0631715456472f945740
Alex Portell <amportell.13@gmail.com>
2023-12-21 12:40:27 -0500

    Added usings, appended reference to solve STA error, and began work on unit tests. Tests are not working at the moment.

--- VACARM_GUI.Tests/AddDeviceDialogTests.xaml.cs
+++ VACARM_GUI.Tests/AddDeviceDialogTests.xaml.cs
@@ -1,16 +1,42 @@
 ﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace VACARM_GUI.Tests
 {
-    [TestFixture]
+    [TestFixture, Apartment(ApartmentState.STA)]
     public class AddDeviceDialogTests
     {
-        private AddDeviceDialog addDeviceDialog;
+        private AddDeviceDialog addDeviceDialog, waveInAddDeviceDialog, waveOutAddDeviceDialog;
+        private Mock<AddDeviceDialog> addDeviceDialogMock;
+        private ComboBox WaveInDeviceType, WaveOutDeviceType;
+        private Mock<MouseDevice> mouseDeviceMock;
 
         [SetUp]
         public void Setup()
         {
-            addDeviceDialog = new AddDeviceDialog();
+            mouseDeviceMock = new Mock<MouseDevice>();
+
+            WaveInDeviceType = new ComboBox()
+            {
+                SelectedIndex = 0
+            };
+
+            WaveOutDeviceType = new ComboBox()
+            {
+                SelectedIndex = 1
+            };
+
+            waveInAddDeviceDialog = new AddDeviceDialog()
+            {
+                selectDevice = WaveInDeviceType,
+                selectDeviceType = WaveInDeviceType
+            };
+
+            waveOutAddDeviceDialog = new AddDeviceDialog()
+            {
+                selectDevice = WaveOutDeviceType
+            };
         }
 
         // Constructor
@@ -23,26 +49,26 @@ namespace VACARM_GUI.Tests
          * _Close_ConfirmClose
          */
 
-        [Test]
-        public void CancelButton_Click_ConfirmClose()
-        {
-            // Arrange
-            object sender = new object();
-            RoutedEventArgs routedEventArgs = new RoutedEventArgs();
+        // [Test]
+        // public void CancelButton_Click_ConfirmClose()
+        // {
+        //     // Arrange
+        //     object sender = new object();
+        //     RoutedEventArgs routedEventArgs = new RoutedEventArgs();
 
-            // Act
-            try
-            {
-                addDeviceDialog.CancelButton_Click(sender, routedEventArgs);
-            }
-            catch (Exception exception)
-            {
-                Assert.Fail(exception.ToString());
-            }
+        //     // Act
+        //     try
+        //     {
+        //         addDeviceDialog.CancelButton_Click(sender, routedEventArgs);
+        //     }
+        //     catch (Exception exception)
+        //     {
+        //         Assert.Fail(exception.ToString());
+        //     }
 
-            // Assert
-            Assert.Pass();
-        }
+        //     // Assert
+        //     Assert.Pass();
+        // }
 
         // OkButton_Click()
         /*
@@ -63,5 +89,29 @@ namespace VACARM_GUI.Tests
          * _MouseButtonChangedButtonIsLeftButton_DragMove
          * _MouseButtonChangedButtonIsNotLeftButton_ReturnVoid
          */
+
+        // [Test]
+        // public void Window_MouseDown_MouseButtonChangedButtonIsLeftButton_DragMove()
+        // {
+        //     // Arrange
+        //     mouseDeviceMock.Setup(x => x.LeftButton).Returns(MouseButtonState.Pressed;
+
+        //     MouseButtonEventArgs mouseButtonEventArgs = new MouseButtonEventArgs(mouseDeviceMock.Object, 0, MouseButton.Left);
+        //     //addDeviceDialogMock.Setup(x => x.selectDevice).Equals(WaveInDeviceType);
+        //     //addDeviceDialogMock.CallBase = true;
+
+        //     // Act
+        //     var result = addDeviceDialogMock.Setup(x => x.Window_MouseDown(WaveInDeviceType, mouseButtonEventArgs));
+
+        //     // Assert
+        //     Assert.Equals(mouseButtonEventArgs.MouseDevice, mouseDeviceMock.Object);
+        //     Assert.Equals(addDeviceDialogMock.Object.selectDeviceType, WaveInDeviceType);
+        //     Mock.Get(addDeviceDialogMock).Verify(x => x.Object.DragMove(), Times.Exactly(1));
+        // }
+
+        //[Test]
+        //public void Window_MouseDown_MouseButtonChangedButtonIsNotLeftButton_ReturnVoid()
+        //{
+        //}
     }
 }
