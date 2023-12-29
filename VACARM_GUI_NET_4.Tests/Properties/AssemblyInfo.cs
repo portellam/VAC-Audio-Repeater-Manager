a2b210fda41589c3d75d02a0d497551196e5ac79
Alex Portell <amportell.13@gmail.com>
2023-12-29 01:32:08 -0500

    Added NET 4 test project.

---
+++ VACARM_GUI_NET_4.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("VACARM_GUI_NET_4.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("VACARM_GUI_NET_4.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("aa5879cd-33cd-43a4-aa4e-9ef7426cd738")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ VACARM_GUI_NET_4.Tests/RepeaterMenuTests.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Moq;
+using NUnit.Framework;
+using TypeMock;
+using VACARM_GUI_NET_4;
+
+namespace VACARM_GUI_NET_4.Tests
+{
+    [TestFixture]
+    public class RepeaterMenuTests
+    {
+        // Constructor()
+        /*
+         * _RepeaterInfoIsNull_ThrowArgumentNullException
+         * _BipartiteDeviceGraphIsNull_ThrowArgumentNullException
+         * _InputsAreValid_SetGridInfoAndUpdateMetadata
+         */
+
+        // DeleteButton_Click()
+        /*
+         * _RemoveFromGraphAndCloseWindow_ConfirmWindowIsClosed
+         */
+
+        // Okay_Click()
+        /*
+         * _CloseWindow_ConfirmWindowIsClosed
+         */
+    }
+}
