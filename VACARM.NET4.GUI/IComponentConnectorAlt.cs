4b8069e22044523bed58d87e40932e9cf38e149a
Alex Portell <amportell.13@gmail.com>
2023-12-31 02:38:18 -0500

    Modularized and refactored new logic for easier reading.

--- VACARM.NET4.GUI/IComponentConnectorAlt.cs
+++ VACARM.NET4.GUI/IComponentConnectorAlt.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO.Packaging;
 using System.Reflection;
 using System.Windows.Markup;
@@ -18,6 +19,11 @@ namespace VACARM.NET4.GUI
         /// </summary>
         public static void InitializeComponent(Object _object)
         {
+            if (_object is null)
+            {
+                throw new ArgumentNullException(nameof(_object));
+            }
+
             Type type = _object.GetType();
             string typeName = type.Name.ToLower();
             string xamlName = $"{typeName}.xaml".ToLower();
@@ -25,7 +31,7 @@ namespace VACARM.NET4.GUI
 
             try
             {
-               InitializeComponentAlt(uri);
+                InitializeComponentAlt(uri);
             }
             catch
             {
@@ -43,42 +49,60 @@ namespace VACARM.NET4.GUI
         }
 
         /// <summary>
-        /// Loads the XAML UI content at XAML parse time.
         /// Unit-test-friendly alternative of the assembly method "IComponentConnector.InitializeComponent".
         /// </summary>
         /// <param name="baseUri">the base URI</param>
         internal static void InitializeComponentAlt(string baseUri)    //TODO: this is not working. Fix!
         {
+            if (baseUri is null || String.Equals(baseUri, String.Empty))
+            {
+                return;
+            }
+
             try
             {
-                var resourceLocater = new Uri(baseUri, UriKind.Relative);
-                BindingFlags bindingFlags = BindingFlags.NonPublic | BindingFlags.Static;
-                const string methodName1 = "GetResourceOrContentPart";
-
-                var packagePart = (PackagePart)typeof(Application).GetMethod(methodName1, bindingFlags)
-                    .Invoke(null, new object[] { resourceLocater });
-                var stream = packagePart.GetStream();
-
-                const string methodName2 = "PackAppBaseUri";
-
-                var uri = new Uri((Uri)typeof(BaseUriHelper).GetProperty(methodName2, bindingFlags)
-                    .GetValue(null, null), resourceLocater);
-
-                var parserContext = new ParserContext
-                {
-                    BaseUri = uri
-                };
-
-                const string methodName3 = "LoadBaml";
-
-                typeof(XamlReader).GetMethod(methodName3, bindingFlags)
-                    .Invoke(null, new object[] { stream, parserContext, true });
+                LoadXAML(baseUri);
             }
             catch
             {
                 //TODO: add logger here.
                 throw;
             }
-        }    
+        }
+
+        /// <summary>
+        /// Loads the XAML UI content at XAML parse time.
+        /// </summary>
+        /// <param name="baseUri">the base URI</param>
+        internal static void LoadXAML(string baseUri)
+        {
+            var resourceLocater = new Uri(baseUri, UriKind.Relative);
+            BindingFlags bindingFlags = BindingFlags.NonPublic | BindingFlags.Static;
+            const string firstMethodName = "GetResourceOrContentPart";
+            var firstMethodParameters = new object[] { resourceLocater };
+
+            var packagePart = (PackagePart)typeof(Application)
+                .GetMethod(firstMethodName, bindingFlags)
+                .Invoke(null, firstMethodParameters);
+
+            var stream = packagePart.GetStream();
+            const string secondConstructorName = "PackAppBaseUri";
+
+            var baseUriHelper = (Uri)typeof(BaseUriHelper)
+                .GetProperty(secondConstructorName, bindingFlags)
+                .GetValue(null, null);
+
+            var parserContext = new ParserContext
+            {
+                BaseUri = new Uri(baseUriHelper, resourceLocater)
+            };
+
+            const string thirdMethodName = "LoadBaml";
+            var thirdMethodParameters = new object[] { stream, parserContext, true };
+
+            typeof(XamlReader)
+                .GetMethod(thirdMethodName, bindingFlags)
+                .Invoke(null, thirdMethodParameters);
+        }
     }
 }
