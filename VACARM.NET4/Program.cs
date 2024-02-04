22346f6c9a3e8240d1995e92d2110a1b6c8c0f35
Alex Portell <amportell.13@gmail.com>
2024-02-03 20:14:13 -0500

    Added Program class. Added arguments as public getter.

---
+++ VACARM.NET4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Forms;
+using VACARM.NET4.Views;
+
+namespace VACARM.NET4
+{
+    public class Program
+    {
+        public static string[] Arguments { get; private set; }
+
+        public static void Main(string[] arguments)
+        {
+            Arguments = arguments;
+            Application.Run(new MainForm());
+        }
+    }
+}
