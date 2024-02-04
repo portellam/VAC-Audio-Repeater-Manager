86f483f55bffc2ceba4e99a6cbfca33b38ce130a
Alex Portell <amportell.13@gmail.com>
2024-02-04 06:55:59 -0500

    Added click event logic for File > Open, and accompanying strings.

--- VACARM.NET4/Views/MainForm.cs
+++ VACARM.NET4/Views/MainForm.cs
@@ -5,10 +5,39 @@ namespace VACARM.NET4.Views
 {
     public partial class MainForm : Form
     {
+        private string fileName;
+        private readonly static string applicationNameAsAbbreviation = "VACARM";
+        private readonly static string fileExtension = "." +
+            applicationNameAsAbbreviation.ToLower();
+
         public MainForm()
         {
             InitializeComponent();
             PostInitializeComponent();
+        }
+
+        /// <summary>
+        /// Click event logic for openToolStripMenuItem.
+        /// Get filename if dialog result is OK.
+        /// </summary>
+        /// <param name="sender">The sender object</param>
+        /// <param name="eventArgs">The event arguments</param>
+        private void openToolStripMenuItem_Click(object sender, EventArgs eventArgs)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog()
+            {
+                InitialDirectory = Application.CommonAppDataPath,
+                Filter = $"{applicationNameAsAbbreviation} files| *{fileExtension}*",
+            };
+
+            if (openFileDialog.ShowDialog() != DialogResult.OK)
+            {
+                return;
+            }
+
+            openFileDialog.FilterIndex = 0;
+            openFileDialog.RestoreDirectory = true;
+            fileName = openFileDialog.FileName;
         }
 
         private void exitToolStripMenuItem_Click(object sender, EventArgs e)
