c1d7461ae103385401ae8c9e9b7dc7c7d7fbd418
Alex Portell <amportell.13@gmail.com>
2024-02-04 06:42:23 -0500

    Set main as single thread.

--- VACARM.NET4/Program.cs
+++ VACARM.NET4/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Windows.Forms;
 using VACARM.NET4.Views;
 
 namespace VACARM.NET4
@@ -7,6 +8,7 @@ namespace VACARM.NET4
     {
         public static string[] Arguments { get; private set; }
 
+        [STAThread]
         public static void Main(string[] arguments)
         {
             Arguments = arguments;
