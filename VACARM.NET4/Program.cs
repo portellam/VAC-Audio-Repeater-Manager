a33414c02851cce2bd18cbdd5f62f0a722494ca7
Alex Portell <amportell.13@gmail.com>
2024-02-05 07:06:46 -0500

    Expanded argument-parsing logic and added dark mode validation logic to Main.

--- VACARM.NET4/Program.cs
+++ VACARM.NET4/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
+using System.Globalization;
+using System.Linq;
 using System.Windows.Forms;
 using VACARM.NET4.Views;
 
@@ -7,12 +10,79 @@ namespace VACARM.NET4
     public class Program
     {
         public static string[] Arguments { get; private set; }
+        public static bool IsDarkModeEnabledBeforeRunTime { get; private set; }
 
         [STAThread]
         public static void Main(string[] arguments)
         {
             Arguments = arguments;
+            ParseArguments();
+            DoesSystemSupportDarkMode();
             Application.Run(new MainForm());
+        }
+
+        /// <summary>
+        /// Parse arguments passed by command line.
+        /// </summary>
+        internal static void ParseArguments()
+        {
+            if (Arguments.Count() == 0)
+            {
+                return;
+            }
+
+            Arguments.ToList().ForEach(argument =>
+            {
+                if (!argument.StartsWith("/"))
+                {
+                    return;
+                }
+
+                switch (argument)
+                {
+                    case "/darkmode":
+                        IsDarkModeEnabledBeforeRunTime = true;
+                        break;
+
+                    default:
+                        break;
+                }
+            });
+        }
+
+        /// <summary>
+        /// Check if system has Registry key to indicate it supports Dark Mode,
+        /// and if it is enabled.
+        /// </summary>
+        internal static void DoesSystemSupportDarkMode()
+        {
+            if (IsDarkModeEnabledBeforeRunTime)
+            {
+                return;
+            }
+
+            const string subKey =
+                @"Software\Microsoft\Windows\CurrentVersion\Themes\Personalize";
+
+            try
+            {
+                var registryKey = Registry.CurrentUser.OpenSubKey(subKey);
+                const string registryKeyValue = "AppsUseLightTheme";
+                var windowsLightThemeIsEnabled = registryKey?.GetValue
+                    (registryKeyValue);
+
+                if (windowsLightThemeIsEnabled is null)
+                {
+                    return;
+                }
+
+                IsDarkModeEnabledBeforeRunTime = !Convert.ToBoolean
+                    (windowsLightThemeIsEnabled, CultureInfo.InvariantCulture);
+            }
+            catch
+            {
+                return;
+            }
         }
     }
 }
