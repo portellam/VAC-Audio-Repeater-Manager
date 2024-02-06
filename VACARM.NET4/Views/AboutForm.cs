3e0b90fd8a23eccef5953386332f06944af552d2
Alex Portell <amportell.13@gmail.com>
2024-02-06 04:41:14 -0500

    Update AboutForm.cs

--- VACARM.NET4/Views/AboutForm.cs
+++ VACARM.NET4/Views/AboutForm.cs
@@ -12,11 +12,11 @@ namespace VACARM.NET4.Views
         public AboutForm()
         {
             InitializeComponent();
-            this.Text = String.Format("About {0}", AssemblyTitle);
+            this.labelCompanyName.Text = AssemblyCompany;
+            this.labelCopyright.Text = AssemblyCopyright;
             this.labelProductName.Text = AssemblyProduct;
             this.labelVersion.Text = String.Format("Version {0}", AssemblyVersion);
-            this.labelCopyright.Text = AssemblyCopyright;
-            this.labelCompanyName.Text = AssemblyCompany;
+            this.Text = String.Format("About {0}", AssemblyTitle);
             this.textBoxDescription.Text = AssemblyDescription;
         }
 
