9bc97f0da5a1e7b339f724b4df10936fafea1606
Alex Portell <amportell.13@gmail.com>
2024-02-14 03:16:42 -0500

    Added BackColor as a color to update (missing in some methods). Added logic to update colors for ToolStripMenuItem recursively. Added logic in nested method to validate for ToolStripMenuItem and ToolStripItem.

--- VACARM.NET4/ViewModels/FormColorUpdater.cs
+++ VACARM.NET4/ViewModels/FormColorUpdater.cs
@@ -48,6 +48,87 @@ namespace VACARM.NET4.ViewModels
         #region Logic
 
         /// <summary>
+        /// Set the colors of every control in list, given dark mode is enabled or not.
+        /// </summary>
+        public static void SetColorsOfControlList(List<Control> controlList)
+        {
+            if (controlList.Count == 0)
+            {
+                return;
+            }
+
+            foreach (Control control in controlList)
+            {
+                control.BackColor = BackColor;
+                control.ForeColor = ForeColor;
+                SetColorsOfControlCollection(control.Controls);
+            };
+        }
+
+        /// <summary>
+        /// Set the colors of all nested tool strip menu items in a drop down
+        /// collection, given dark mode is enabled or not.
+        /// </summary>
+        internal static void SetColorsOfNestedToolStripMenuItem
+            (ToolStripMenuItem toolStripMenuItem)
+        {
+            SetColorsOfToolStripMenuItem(toolStripMenuItem);
+
+            if (toolStripMenuItem.DropDownItems.Count == 0)
+            {
+                return;
+            }
+
+            foreach (var item in
+                toolStripMenuItem.DropDownItems)
+            {
+                if (item is ToolStripMenuItem)
+                {
+                    SetColorsOfNestedToolStripMenuItem(item as ToolStripMenuItem);
+                }
+
+                if (item is ToolStripItem)
+                {
+                    SetColorsOfToolStripItem(item as ToolStripItem);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Set the colors of every tool strip menu item in drop down list, 
+        /// given dark mode is enabled or not.
+        /// </summary>
+        internal static void SetColorsOfToolStripItemDropDownList
+            (ToolStripItemCollection toolStripItemCollection)
+        {
+            foreach (ToolStripItem toolStripItem in toolStripItemCollection)
+            {
+                //toolStripItem.BackColor = BackColor;
+                toolStripItem.ForeColor = ForeColor;
+            }
+        }
+
+        /// <summary>
+        /// Set the colors of a tool strip item, given dark mode is enabled or not.
+        /// </summary>
+        internal static void SetColorsOfToolStripItem
+            (ToolStripItem toolStripItem)
+        {
+            toolStripItem.BackColor = BackColor;
+            toolStripItem.ForeColor = ForeColor;
+        }
+
+        /// <summary>
+        /// Set the colors of a tool strip menu item, given dark mode is enabled or not.
+        /// </summary>
+        internal static void SetColorsOfToolStripMenuItem
+            (ToolStripMenuItem toolStripMenuItem)
+        {
+            toolStripMenuItem.BackColor = BackColor;
+            toolStripMenuItem.ForeColor = ForeColor;
+        }
+
+        /// <summary>
         /// Set the colors of the constructor, given dark mode is enabled or not.
         /// </summary>
         public static void SetColorsOfConstructor(object parentObject)
@@ -89,24 +170,6 @@ namespace VACARM.NET4.ViewModels
         }
 
         /// <summary>
-        /// Set the colors of every control in list, given dark mode is enabled or not.
-        /// </summary>
-        public static void SetColorsOfControlList(List<Control> controlList)
-        {
-            if (controlList.Count == 0)
-            {
-                return;
-            }
-
-            foreach (Control control in controlList)
-            {
-                control.BackColor = BackColor;
-                control.ForeColor = ForeColor;
-                SetColorsOfControlCollection(control.Controls);
-            };
-        }
-
-        /// <summary>
         /// Set the colors of every tool strip menu item in list, 
         /// given dark mode is enabled or not.
         /// </summary>
@@ -115,22 +178,8 @@ namespace VACARM.NET4.ViewModels
         {
             toolStripMenuItemList.ForEach(toolStripMenuItem =>
             {
-                toolStripMenuItem.ForeColor = ForeColor;
-                SetColorsOfToolStripItemDropDownList(toolStripMenuItem.DropDownItems);
+                SetColorsOfNestedToolStripMenuItem(toolStripMenuItem);
             });
-        }
-
-        /// <summary>
-        /// Set the colors of every tool strip menu item in drop down list, 
-        /// given dark mode is enabled or not.
-        /// </summary>
-        internal static void SetColorsOfToolStripItemDropDownList
-            (ToolStripItemCollection toolStripItemCollection)
-        {
-            foreach(ToolStripItem toolStripItem in toolStripItemCollection)
-            {
-                toolStripItem.ForeColor = ForeColor;
-            }
         }
 
         #endregion
