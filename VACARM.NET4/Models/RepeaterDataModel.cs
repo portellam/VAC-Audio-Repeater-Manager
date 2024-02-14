47388c4058dff7406cedc706a9876d6f43562d04
Alex Portell <amportell.13@gmail.com>
2024-02-14 04:59:58 -0500

    Added repeater data model.

---
+++ VACARM.NET4/Models/RepeaterDataModel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using VACARM.NET4.ViewModels;
+
+namespace VACARM.NET4.Models
+{
+    public class RepeaterDataModel
+    {
+        #region Parameters
+
+        public Dictionary<DeviceControl, Dictionary<DeviceControl, RepeaterModel>>
+            RepeaterData { get; private set; }
+
+        #endregion
+
+        #region Logic
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public RepeaterDataModel()
+        {
+            RepeaterData = new Dictionary<DeviceControl,
+                Dictionary<DeviceControl, RepeaterModel>>();
+        }
+
+        #endregion
+    }
+}
