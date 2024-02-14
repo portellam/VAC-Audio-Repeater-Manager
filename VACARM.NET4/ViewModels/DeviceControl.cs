575ae3a3ac643f9473fd0088b86bcdcf31b02bc7
Alex Portell <amportell.13@gmail.com>
2024-02-14 04:56:56 -0500

    Added DeviceControl.

---
+++ VACARM.NET4/ViewModels/DeviceControl.cs
@@ -0,0 +1,23 @@
+﻿using NAudio.CoreAudioApi;
+using System.Windows.Forms;
+
+namespace VACARM.NET4.ViewModels
+{
+    public partial class DeviceControl : UserControl
+    {
+        #region Parameters
+
+        public MMDevice MMDevice { get; set; }
+
+        #endregion
+
+        #region Logic
+
+        public DeviceControl()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+    }
+}
