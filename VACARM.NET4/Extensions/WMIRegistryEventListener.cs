0c751fb4d541a8485a9b2f29538b1123be6fad9b
Alex Portell <amportell.13@gmail.com>
2024-02-20 21:40:03 -0500

    Added WMIRegistryEventListener, see desc for details.
    URL: https://erikengberg.com/4-ways-to-monitor-windows-registry-using-c/

---
+++ VACARM.NET4/Extensions/WMIRegistryEventListener.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Management;
+
+namespace VACARM.NET4.Extensions
+{
+    public class WmiRegistryEventListener : IDisposable
+    {
+        #region Parameters
+
+        private const string databaseQuery = @"SELECT * FROM RegistryTreeChangeEvent " +
+            "WHERE Hive='HKEY_LOCAL_MACHINE' " +
+            "AND RootPath='SOFTWARE'";
+
+        private ManagementEventWatcher managementEventWatcher;
+
+        #endregion
+
+        #region Logic
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public WmiRegistryEventListener()
+        {
+            managementEventWatcher = new ManagementEventWatcher(databaseQuery);
+
+            managementEventWatcher.EventArrived +=
+                new EventArrivedEventHandler(registryEventHandler);
+
+            managementEventWatcher.Start();
+        }
+
+        /// <summary>
+        /// Whenever an event is received, write the event to console.
+        /// </summary>
+        /// <param name="sender">The sender object</param>
+        /// <param name="eventArrivedEventArgs">The event arrived event arguments
+        /// </param>
+        internal void registryEventHandler
+            (object sender, EventArrivedEventArgs eventArrivedEventArgs)
+        {
+            Console.WriteLine("Received an event:");
+
+            foreach (var prop in eventArrivedEventArgs.NewEvent.Properties)
+            {
+                Console.WriteLine($"{prop.Name}:{prop.Value.ToString()}");
+            }
+
+            Console.WriteLine();
+        }
+
+        /// <summary>
+        /// Dispose the constructor object.
+        /// </summary>
+        public void Dispose()
+        {
+            this.managementEventWatcher?.Dispose();
+        }
+
+        #endregion
+    }
+}
