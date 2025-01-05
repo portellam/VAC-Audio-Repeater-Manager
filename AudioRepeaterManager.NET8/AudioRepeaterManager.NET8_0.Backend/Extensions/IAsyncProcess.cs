9332e3a708f62c9ccf6834bec670feb602f45ce9
portellam <amportell.13@gmail.com>
2025-01-05 17:56:04 -0500

    Added logic to start processes.

---
+++ AudioRepeaterManager.NET8/AudioRepeaterManager.NET8_0.Backend/Extensions/IAsyncProcess.cs
@@ -0,0 +1,15 @@
+﻿namespace AudioRepeaterManager.NET8_0.Backend.Extensions
+{
+  public interface IAsyncProcess
+  {
+    #region Logic
+
+    async Task<int> RunProcessAsync
+    (
+      string fileName,
+      string arguments
+    ) => throw new NotImplementedException();
+
+    #endregion
+  }
+}
