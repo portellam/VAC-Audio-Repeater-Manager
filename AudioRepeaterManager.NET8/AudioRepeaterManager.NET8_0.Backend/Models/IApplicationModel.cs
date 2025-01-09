312d0c8845b7a6b266b872e78730333113548507
portellam <amportell.13@gmail.com>
2025-01-09 07:14:37 -0500

    Create IApplicationModel.cs

---
+++ AudioRepeaterManager.NET8/AudioRepeaterManager.NET8_0.Backend/Models/IApplicationModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AudioRepeaterManager.NET8_0.Backend.Models
+{
+  public interface IApplicationModel
+  {
+    #region Parameters
+
+    /// <summary>
+    /// Primary Key
+    /// </summary>
+    uint Id { get; set; }
+
+    /// <summary>
+    /// Foreign Key
+    /// </summary>
+    uint RepeaterId { get; set; }
+
+    bool IsRunning { get; set; }
+    event PropertyChangedEventHandler PropertyChanged;
+    string Priority { get; set; }
+    string StartArguments { get; }
+    string StopArguments { get; }
+    string WindowName { get; }
+    uint ProcessId { get; set; }
+
+    #endregion
+
+    #region Logic
+
+    Task<int> Restart();
+    Task<int> Start();
+    Task<int> Stop();
+    void Update();
+
+    #endregion
+  }
+}
