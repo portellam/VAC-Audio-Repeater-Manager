8a17e380176297dff669b472ac3d874febd02098
portellam <amportell.13@gmail.com>
2025-01-13 05:03:36 -0500

    Added params and methods.

--- AudioRepeaterManager.NET8/AudioRepeaterManager.NET8_0.Backend/Repositories/IProcessRepository.cs
+++ AudioRepeaterManager.NET8/AudioRepeaterManager.NET8_0.Backend/Repositories/IProcessRepository.cs
@@ -8,19 +8,21 @@ namespace AudioRepeaterManager.NET8_0.Backend.Repositories
     #region Parameters
 
     event PropertyChangedEventHandler PropertyChanged;
+    List<string> ExecutableNameList { get; set; }
 
     #endregion
 
     #region Logic
 
     List<Process> GetAll();
-    List<Process> GetAllStarted();
-    List<Process> GetAllStopped();
     List<Process> GetRange(List<int> idList);
     Process Get(int id);
-    Task<int> Restart(int id);
-    Task<int> Start(int id);
-    Task<int> Stop(int id);
+    Task<int> Run(int id);
+    Task<int> RunAll();
+    Task<int> RunRange(int id);
+    Task<int> Kill(int id);
+    Task<int> KillAll();
+    Task<int> KillRange(int id);
     void Update();
 
     #endregion
