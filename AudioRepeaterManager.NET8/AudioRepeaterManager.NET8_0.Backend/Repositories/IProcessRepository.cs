ec1516743f77015b9b9453f8f6c1f035ecb8d32f
portellam <amportell.13@gmail.com>
2025-01-12 21:23:10 -0500

    Added process repo.

---
+++ AudioRepeaterManager.NET8/AudioRepeaterManager.NET8_0.Backend/Repositories/IProcessRepository.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using System.Diagnostics;
+
+namespace AudioRepeaterManager.NET8_0.Backend.Repositories
+{
+  public interface IProcessRepository
+  {
+    #region Parameters
+
+    event PropertyChangedEventHandler PropertyChanged;
+
+    #endregion
+
+    #region Logic
+
+    List<Process> GetAll();
+    List<Process> GetAllStarted();
+    List<Process> GetAllStopped();
+    List<Process> GetRange(List<int> idList);
+    Process Get(int id);
+    Task<int> Restart(int id);
+    Task<int> Start(int id);
+    Task<int> Stop(int id);
+    void Update();
+
+    #endregion
+  }
+}
---
+++ AudioRepeaterManager.NET8/AudioRepeaterManager.NET8_0.Backend/Repositories/ProcessRepository.cs
@@ -0,0 +1,105 @@
+﻿using NAudio.CoreAudioApi;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+
+namespace AudioRepeaterManager.NET8_0.Backend.Repositories
+{
+  public class ProcessRepository :
+    INotifyPropertyChanged,
+    IProcessRepository
+  {
+    #region Parameters
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    private List<string> ExecutableNameList
+    {
+      get
+      {
+        return new List<string>
+        {
+          Global.KSExecutableName,
+          Global.MMEExecutableName,
+        };
+      }
+    }
+
+    /// <summary>
+    /// The list of processes.
+    /// </summary>
+    private List<Process> List;
+
+    #endregion
+
+    #region Logic
+
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    [ExcludeFromCodeCoverage]
+    public ProcessRepository()
+    {
+      List = new List<Process>();
+      Update();
+    }
+
+    /// <summary>
+    /// Set the process list.
+    /// </summary>
+    public void Update()
+    {
+      List.Clear();
+
+      ExecutableNameList
+        .ForEach
+        (
+          x =>
+          {
+            List<Process> list = Process
+              .GetProcessesByName(x)
+              .ToList();
+
+            if (list.Count() == 0)
+            {
+              Debug.WriteLine
+              (
+                string.Format
+                (
+                  "Found no processes\t=> ExecutableName: {0}",
+                  x
+                )
+              );
+            }
+
+            else
+            {
+              Debug.WriteLine
+              (
+               string.Format
+               (
+                 "Found processes\t=> ExecutableName: {0}, Count: {1}",
+                 x,
+                 list.Count()
+               )
+             );
+            }
+
+            List.AddRange(list);
+          }
+        );
+
+      Debug.WriteLine
+        (
+          string.Format
+          (
+            "Updated process list\t=> Count: {0}",
+            List.Count()
+          )
+        );
+    }
+  }
+
+  #endregion
+}
