6306b43e9c875154ea8eececade96705cf35dce8
portellam <amportell.13@gmail.com>
2025-01-26 14:12:16 -0500

    Create RepeaterCommands.cs

---
+++ AudioRepeaterManager.NET8/AudioRepeaterManager.NET8_0.Application/Commands/RepeaterCommands.cs
@@ -0,0 +1,15 @@
+﻿namespace AudioRepeaterManager.NET8_0.Application.Commands
+{
+  public class RepeaterCommands
+  {
+    /*
+     * TODO:
+     * 
+     * -add command logic
+     *  -start a repeater; starts the repeater process.
+     *  -stop a repeater; stops the repeater process.
+     *  -restart a repeater; restarts the repeater process.
+     * 
+     */
+  }
+}
