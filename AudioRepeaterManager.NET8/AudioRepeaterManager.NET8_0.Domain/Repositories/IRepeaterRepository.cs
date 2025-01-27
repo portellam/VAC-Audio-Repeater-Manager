0a474dc8f29a3fae609b1ede10e74f3f3eea346b
portellam <amportell.13@gmail.com>
2025-01-27 14:28:00 -0500

    Added NOTES.

--- AudioRepeaterManager.NET8/AudioRepeaterManager.NET8_0.Domain/Repositories/IRepeaterRepository.cs
+++ AudioRepeaterManager.NET8/AudioRepeaterManager.NET8_0.Domain/Repositories/IRepeaterRepository.cs
@@ -20,29 +20,23 @@ namespace AudioRepeaterManager.NET8_0.Domain.Repositories
     List<RepeaterModel> GetAllEnabled();
     List<RepeaterModel> GetAllStarted();
     List<RepeaterModel> GetAllStopped();
-    List<RepeaterModel> GetRange(string deviceName);
+    List<RepeaterModel> GetRange(string deviceName); // NOTE: this may be useful.
     List<RepeaterModel> GetRange(uint? id);
-    List<RepeaterModel> GetRange(List<string> deviceNameList);
+    List<RepeaterModel> GetRange(List<string> deviceNameList); // NOTE: I may keep this one.
     List<RepeaterModel> GetRange(List<uint?> idList);
     List<RepeaterModel> GetRangeByDeviceId(List<uint?> deviceIdList);
     Task<int> Restart(RepeaterModel model);
     Task<int> Restart(uint? id);
-    Task<int> Restart(KeyValuePair<uint?, uint?> inputAndOutputDeviceId);
     Task<int> RestartRange(List<RepeaterModel> model);
     Task<int> RestartRange(List<uint?> idList);
-    Task<int> RestartRange(KeyValuePair<uint?, uint?> inputAndOutputDeviceId);
     Task<int> Start(RepeaterModel model);
     Task<int> Start(uint? id);
-    Task<int> Start(KeyValuePair<uint?, uint?> inputAndOutputDeviceId);
     Task<int> StartRange(List<RepeaterModel> model);
     Task<int> StartRange(List<uint?> idList);
-    Task<int> StartRange(KeyValuePair<uint?, uint?> inputAndOutputDeviceId);
     Task<int> Stop(RepeaterModel model);
     Task<int> Stop(uint? id);
-    Task<int> Stop(KeyValuePair<uint?, uint?> inputAndOutputDeviceId);
     Task<int> StopRange(List<RepeaterModel> model);
     Task<int> StopRange(List<uint?> idList);
-    Task<int> StopRange(KeyValuePair<uint?, uint?> inputAndOutputDeviceId);
 
     void Add
     (
