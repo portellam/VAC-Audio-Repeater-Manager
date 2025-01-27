5e0d35afd03d666e2f2f4998305b996c120ec213
portellam <amportell.13@gmail.com>
2025-01-26 22:23:27 -0500

    Update IProcessRepository.cs

--- AudioRepeaterManager.NET8/AudioRepeaterManager.NET8_0.Domain/Repositories/IProcessRepository.cs
+++ AudioRepeaterManager.NET8/AudioRepeaterManager.NET8_0.Domain/Repositories/IProcessRepository.cs
@@ -8,7 +8,7 @@ namespace AudioRepeaterManager.NET8_0.Domain.Repositories
     #region Parameters
 
     event PropertyChangedEventHandler PropertyChanged;
-    List<string> ExecutableNameList { get; set; }
+    List<string> FileNameList { get; set; }
 
     #endregion
 
@@ -18,11 +18,11 @@ namespace AudioRepeaterManager.NET8_0.Domain.Repositories
     List<Process> GetRange(List<int> idList);
     Process Get(int id);
     Task<int> Run(int id);
-    Task<int> RunAll();
-    Task<int> RunRange(List<int> id);
+    Task<int[]> RunAll();
+    Task<int[]> RunRange(List<int> id);
     Task<int> Kill(int id);
-    Task<int> KillAll();
-    Task<int> KillRange(List<int> id);
+    Task<int[]> KillAll();
+    Task<int[]> KillRange(List<int> id);
     void Update();
 
     #endregion
