52cc631355fb7c4a0747241f5a3e4bb2b93ba4ee
portellam <amportell.13@gmail.com>
2025-01-28 14:13:04 -0500

    Create IRepository.cs

---
+++ VACARM.Core/VACARM.Core/Repositories/IRepository.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq.Expressions;
+using VACARM.Core.Entities;
+
+namespace VACARM.Core.Repositories
+{
+  public interface IRepository<T>
+  {
+    #region Logic
+
+    T? Get(Func<T, bool> predicate);
+    IEnumerable<T> GetAll();
+    IEnumerable<T> GetRange(Func<T, bool> predicate);
+    IQueryable<T> Queryable();
+    void Remove(T t);
+    void RemoveRange(Predicate<T> predicate);
+    void RemoveRange(IEnumerable<T> enumerable);
+    void Set(T t);
+    void SetRange(IEnumerable<T> enumerable);
+    void Update(T t);
+    void UpdateRange(IEnumerable<T> enumerable);
+
+    #endregion
+  }
+}
