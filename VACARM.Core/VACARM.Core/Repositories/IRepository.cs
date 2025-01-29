1fc4ddc5a59df81672b829223369e1c94b1ea466
portellam <amportell.13@gmail.com>
2025-01-29 10:22:53 -0500

    Update IRepository.cs

--- VACARM.Core/VACARM.Core/Repositories/IRepository.cs
+++ VACARM.Core/VACARM.Core/Repositories/IRepository.cs
@@ -1,19 +1,16 @@
-﻿using System.Linq.Expressions;
-using VACARM.Core.Entities;
-
-namespace VACARM.Core.Repositories
+﻿namespace VACARM.Core.Repositories
 {
   public interface IRepository<T>
   {
     #region Logic
 
     T? Get(Func<T, bool> predicate);
-    IEnumerable<T> GetAll();
-    IEnumerable<T> GetRange(Func<T, bool> predicate);
+    List<T> GetAll();
+    List<T> GetRange(Func<T, bool> predicate);
     IQueryable<T> Queryable();
     void Remove(T t);
-    void RemoveRange(Predicate<T> predicate);
-    void RemoveRange(IEnumerable<T> enumerable);
+    void RemoveAll();
+    void RemoveRange(Func<T, bool> predicate);
     void Set(T t);
     void SetRange(IEnumerable<T> enumerable);
     void Update(T t);
