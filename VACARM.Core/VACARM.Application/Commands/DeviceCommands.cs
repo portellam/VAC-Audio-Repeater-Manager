12b33df96a6658631c0aff2068ace17ddf7e2f10
portellam <amportell.13@gmail.com>
2025-01-29 10:22:33 -0500

    Added Commands to Application.

---
+++ VACARM.Core/VACARM.Application/Commands/DeviceCommands.cs
@@ -0,0 +1,259 @@
+﻿using System.Diagnostics;
+using NAudio.CoreAudioApi;
+
+namespace VACARM.Application.Commands
+{
+  public static class DeviceCommands
+  {
+    #region Logic
+
+    /// <summary>
+    /// Is the audio device started.
+    /// </summary>
+    /// <param name="model">The audio device</param>
+    /// <returns>True/false is the audio device started.</returns>
+    private static bool IsStarted(MMDevice model)
+    {
+      return model.State != DeviceState.Disabled;
+    }
+
+    /// <summary>
+    /// Reset the audio device.
+    /// </summary>
+    /// <param name="model">the audio device</param>
+    public static void Reset(MMDevice? model)
+    {
+      if (model is null)
+      {
+        Debug.WriteLine
+          (
+            "Failed to reset audio device. " +
+            "Audio device is null."
+          );
+
+        return;
+      }
+
+      if (IsStarted(model))
+      {
+        Debug
+          .WriteLine
+          (
+            string
+            .Format
+            (
+              "Failed to reset audio device. " +
+              "Audio device is already started\t=> Name: {0}.",
+              model.FriendlyName
+            )
+          );
+
+        return;
+      }
+
+      try
+      {
+        model.AudioClient
+          .Reset();
+      }
+      catch
+      {
+        Debug.WriteLine
+        (
+          string.Format
+          (
+            "Failed to reset audio device\t=> Name: {0}.",
+            model.FriendlyName
+          )
+        );
+
+        return;
+      }
+
+      Debug.WriteLine
+      (
+        string.Format
+        (
+          "Reset audio device\t=> Name: {0}.",
+          model.FriendlyName
+        )
+      );
+    }
+
+    /// <summary>
+    /// Start the audio device.
+    /// </summary>
+    /// <param name="model">the audio device</param>
+    public static void Start(MMDevice? model)
+    {
+      if (model is null)
+      {
+        Debug.WriteLine
+          (
+            "Failed to start the audio device. " +
+            "The audio device is null."
+          );
+
+        return;
+      }
+
+      if (IsStarted(model))
+      {
+        Debug
+          .WriteLine
+          (
+            string
+            .Format
+            (
+              "The audio device is already started\t=> Name: {0}.",
+              model.FriendlyName
+            )
+          );
+
+        return;
+      }
+
+      try
+      {
+        model.AudioClient
+          .Start();
+      }
+      catch
+      {
+        Debug.WriteLine
+        (
+          string.Format
+          (
+            "Failed to start the audio device\t=> Name: {0}.",
+            model.FriendlyName
+          )
+        );
+
+        return;
+      }
+
+      Debug.WriteLine
+      (
+        string.Format
+        (
+          "Start the audio device\t=> Name: {0}.",
+          model.FriendlyName
+        )
+      );
+
+      Update(model);
+    }
+
+    /// <summary>
+    /// Stop the audio device.
+    /// </summary>
+    /// <param name="model">the audio device</param>
+    public static void Stop(MMDevice? model)
+    {
+      if (model is null)
+      {
+        Debug.WriteLine
+          (
+            "Failed to stop the audio device. " +
+            "The audio device is null."
+          );
+
+        return;
+      }
+
+      if (!IsStarted(model))
+      {
+        Debug
+          .WriteLine
+          (
+            string
+            .Format
+            (
+              "The audio device is already stopped\t=> Name: {0}.",
+              model.FriendlyName
+            )
+          );
+
+        return;
+      }
+
+      try
+      {
+        model.AudioClient
+          .Stop();
+      }
+      catch
+      {
+        Debug.WriteLine
+        (
+          string.Format
+          (
+            "Failed to stop the audio device\t=> Name: {0}.",
+            model.FriendlyName
+          )
+        );
+
+        return;
+      }
+
+      Debug.WriteLine
+      (
+        string.Format
+        (
+          "Stopped the audio device\t=> Name: {0}.",
+          model.FriendlyName
+        )
+      );
+
+      Update(model);
+    }
+
+    /// <summary>
+    /// Update the audio device.
+    /// </summary>
+    /// <param name="model">The audio device</param>
+    public static void Update(MMDevice? model)
+    {
+      if (model is null)
+      {
+        Debug.WriteLine
+          (
+            "Failed to update the audio device. " +
+            "The audio device is null."
+          );
+
+        return;
+      }
+
+      try
+      {
+        model.AudioSessionManager
+          .RefreshSessions();
+      }
+      catch
+      {
+        Debug.WriteLine
+        (
+          string.Format
+          (
+            "Failed to update the audio device\t=> Name: {0}.",
+            model.FriendlyName
+          )
+        );
+
+        return;
+      }
+
+      Debug.WriteLine
+      (
+        string.Format
+        (
+          "Updated the audio device\t=> Name: {0}.",
+          model.FriendlyName
+        )
+      );
+    }
+
+    #endregion
+  }
+}
---
+++ VACARM.Core/VACARM.Application/Commands/ExecutableCommands.cs
@@ -0,0 +1,320 @@
+﻿using System.Diagnostics;
+
+namespace VACARM.Application.Commands
+{
+  public class ExecutableCommands
+  {
+    #region Logic
+
+    /// <summary>
+    /// The default process.
+    /// </summary>
+    private static Process DefaultProcess { get; set; } = new Process()
+    {
+      EnableRaisingEvents = true,
+
+      StartInfo =
+        {
+          CreateNoWindow = true,
+          RedirectStandardError = true,
+          RedirectStandardOutput = true,
+          UseShellExecute = false,
+          WindowStyle = ProcessWindowStyle.Hidden,
+        },
+    };
+
+    /// <summary>
+    /// Get the process for the executable.
+    /// </summary>
+    /// <param name="processId">The process ID</param>
+    /// <returns>The process</returns>
+    private static Process? Get(int? processId)
+    {
+      Process? process = null;
+
+      if
+      (
+        processId is null
+        || processId < 0
+      )
+      {
+        Debug.WriteLine
+        (
+          "Process ID is either null or less than zero."
+        );
+
+        return process;
+      }
+
+      try
+      {
+        process = Process.GetProcessById((int)processId);
+
+        Debug.WriteLine
+        (
+          string.Format
+          (
+            "Process: {0}",
+            process
+          )
+        );
+      }
+
+      catch (Exception exception)
+      {
+        Debug.WriteLine
+        (
+          "Failed to get process. " +
+          "Process ID is either null or less than zero."
+        );
+
+        process = null;
+      }
+
+      return process;
+    }
+
+    /// <summary>
+    /// Is the executable running.
+    /// </summary>
+    /// <param name="processId">The process ID</param>
+    /// <returns>True/false is the executable running.</returns>
+    public static bool IsRunning(int? processId)
+    {
+      return Get(processId) != null;
+    }
+
+    /// <summary>
+    /// Restart the executable.
+    /// </summary>
+    /// <param name="processId">The process ID</param>
+    /// <param name="fileName">The executable file name</param>
+    /// <param name="startArguments">The start arguments</param>
+    /// <param name="stopArguments">The stop arguments</param>
+    /// <returns>The process ID.</returns>
+    public async static Task<int?> Restart
+    (
+      int? processId,
+      string fileName,
+      string startArguments,
+      string stopArguments
+    )
+    {
+      Debug.WriteLine
+      (
+        string.Format
+        (
+          "Restarting the executable\t=> Process ID: {0}",
+          processId
+        )
+      );
+
+      int? result = await Stop
+        (
+          processId,
+          stopArguments
+        );
+
+      bool isRunning = result == 0;
+
+      if (isRunning)
+      {
+        Debug.WriteLine("Failed to restart the executable.");
+        return result;
+      }
+
+      result = await Start
+        (
+          processId,
+          fileName,
+          startArguments
+        );
+
+      isRunning = IsRunning(result);
+
+      if (!isRunning)
+      {
+        Debug.WriteLine("Failed to restart the executable.");
+      }
+
+      else
+      {
+        Debug.WriteLine("Restarted the executable.");
+      }
+
+      return result;
+    }
+
+    /// <summary>
+    /// Start the executable.
+    /// </summary>
+    /// <param name="processId">The process ID</param>
+    /// <param name="fileName">The executable file name</param>
+    /// <param name="startArguments">The start arguments</param>
+    /// <returns>The actual process ID.</returns>
+    public async static Task<int?> Start
+    (
+      int? processId,
+      string fileName,
+      string startArguments
+    )
+    {
+      Process? process = Get(processId);
+
+      if
+      (
+        string.IsNullOrEmpty(fileName)
+        || string.IsNullOrWhiteSpace(fileName)
+      )
+      {
+        Debug.WriteLine
+        (
+          string.Format
+          (
+            "Failed to start executable. " +
+            "File name is either empty, null, or whitespace\t=> " +
+            "Process ID: {0}, File Name: {1}, Start Arguments: {2}",
+            processId,
+            fileName,
+            startArguments
+          )
+        );
+
+        return null;
+      }
+
+      if (process != null)
+      {
+        Debug.WriteLine
+        (
+          string.Format
+          (
+            "Executable already started\t=> " +
+            "Process ID: {0}, File Name: {1}, Start Arguments: {2}",
+            processId,
+            fileName,
+            startArguments
+          )
+        );
+
+        return processId;
+      }
+
+      Debug.WriteLine
+      (
+        string.Format
+        (
+          "Starting executable\t=> " +
+          "Process ID: {0}, File Name: {1}, Start Arguments: {2}",
+          processId,
+          fileName,
+          startArguments
+        )
+      );
+
+      process = DefaultProcess;
+      process.StartInfo.FileName = fileName;
+
+      bool startAnArgument =
+        !(
+          string.IsNullOrEmpty(startArguments)
+          || string.IsNullOrWhiteSpace(startArguments)
+        );
+
+      if (startAnArgument)
+      {
+        process.StartInfo.Arguments = startArguments;
+      }
+
+      var result = await ProcessCommands.RunAsync(process);
+      bool isRunning = IsRunning(result);
+
+      if (isRunning)
+      {
+        Debug.WriteLine("Failed to start the executable.");
+      }
+
+      else
+      {
+        Debug.WriteLine("Started the executable.");
+      }
+
+      return process.Id;
+    }
+
+    /// <summary>
+    /// Stop the executable.
+    /// </summary>
+    /// <param name="processId">The process ID</param>
+    /// <param name="stopArguments">The stop arguments</param>
+    /// <returns>The exit code.</returns>
+    public async static Task<int> Stop
+    (
+      int? processId,
+      string stopArguments
+    )
+    {
+      int result = 1;
+      Process? process = Get(processId);
+
+      if (process is null)
+      {
+        Debug.WriteLine
+        (
+          string.Format
+          (
+            "The executable has already stopped\t=> " +
+            "Process ID: {0}, Stop Arguments: {1}",
+            processId,
+            stopArguments
+          )
+        );
+
+        result = 0;
+        return result;
+      }
+
+      Debug.WriteLine
+      (
+        string.Format
+        (
+          "Starting the executable\t=> " +
+          "Process ID: {0}, Stop Arguments: {1}",
+          processId,
+          stopArguments
+        )
+      );
+
+      bool killWithPrejudice =
+        string.IsNullOrEmpty(stopArguments)
+        || string.IsNullOrWhiteSpace(stopArguments);
+
+      if (killWithPrejudice)
+      {
+        result = await ProcessCommands.KillAsync(process);
+      }
+
+      else
+      {
+        process.StartInfo.Arguments = stopArguments;
+        result = await ProcessCommands.RunAsync(process);
+      }
+
+      bool isRunning = IsRunning(result);
+
+      if (isRunning)
+      {
+        Debug.WriteLine("Failed to stop the executable.");
+      }
+
+      else
+      {
+        Debug.WriteLine("Stopped the executable.");
+      }
+
+      return result;
+    }
+  }
+
+  #endregion
+}
