8da9e6341d218449bd76eea4560e724a65652f25
portellam <amportell.13@gmail.com>
2025-01-29 10:22:21 -0500

    Copied shared parameters into a Common project.

---
+++ VACARM.Core/VACARM.Common/CommonPaths.cs
@@ -0,0 +1,102 @@
+﻿namespace VACARM.Common
+{
+  public partial class Common
+  {
+    #region Parameters
+
+    /// <summary>
+    /// Typically "C:\Program Files\Virtual Audio Cable\".
+    /// </summary>
+    private static string parentPathNameForBitMatchedProcessAndSystem =
+      $"{systemRootPathName}Program Files\\{ReferencedApplicationName}\\";
+
+    /// <summary>
+    /// Typically "C:\Program Files (x86)\Virtual Audio Cable\".
+    /// </summary>
+    private static string parentPathNameForBitUnmatchedProcessAndSystem =
+      $"{systemRootPathName}Program Files (x86)\\{ReferencedApplicationName}\\";
+
+    /// <summary>
+    /// Typically "C:\".
+    /// </summary>
+    private static string systemRootPathName = Path.GetPathRoot
+      (
+        Environment.GetFolderPath
+        (
+          Environment.SpecialFolder.System
+        )
+      );
+
+    private static bool doesProcessAndSystemBitMatch =
+      Environment.Is64BitProcess == Environment.Is64BitOperatingSystem;
+
+    public static bool PreferLegacyExecutable = false;
+
+    /// <summary>
+    /// The name of the executable for Multi Media Extensions (MME) or legacy use-cases.
+    /// </summary>
+    public static string MMEExecutableName
+    {
+      get
+      {
+        return "audiorepeater.exe";
+      }
+    }
+
+    /// <summary>
+    /// The name of the executable for Kernel Streaming (KS).
+    /// </summary>
+    public static string KSExecutableName
+    {
+      get
+      {
+        return "audiorepeater_ks.exe";
+      }
+    }
+
+    /// <summary>
+    /// The preferred name of the executable.
+    /// </summary>
+    public static string PreferredExecutableName
+    {
+      get
+      {
+        if (PreferLegacyExecutable)
+        {
+          return MMEExecutableName;
+        }
+
+        return KSExecutableName;
+      }
+    }
+
+    /// <summary>
+    /// The expected executable full path name.
+    /// </summary>
+    public static string ExpectedExecutableFullPathName
+    {
+      get
+      {
+        if (doesProcessAndSystemBitMatch)
+        {
+          return $"{parentPathNameForBitMatchedProcessAndSystem}{PreferredExecutableName}";
+        }
+
+        return $"{parentPathNameForBitUnmatchedProcessAndSystem}{PreferredExecutableName}";
+      }
+    }
+
+    /// <summary>
+    /// Does audio repeater executable exist.
+    /// </summary>
+    public static bool DoesExecutableExist
+    {
+      get
+      {
+        return File.Exists(ExpectedExecutableFullPathName);
+      }
+    }
+
+    #endregion
+  }
+}
---
+++ VACARM.Core/VACARM.Common/CommonRestrictions.cs
@@ -0,0 +1,64 @@
+﻿namespace VACARM.Common
+{
+  public partial class Common
+  {
+    #region Parameters
+
+    public static bool DoIgnoreSafeMaxRepeaterCount = false;
+    public static bool DoIgnoreMaxLegacyEndpointCount = false;
+
+    /// <summary>
+    /// Limit the maximum amount of endpoints (audio devices).
+    /// Note: Windows NT 6.x and newer does not have a maximum amount of endpoints.
+    /// </summary>
+    public static uint MaxEndpointCount
+    {
+      get
+      {
+        if
+        (
+          Environment.OSVersion.Version.Major < 6
+          ! && DoIgnoreMaxLegacyEndpointCount
+        )
+        {
+          return WindowsNT5MaxEndpointCount;
+        }
+
+        return AudioRepeaterManagerMaxVirtualEndpointCount;
+      }
+    }
+
+    /// <summary>
+    /// Limit the maximum amount of repeaters (pairs of audio devices).
+    /// </summary>
+    public static uint MaxRepeaterCount
+    {
+      get
+      {
+        if (DoIgnoreSafeMaxRepeaterCount)
+        {
+          return uint.MaxValue;
+        }
+
+        return SafeMaxRepeaterCount;
+      }
+    }
+
+    /// <summary>
+    /// Arbitrary maximum amount of repeaters.
+    /// </summary>
+    public readonly static uint SafeMaxRepeaterCount = 1024;
+
+    /// <summary>
+    /// Maximum amount of virtual endpoints (virtual audio cables) for VAC.
+    /// </summary>
+    public readonly static uint AudioRepeaterManagerMaxVirtualEndpointCount = 256;
+
+    /// <summary>
+    /// Maximum amount of endpoints (audio devices) for Windows 5.x and older.
+    /// </summary>
+    public readonly static uint WindowsNT5MaxEndpointCount = 32;
+
+    #endregion
+  }
+}
