71f912a45f4e44a704e635451d0472b5d5fa2ac9
portellam <amportell.13@gmail.com>
2025-01-29 10:51:24 -0500

    Create IMMDeviceRepository.cs

---
+++ VACARM.Backend/VACARM.Infrastructure/Repositories/IMMDeviceRepository.cs
@@ -0,0 +1,20 @@
+﻿using NAudio.CoreAudioApi;
+
+namespace VACARM.Infrastructure.Repositories
+{
+  public interface IMMDeviceRepository
+  {
+    #region Logic
+
+    List<MMDevice> GetAll();
+    List<MMDevice> GetAllStarted();
+    List<MMDevice> GetAllStopped();
+    List<MMDevice> GetRange(List<string> idList);
+    MMDevice? Get(string id);
+    void Update(string id);
+    void UpdateAll();
+    void UpdateRange(List<string> idList);
+    
+    #endregion
+  }
+}
