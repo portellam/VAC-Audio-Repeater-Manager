8ddf8fab2ff8c78870ad7f64361ab69c196afaac
portellam <amportell.13@gmail.com>
2025-01-29 11:36:29 -0500

    Began work on model controllers.

---
+++ VACARM.Backend/VACARM.Application/Controllers/IDeviceController.cs
@@ -0,0 +1,34 @@
+﻿using VACARM.Domain.Models;
+
+namespace VACARM.Application.Controllers
+{
+  public interface IDeviceController
+  {
+    #region Logic
+
+    void SetAsDefault(string actualId);
+    void SetAsDefault(uint id);
+    DeviceModel Get(string actualId);
+    DeviceModel Get(uint id);
+    List<DeviceModel> GetRange(List<string> actualIdList);
+    List<DeviceModel> GetRange(List<uint> idList);
+    void Restart(string id);
+    void Restart(uint id);
+    void RestartRange(List<string> actualIdList);
+    void RestartRange(List<uint> idList);
+    void Start(string id);
+    void Start(uint id);
+    void StartRange(List<string> actualIdList);
+    void StartRange(List<uint> idList);
+    void Stop(string id);
+    void Stop(uint id);
+    void StopRange(List<string> actualIdList);
+    void StopRange(List<uint> idList);
+    void Update(string id);
+    void Update(uint id);
+    void UpdateRange(List<string> actualIdList);
+    void UpdateRange(List<uint> idList);
+
+    #endregion
+  }
+}
---
+++ VACARM.Backend/VACARM.Application/Controllers/IRepeaterController.cs
@@ -0,0 +1,48 @@
+﻿using VACARM.Domain.Models;
+
+namespace VACARM.Application.Controllers
+{
+  public interface IRepeaterController
+  {
+    #region Logic
+
+    IDeviceController Get();
+    List<RepeaterModel> Get(uint id);
+    List<RepeaterModel> GetAll();
+    List<RepeaterModel> GetByDeviceId(uint deviceId);
+    List<RepeaterModel> GetRange(List<uint> idList);
+    List<RepeaterModel> GetRangeByDeviceId(List<uint> deviceIdList);
+    void Restart(uint id);
+    void RestartAll();
+    void RestartByDeviceId(uint deviceId);
+    void RestartByDeviceName(string deviceName);
+    void RestartRange(List<uint> idList);
+    void RestartRangeByDeviceId(List<uint> deviceIdList);
+    void RestartRangeByDeviceName(List<string> deviceNameList);
+    void Start(uint id);
+    void StartAll();
+    void StartByDeviceId(uint deviceId);
+    void StartByDeviceName(string deviceName);
+    void StartRange(List<uint> idList);
+    void StartRangeByDeviceId(List<uint> deviceIdList);
+    void StartRangeByDeviceName(List<string> deviceNameList);
+    void Stop(string id);
+    void Stop(uint id);
+    void StopAll();
+    void StopByDeviceId(uint deviceId);
+    void StopByDeviceName(string deviceName);
+    void StopRange(List<uint> idList);
+    void StopRangeByDeviceId(List<uint> deviceIdList);
+    void StopRangeByDeviceName(List<string> deviceNameList);
+    void Update(string id);
+    void Update(uint id);
+    void UpdateAll();
+    void UpdateByDeviceId(uint deviceId);
+    void UpdateByDeviceName(string deviceName);
+    void UpdateRange(List<uint> idList);
+    void UpdateRangeByDeviceId(List<uint> deviceIdList);
+    void UpdateRangeByDeviceName(List<string> deviceNameList);
+
+    #endregion
+  }
+}
