d18dcedd55c71362c13a0e6f7df77cfba2505104
portellam <amportell.13@gmail.com>
2025-01-30 19:02:50 -0500

    Added generic Controller.

---
+++ VACARM.Backend/VACARM.Application/Controllers/Controller.cs
@@ -0,0 +1,194 @@
+﻿using System.ComponentModel;
+using System.Diagnostics;
+using VACARM.Infrastructure.Repositories;
+
+namespace VACARM.Application.Controllers
+{
+  public class Controller<T> : IController<T>
+  {
+    #region Parameters
+
+    private IRepository<T> repository { get; set; } = new Repository<T>();
+
+    public IRepository<T> Repository
+    {
+      get
+      {
+        return repository;
+      }
+      set
+      {
+        repository = value;
+        OnPropertyChanged(nameof(repository));
+      }
+    }
+
+    public virtual event PropertyChangedEventHandler PropertyChanged;
+
+    #endregion
+
+    #region Logic
+
+    /// <summary>
+    /// Logs event when property has changed.
+    /// </summary>
+    /// <param name="propertyName">The property name</param>
+    private void OnPropertyChanged(string propertyName)
+    {
+      PropertyChanged?.Invoke
+      (
+        this,
+        new PropertyChangedEventArgs(propertyName)
+      );
+
+      Debug.WriteLine
+      (
+        string.Format
+        (
+          "PropertyChanged: {0}",
+          propertyName
+        )
+      );
+    }
+
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    public Controller()
+    {
+      Repository = new Repository<T>();
+    }
+
+    public T? Get(Func<T, bool> func)
+    {
+      return Repository.Get(func);
+    }
+
+    public IEnumerable<T> GetAll()
+    {
+      return Repository.GetAll();
+    }
+
+    public IEnumerable<T> GetRange(Func<T, bool> func)
+    {
+      return Repository.GetRange(func);
+    }
+
+    public IQueryable<T> Queryable()
+    {
+      return Repository.Queryable();
+    }
+
+    public void Do
+    (
+      Action<T> action,
+      Func<T, bool> func
+    )
+    {
+      if (action == null)
+      {
+        return;
+      }
+
+      if (func == null)
+      {
+        return;
+      }
+
+      var t = Repository.Get(func);
+
+      Do
+      (
+        action,
+        t
+      );
+    }
+
+    public void Do
+    (
+      Action<T> action,
+      T? t
+    )
+    {
+      if (action == null)
+      {
+        return;
+      }
+
+      if (t == null)
+      {
+        return;
+      }
+
+      action(t);
+    }
+
+    public void DoAll(Action<T> action)
+    {
+      if (action == null)
+      {
+        return;
+      }
+
+      foreach (var t in Repository.GetAll())
+      {
+        Do
+        (
+          action,
+          t
+        );
+      }
+    }
+
+    public void DoRange
+    (
+      Action<T> action,
+      IEnumerable<T> enumerable
+    )
+    {
+      if (enumerable == null)
+      {
+        return;
+      }
+
+      if (enumerable.Count() == 0)
+      {
+        return;
+      }
+
+      foreach (var t in enumerable)
+      {
+        Do
+        (
+          action,
+          t
+        );
+      }
+    }
+
+    public void DoRange
+    (
+      Action<T> action,
+      Func<T, bool> func
+    )
+    {
+      if (action is null)
+      {
+        return;
+      }
+
+      if (func is null)
+      {
+        return;
+      }
+
+      DoRange
+      (
+        action,
+        Repository.GetRange(func)
+      );
+    }
+
+    #endregion
+  }
+}
---
+++ VACARM.Backend/VACARM.Application/Controllers/IController.cs
@@ -0,0 +1,92 @@
+﻿using System.ComponentModel;
+using VACARM.Infrastructure.Repositories;
+
+namespace VACARM.Application.Controllers
+{
+  public interface IController<T>
+  {
+    #region Parameters
+
+    /// <summary>
+    /// The <typeparamref name="T"/> repository.
+    /// </summary>
+    IRepository<T> Repository { get; set; }
+    event PropertyChangedEventHandler PropertyChanged;
+
+    #endregion
+
+    #region Logic
+
+    /// <summary>
+    /// Get a <typeparamref name="T"/> item.
+    /// </summary>
+    /// <param name="func">The function</param>
+    /// <returns>The item.</returns>
+    T? Get(Func<T, bool> func);
+
+    /// <summary>
+    /// Get an enumerable of all <typeparamref name="T"/> item(s).
+    /// </summary>
+    /// <returns>The enumerable.</returns>
+    IEnumerable<T> GetAll();
+
+    /// <summary>
+    /// Get an enumerable of some <typeparamref name="T"/> item(s).
+    /// </summary>
+    /// <param name="func">The function</param>
+    /// <returns>The enumerable.</returns>
+    IEnumerable<T> GetRange(Func<T, bool> func);
+
+    /// <summary>
+    /// Do an action for a given <typeparamref name="T"/> item.
+    /// </summary>
+    /// <param name="action">The action</param>
+    /// <param name="func">The function</param>
+    void Do
+    (
+      Action<T> action,
+      Func<T, bool> func
+    );
+
+    /// <summary>
+    /// Do an action for a given <typeparamref name="T"/> item.
+    /// </summary>
+    /// <param name="action">The action</param>
+    /// <param name="t">The item</param>
+    void Do
+    (
+      Action<T> action,
+      T t
+    );
+
+    /// <summary>
+    /// Do an action for all <typeparamref name="T"/> item(s).
+    /// </summary>
+    /// <param name="action">The action</param>
+    void DoAll(Action<T> action);
+
+    /// <summary>
+    /// Do an action for an enumerable of <typeparamref name="T"/> item(s).
+    /// </summary>
+    /// <param name="action">The action</param>
+    /// <param name="enumerable">The enumerable</param>
+    void DoRange
+    (
+      Action<T> action,
+      IEnumerable<T> enumerable
+    );
+
+    /// <summary>
+    /// Do an action for some <typeparamref name="T"/> item(s).
+    /// </summary>
+    /// <param name="action">The action</param>
+    /// <param name="func">The function</param>
+    void DoRange
+    (
+      Action<T> action,
+      Func<T, bool> func
+    );
+
+    #endregion
+  }
+}
