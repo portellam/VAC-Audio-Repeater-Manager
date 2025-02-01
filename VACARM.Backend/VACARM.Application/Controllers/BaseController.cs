c990eed4b9e4b0d669d1170efe6832da6f882de1
portellam <amportell.13@gmail.com>
2025-02-01 14:07:12 -0500

    Update BaseController.cs

--- VACARM.Backend/VACARM.Application/Controllers/BaseController.cs
+++ VACARM.Backend/VACARM.Application/Controllers/BaseController.cs
@@ -1,22 +1,74 @@
-﻿using VACARM.Domain.Models;
+﻿using System.ComponentModel;
+using System.Diagnostics;
+using VACARM.Domain.Models;
+using VACARM.Infrastructure.Repositories;
 
 namespace VACARM.Application.Controllers
 {
-  public class BaseController :
-    GenericController<BaseModel>,
-    IBaseController
+  public class BaseController<T> :
+    GenericController<T>,
+    IBaseController<BaseModel> where T : BaseModel
   {
+    #region Parameters
+
+    private IBaseRepository<BaseModel> repository { get; set; } =
+      new BaseRepository<BaseModel>();
+
+    private IBaseRepository<BaseModel> Repository
+    {
+      get
+      {
+        return repository;
+      }
+      set
+      {
+        repository = value;
+        OnPropertyChanged(nameof(repository));
+      }
+    }
+
+    public override event PropertyChangedEventHandler PropertyChanged;
+
     #region Logic
+
+    /// <summary>
+    /// Logs event when property has changed.
+    /// </summary>
+    /// <param name="propertyName">The property name</param>
+    private void OnPropertyChanged(string propertyName)
+    {
+      PropertyChanged?.Invoke
+      (
+        this,
+        new PropertyChangedEventArgs(propertyName)
+      );
+
+      Debug.WriteLine
+      (
+        string.Format
+        (
+          "PropertyChanged: {0}",
+          propertyName
+        )
+      );
+    }
+
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    public BaseController()
+    {
+      Repository = new BaseRepository<BaseModel>();
+    }
 
     public BaseModel? Get(uint id)
     {
-      Func<BaseModel, bool> func = (BaseModel x) => x.Id == id;
-      return Get(func);
+      return Repository.Get(id);
     }
 
     public IEnumerable<BaseModel> GetRange(List<uint> idList)
     {
-
+      return Repository.GetRange(idList);
     }
 
     public IEnumerable<BaseModel> GetRange
@@ -25,7 +77,11 @@ namespace VACARM.Application.Controllers
       uint endId
     )
     {
-
+      return Repository.GetRange
+        (
+          startId,
+          endId
+        );
     }
 
     #endregion
