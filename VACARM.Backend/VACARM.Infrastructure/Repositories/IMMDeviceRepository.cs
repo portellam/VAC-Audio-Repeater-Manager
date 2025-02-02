53da628d081c151f1432cc489963a88ba7c28d2e
portellam <amportell.13@gmail.com>
2025-02-02 03:13:03 -0500

    Added logic to find default device (without async). Updated logic and comments.

--- VACARM.Backend/VACARM.Infrastructure/Repositories/IMMDeviceRepository.cs
+++ VACARM.Backend/VACARM.Infrastructure/Repositories/IMMDeviceRepository.cs
@@ -2,85 +2,122 @@
 
 namespace VACARM.Infrastructure.Repositories
 {
-  public interface IMMDeviceRepository
+  public interface IMMDeviceRepository : 
+    IGenericRepository<MMDevice>
   {
     #region Logic
 
     /// <summary>
-    /// Is the audio device present.
-    /// </summary>
-    /// <param name="mMDevice">The audio device</param>
-    /// <returns>True/false is the audio device present.</returns>
-    bool IsPresent(MMDevice? mMDevice);
-
-    /// <summary>
-    /// Is the audio device started.
-    /// </summary>
-    /// <param name="mMDevice">The audio device</param>
-    /// <returns>True/false is the audio device started.</returns>
-    bool IsStarted(MMDevice? mMDevice);
-
-    /// <summary>
-    /// Get an audio device.
+    /// Is a capture <typeparamref name="MMDevice"/> item.
     /// </summary>
     /// <param name="id">The ID</param>
-    /// <returns>The audio device.</returns>
-    MMDevice? Get(string id);
+    /// <returns>The true/false result.</returns>
+    bool IsCapture(string id);
+
+    /// <summary>
+    /// Is a default <typeparamref name="MMDevice"/> item.
+    /// </summary>
+    /// <param name="id">The ID</param>
+    /// <returns>The true/false result.</returns>
+    bool IsDefault(string id);
+
+    /// <summary>
+    /// Is a duplex <typeparamref name="MMDevice"/> item.
+    /// </summary>
+    /// <param name="id">The ID</param>
+    /// <returns>The true/false result.</returns>
+    bool IsDuplex(string id);
+
+    /// <summary>
+    /// Is an enabled <typeparamref name="MMDevice"/> item.
+    /// </summary>
+    /// <param name="id">The ID</param>
+    /// <returns>True/false is an enabled item.</returns>
+    bool IsEnabled(string id);
+
+    /// <summary>
+    /// Is a <typeparamref name="MMDevice"/> item present.
+    /// </summary>
+    /// <param name="id">The ID</param>
+    /// <returns>The true/false result.</returns>
+    bool IsPresent(string id);
+
+    /// <summary>
+    /// Is a render <typeparamref name="MMDevice"/> item.
+    /// </summary>
+    /// <param name="id">The ID</param>
+    /// <returns>The true/false result.</returns>
+    bool IsRender(string id);
 
     /// <summary>
     /// Get a <typeparamref name="MMDevice"/> item.
     /// </summary>
-    /// <param name="func">The function</param>
-    /// <returns>The <typeparamref name="MMDevice"/> item.</returns>
-    MMDevice? Get(Func<MMDevice, bool> func);
+    /// <param name="id">The ID</param>
+    /// <returns>The item.</returns>
+    MMDevice? Get(string id);
+
+    /// <summary>
+    /// Get the default <typeparamref name="MMDevice"/> item.
+    /// </summary>
+    /// <param name="id">The ID</param>
+    /// <returns>The item.</returns>
+    MMDevice? GetDefault();
 
     /// <summary>
     /// Get an enumerable of all <typeparamref name="MMDevice"/> item(s).
     /// </summary>
-    /// <returns>The <typeparamref name="MMDevice"/> enumerable of 
-    /// item(s).</returns>
+    /// <returns>The enumerable of item(s).</returns>
     IEnumerable<MMDevice> GetAll();
 
     /// <summary>
-    /// Get a list of all absent audio device(s).
+    /// Get an enumerable of all absent <typeparamref name="MMDevice"/> item(s).
     /// </summary>
-    /// <returns>The list of all absent audio device(s).</returns>
-    List<MMDevice> GetAllAbsent();
+    /// <returns>The enumerable of item(s).</returns>
+    IEnumerable<MMDevice> GetAllAbsent();
 
     /// <summary>
-    /// Get a list of all present audio device(s).
+    /// Get an enumerable of all capture <typeparamref name="MMDevice"/> item(s).
     /// </summary>
-    /// <returns>The list of all present audio device(s).</returns>
-    List<MMDevice> GetAllPresent();
+    /// <returns>The enumerable of item(s).</returns>
+    IEnumerable<MMDevice> GetAllCapture();
 
     /// <summary>
-    /// Get a list of all started audio device(s).
+    /// Get an enumerable of all disabled <typeparamref name="MMDevice"/> item(s).
     /// </summary>
-    /// <returns>The list of all started audio device(s).</returns>
-    List<MMDevice> GetAllStarted();
+    /// <returns>The enumerable of item(s).</returns>
+    IEnumerable<MMDevice> GetAllDisabled();
 
     /// <summary>
-    /// Get a list of all stopped audio device(s).
+    /// Get an enumerable of all duplex <typeparamref name="MMDevice"/> item(s).
     /// </summary>
-    /// <returns>The list of all stopped audio device(s).</returns>
-    List<MMDevice> GetAllStopped();
+    /// <returns>The enumerable of item(s).</returns>
+    IEnumerable<MMDevice> GetAllDuplex();
+
+    /// <summary>
+    /// Get an enumerable of all enabled <typeparamref name="MMDevice"/> item(s).
+    /// </summary>
+    /// <returns>The enumerable of item(s).</returns>
+    IEnumerable<MMDevice> GetAllEnabled();
+
+    /// <summary>
+    /// Get an enumerable of all present <typeparamref name="MMDevice"/> item(s).
+    /// </summary>
+    /// <returns>The enumerable of item(s).</returns>
+    IEnumerable<MMDevice> GetAllPresent();
+
+    /// <summary>
+    /// Get an enumerable of all render <typeparamref name="MMDevice"/> item(s).
+    /// </summary>
+    /// <returns>The enumerable of item(s).</returns>
+    IEnumerable<MMDevice> GetAllRender();
 
     /// <summary>
     /// Get an enumerable of some <typeparamref name="MMDevice"/> item(s).
     /// </summary>
-    /// <param name="func">The function</param>
-    /// <returns>The <typeparamref name="MMDevice"/> enumerable of 
-    /// item(s).</returns>
-    IEnumerable<MMDevice> GetRange(Func<MMDevice, bool> func);
+    /// <param name="idList">The enumerable of ID(s)</param>
+    /// <returns>The enumerable of item(s).</returns>
+    IEnumerable<MMDevice> GetRange(IEnumerable<string> idList);
 
-    /// <summary>
-    /// Get an enumerable of some <typeparamref name="MMDevice"/> item(s).
-    /// </summary>
-    /// <param name="idList">The list of ID(s)</param>
-    /// <returns>The <typeparamref name="MMDevice"/> enumerable of 
-    /// item(s).</returns>
-    List<MMDevice> GetRange(List<string> idList);
-    
     #endregion
   }
 }
