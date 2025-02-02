c0eca95d82c787c2b3546e513dddd153ba24771b
portellam <amportell.13@gmail.com>
2025-02-02 03:13:41 -0500

    Fleshed out model specific logic.

--- VACARM.Backend/VACARM.Infrastructure/Repositories/IDeviceRepository.cs
+++ VACARM.Backend/VACARM.Infrastructure/Repositories/IDeviceRepository.cs
@@ -2,15 +2,79 @@
 
 namespace VACARM.Infrastructure.Repositories
 {
-  public interface IDeviceRepository : IBaseRepository<DeviceModel>
+  public interface IDeviceRepository : 
+    IBaseRepository<DeviceModel>
   {
     #region Logic
 
-    List<DeviceModel> GetAllAbsent();
-    List<DeviceModel> GetAllInAlphabeticalOrder();
-    List<DeviceModel> GetAllInput();
-    List<DeviceModel> GetAllOutput();
-    List<DeviceModel> GetAllPresent();
+    /// <summary>
+    /// Get an enumerable of all absent <typeparamref name="DeviceModel"/> item(s).
+    /// </summary>
+    /// <returns>The enumerable of item(s).</returns>
+    IEnumerable<DeviceModel> GetAllAbsent();
+
+    /// <summary>
+    /// Get an enumerable of all alphebetical order 
+    /// <typeparamref name="DeviceModel"/> item(s).
+    /// </summary>
+    /// <returns>The enumerable of item(s).</returns>
+    IEnumerable<DeviceModel> GetAllAlphabeticalOrder();
+
+    /// <summary>
+    /// Get an enumerable of all capture <typeparamref name="DeviceModel"/> item(s).
+    /// </summary>
+    /// <returns>The enumerable of item(s).</returns>
+    IEnumerable<DeviceModel> GetAllCapture();
+
+    /// <summary>
+    /// Get an enumerable of all disabled <typeparamref name="DeviceModel"/> item(s).
+    /// </summary>
+    /// <returns>The enumerable of item(s).</returns>
+    IEnumerable<DeviceModel> GetAllDisabled();
+
+    /// <summary>
+    /// Get an enumerable of all duplex <typeparamref name="DeviceModel"/> item(s).
+    /// </summary>
+    /// <returns>The enumerable of item(s).</returns>
+    IEnumerable<DeviceModel> GetAllDuplex();
+
+    /// <summary>
+    /// Get an enumerable of all enabled <typeparamref name="DeviceModel"/> item(s).
+    /// </summary>
+    /// <returns>The enumerable of item(s).</returns>
+    IEnumerable<DeviceModel> GetAllEnabled();
+
+    /// <summary>
+    /// Get an enumerable of all muted <typeparamref name="DeviceModel"/> item(s).
+    /// </summary>
+    /// <returns>The enumerable of item(s).</returns>
+    IEnumerable<DeviceModel> GetAllMuted();
+
+    /// <summary>
+    /// Get an enumerable of all not muted
+    /// <typeparamref name="DeviceModel"/> item(s).
+    /// </summary>
+    /// <returns>The enumerable of item(s).</returns>
+    IEnumerable<DeviceModel> GetAllNotMuted();
+
+    /// <summary>
+    /// Get an enumerable of all present <typeparamref name="DeviceModel"/> item(s).
+    /// </summary>
+    /// <returns>The enumerable of item(s).</returns>
+    IEnumerable<DeviceModel> GetAllPresent();
+
+    /// <summary>
+    /// Get an enumerable of all render <typeparamref name="DeviceModel"/> item(s).
+    /// </summary>
+    /// <returns>The enumerable of item(s).</returns>
+    IEnumerable<DeviceModel> GetAllRender();
+
+    /// <summary>
+    /// Get an enumerable of all reverse alphebetical order 
+    /// <typeparamref name="DeviceModel"/> item(s).
+    /// </summary>
+    /// <returns>The enumerable of item(s).</returns>
+    IEnumerable<DeviceModel> GetAllInReverseAlphabeticalOrder();
 
     #endregion
   }
--- VACARM.Backend/VACARM.Infrastructure/Repositories/IRepeaterRepository.cs
+++ VACARM.Backend/VACARM.Infrastructure/Repositories/IRepeaterRepository.cs
@@ -2,12 +2,24 @@
 
 namespace VACARM.Infrastructure.Repositories
 {
-  public interface IRepeaterRepository : IBaseRepository<RepeaterModel>
+  public interface IRepeaterRepository : 
+    IBaseRepository<RepeaterModel>
   {
     #region Logic
 
-    List<RepeaterModel> GetAllStarted();
-    List<RepeaterModel> GetAllStopped();
+    /// <summary>
+    /// Get an enumerable of all started <typeparamref name="RepeaterModel"/> 
+    /// item(s).
+    /// </summary>
+    /// <returns>The enumerable of item(s).</returns>
+    IEnumerable<RepeaterModel> GetAllStarted();
+
+    /// <summary>
+    /// Get an enumerable of all started <typeparamref name="RepeaterModel"/> 
+    /// item(s).
+    /// </summary>
+    /// <returns>The enumerable of item(s).</returns>
+    IEnumerable<RepeaterModel> GetAllStopped();
 
     #endregion
   }
