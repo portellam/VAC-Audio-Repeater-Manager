29483e5bed72ba1985da754cedc08c17f5eae8cb
portellam <amportell.13@gmail.com>
2025-02-04 15:01:21 -0500

    Updated summary. Defined logic. TODO: implement in model.

--- VACARM.Backend/VACARM.Infrastructure/Repositories/RepeaterRepository.cs
+++ VACARM.Backend/VACARM.Infrastructure/Repositories/RepeaterRepository.cs
@@ -1,14 +1,48 @@
-﻿using VACARM.Domain.Models;
+﻿using System.Diagnostics.CodeAnalysis;
+using VACARM.Domain.Models;
 
 namespace VACARM.Infrastructure.Repositories
 {
   /// <summary>
-  /// Constructor
+  /// A snapshot of audio device repeaters.
   /// </summary>
-  /// <param name="list">the device list</param>
   public class RepeaterRepository :
     BaseRepository<RepeaterModel>,
     IRepeaterRepository
   {
+    #region Logic
+
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    [ExcludeFromCodeCoverage]
+    public RepeaterRepository()
+    {
+      List = new List<RepeaterModel>();
+    }
+
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    /// <param name="enumerable">The enumerable of item(s)</param>
+    [ExcludeFromCodeCoverage]
+    public RepeaterRepository(IEnumerable<RepeaterModel> enumerable)
+    {
+      List = enumerable.ToList();
+    }
+
+    public IEnumerable<RepeaterModel> GetAllStarted()
+    {
+      return List
+        .Select(x => x.IsStarted);
+    }
+
+    public IEnumerable<RepeaterModel> GetAllStopped()
+    {
+      return List
+        .Select(x => !x.IsStarted);
+    }
+
+    #endregion
   }
 }
