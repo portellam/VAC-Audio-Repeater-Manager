5a1857bc1c8226ec984193b89c2c7e7799ee17e9
portellam <amportell.13@gmail.com>
2025-02-05 23:56:45 -0500

    Moved logic into Generic Controller. Made async partial classes.

---
+++ VACARM.Backend/VACARM.Application/Controllers/GenericController.cs
@@ -0,0 +1,228 @@
+﻿using System.ComponentModel;
+using System.Diagnostics;
+using VACARM.Infrastructure.Extensions;
+using VACARM.Infrastructure.Repositories;
+
+namespace VACARM.Application.Controllers
+{
+  public partial class GenericController<T1, T2> :
+    IGenericController<T1, T2> where T1 :
+    GenericRepository<T2> where T2 :
+    class
+  {
+    #region Parameters
+
+    private IGenericRepository<T2> repository { get; set; } =
+      new GenericRepository<T2>();
+
+    internal IGenericRepository<T2> Repository
+    {
+      get
+      {
+        return repository;
+      }
+      set
+      {
+        repository = value;
+        OnPropertyChanged(nameof(Repository));
+      }
+    }
+
+    public virtual event PropertyChangedEventHandler PropertyChanged;
+
+    #endregion
+
+    #region Logic
+
+    /// <summary>
+    /// Logs event when property has changed.
+    /// </summary>
+    /// <param name="propertyName">The property name</param>
+    private void OnPropertyChanged(string propertyName)
+    {
+      PropertyChanged?.Invoke
+      (
+        this,
+        new PropertyChangedEventArgs(propertyName)
+      );
+
+      Debug.WriteLine
+      (
+        string.Format
+        (
+          "PropertyChanged: {0}",
+          propertyName
+        )
+      );
+    }
+
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    public GenericController()
+    {
+      Repository = new GenericRepository<T2>();
+    }
+
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    /// <param name="repository">The repository</param>
+    public GenericController(IGenericRepository<T2> repository)
+    {
+      Repository = repository;
+    }
+
+    public T2? Get(Func<T2, bool> func)
+    {
+      return Repository.Get(func);
+    }
+
+    public IEnumerable<T2> GetAll()
+    {
+      return Repository.GetAll();
+    }
+
+    public IEnumerable<T2> GetRange(Func<T2, bool> func)
+    {
+      return Repository.GetRange(func);
+    }
+
+    public void Add(T2 item)
+    {
+      Repository.Add(item);
+    }
+
+    public void AddRange(IEnumerable<T2> enumerable)
+    {
+      Repository.AddRange(enumerable);
+    }
+
+    public void DoWork
+    (
+      Action<T2> action,
+      Func<T2, bool> func
+    )
+    {
+      if (action == null)
+      {
+        return;
+      }
+
+      if (func == null)
+      {
+        return;
+      }
+
+      var item = Repository.Get(func);
+
+      DoWork
+      (
+        action,
+        item
+      );
+    }
+
+    public void DoWork
+    (
+      Action<T2> action,
+      T2 item
+    )
+    {
+      if (action == null)
+      {
+        return;
+      }
+
+      if (item == null)
+      {
+        return;
+      }
+
+      action(item);
+    }
+
+    public void DoWorkAll(Action<T2> action)
+    {
+      if (action == null)
+      {
+        return;
+      }
+
+      foreach (var item in GetAll())
+      {
+        DoWork
+        (
+          action,
+          item
+        );
+      }
+    }
+
+    public void DoWorkRange
+    (
+      Action<T2> action,
+      IEnumerable<T2> enumerable
+    )
+    {
+      if (IEnumerableExtension<T2>.IsNullOrEmpty(enumerable))
+      {
+        return;
+      }
+
+      foreach (var item in enumerable)
+      {
+        DoWork
+        (
+          action,
+          item
+        );
+      }
+    }
+
+    public void DoWorkRange
+    (
+      Action<T2> action,
+      Func<T2, bool> func
+    )
+    {
+      if (action == null)
+      {
+        return;
+      }
+
+      if (func == null)
+      {
+        return;
+      }
+
+      DoWorkRange
+      (
+        action,
+        Repository.GetRange(func)
+      );
+    }
+
+    public void Remove(T2 item)
+    {
+      Repository.Remove(item);
+    }
+
+    public void RemoveAll()
+    {
+      Repository.RemoveAll();
+    }
+
+    public void RemoveRange(Func<T2, bool> func)
+    {
+      RemoveRange(func);
+    }
+
+    public void RemoveRange(IEnumerable<T2> enumerable)
+    {
+      Repository.RemoveRange(enumerable);
+    }
+
+    #endregion
+  }
+}
---
+++ VACARM.Backend/VACARM.Application/Controllers/IGenericController.cs
@@ -0,0 +1,125 @@
+﻿using System.ComponentModel;
+using VACARM.Infrastructure.Repositories;
+
+namespace VACARM.Application.Controllers
+{
+  public partial interface IGenericController<T1, T2> where T1 :
+    IGenericRepository<T2> where T2 :
+    class
+  {
+    #region Parameters
+
+    event PropertyChangedEventHandler PropertyChanged;
+
+    #endregion
+
+    #region Logic
+
+    /// <summary>
+    /// Get a <typeparamref name="T2"/> item.
+    /// </summary>
+    /// <param name="func">The function</param>
+    /// <returns>The item.</returns>
+    T2? Get(Func<T2, bool> func);
+
+    /// <summary>
+    /// Get an enumerable of all <typeparamref name="T2"/> item(s).
+    /// </summary>
+    /// <returns>The enumerable of item(s).</returns>
+    IEnumerable<T2> GetAll();
+
+    /// <summary>
+    /// Get an enumerable of some <typeparamref name="T2"/> item(s).
+    /// </summary>
+    /// <param name="func">The function</param>
+    /// <returns>The enumerable of item(s).</returns>
+    IEnumerable<T2> GetRange(Func<T2, bool> func);
+
+    /// <summary>
+    /// Add a <typeparamref name="T2"/> item.
+    /// </summary>
+    /// <param name="item">The item</param>
+    void Add(T2 item);
+
+    /// <summary>
+    /// Add an enumerable of some <typeparamref name="T2"/> item(s).
+    /// </summary>
+    /// <param name="item">The enumerable of item(s)</param>
+    void AddRange(IEnumerable<T2> enumerable);
+
+    /// <summary>
+    /// Do an action for a <typeparamref name="T2"/> item.
+    /// </summary>
+    /// <param name="action">The action</param>
+    /// <param name="func">The function</param>
+    void DoWork
+    (
+      Action<T2> action,
+      Func<T2, bool> func
+    );
+
+    /// <summary>
+    /// Do an action for a <typeparamref name="T2"/> item.
+    /// </summary>
+    /// <param name="action">The action</param>
+    /// <param name="item">The item</param>
+    void DoWork
+    (
+      Action<T2> action,
+      T2 item
+    );
+
+    /// <summary>
+    /// Do an action for an enumerable of all <typeparamref name="T2"/> item(s).
+    /// </summary>
+    /// <param name="action">The action</param>
+    void DoWorkAll(Action<T2> action);
+
+    /// <summary>
+    /// Do an action for an enumerable of some <typeparamref name="T2"/> item(s).
+    /// </summary>
+    /// <param name="action">The action</param>
+    /// <param name="enumerable">The enumerable of item(s)</param>
+    void DoWorkRange
+    (
+      Action<T2> action,
+      IEnumerable<T2> enumerable
+    );
+
+    /// <summary>
+    /// Do an action for an enumerable of some <typeparamref name="T2"/> item(s).
+    /// </summary>
+    /// <param name="action">The action</param>
+    /// <param name="func">The function</param>
+    void DoWorkRange
+    (
+      Action<T2> action,
+      Func<T2, bool> func
+    );
+
+    /// <summary>
+    /// Remove a <typeparamref name="T2"/> item.
+    /// </summary>
+    /// <param name="item">The item</param>
+    void Remove(T2 item);
+
+    /// <summary>
+    /// Remove an enumerable of all <typeparamref name="T2"/> item(s).
+    /// </summary>
+    void RemoveAll();
+
+    /// <summary>
+    /// Remove an enumerable of some <typeparamref name="T2"/> item(s).
+    /// </summary>
+    /// <param name="func">The function</param>
+    void RemoveRange(Func<T2, bool> func);
+
+    /// <summary>
+    /// Remove an enumerable of some <typeparamref name="T2"/> item(s).
+    /// </summary>
+    /// <param name="enumerable">The enumerable of item(s)</param>
+    void RemoveRange(IEnumerable<T2> enumerable);
+
+    #endregion
+  }
+}
