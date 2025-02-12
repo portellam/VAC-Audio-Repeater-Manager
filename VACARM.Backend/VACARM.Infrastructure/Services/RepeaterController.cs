da289510c164d7f0a939ee1ee5110c9c351d6295
portellam <amportell.13@gmail.com>
2025-02-12 14:10:31 -0500

    Worked on repeater controller.

--- VACARM.Backend/VACARM.Infrastructure/Services/RepeaterController.cs
+++ VACARM.Backend/VACARM.Infrastructure/Services/RepeaterController.cs
@@ -6,10 +6,10 @@ namespace VACARM.Application.Services
   /// <summary>
   /// The service of the <typeparamref name="RepeaterRepository"/>.
   /// </summary>
-  public class RepeaterService<TRepository, TItem> :
-    BaseService<RepeaterRepository<RepeaterModel>, RepeaterModel>,
-    IRepeaterService<RepeaterRepository<RepeaterModel>, RepeaterModel> where TRepository :
-    RepeaterRepository<RepeaterModel> where TItem :
+  public class RepeaterService<TRepository, TRepeaterModel> :
+    BaseService<RepeaterRepository<TRepeaterModel>, TRepeaterModel>,
+    IRepeaterService<RepeaterRepository<TRepeaterModel>, TRepeaterModel> where TRepository :
+    RepeaterRepository<TRepeaterModel> where TRepeaterModel :
     RepeaterModel
   {
     #region Parameters
@@ -20,44 +20,7 @@ namespace VACARM.Application.Services
 
     public RepeaterService()
     {
-      base.Repository = new RepeaterRepository<RepeaterModel>();
-
-      RepeaterModel? model = (RepeaterModel)base.Get((uint)0);
-    }
-
-    public IEnumerable<RepeaterModel> GetAllAlphabeticalOrder()
-    {
-      return base.GetAll()
-        .OrderBy(x => x.WindowName);
-    }
-
-    public IEnumerable<RepeaterModel> GetAllByDeviceId(uint deviceId)
-    {
-      return base.GetAll()
-        .Where
-        (
-          x =>
-          x.InputDeviceId == deviceId
-          || x.OutputDeviceId == deviceId
-        );
-    }
-
-    public IEnumerable<RepeaterModel> GetAllReverseAlphabeticalOrder()
-    {
-      return base.GetAll()
-        .OrderByDescending(x => x.WindowName);
-    }
-
-    public IEnumerable<RepeaterModel> GetAllStarted()
-    {
-      return base.GetAll()
-        .Where(x => x.IsStarted);
-    }
-
-    public IEnumerable<RepeaterModel> GetAllStopped()
-    {
-      return base.GetAll()
-        .Where(x => !x.IsStarted);
+      base._Repository = new RepeaterRepository<TRepeaterModel>();
     }
 
     #endregion
