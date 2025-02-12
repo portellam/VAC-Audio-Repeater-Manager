7d12e98d96f5cbfc643b22d557c4c4766a17d049
portellam <amportell.13@gmail.com>
2025-02-12 07:59:43 -0500

    Worked on Base Service.

---
+++ VACARM.Backend/VACARM.Infrastructure/Services/BaseService.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel;
+using System.Diagnostics;
+using VACARM.Domain.Models;
+using VACARM.Infrastructure.Repositories;
+
+namespace VACARM.Application.Services
+{
+  public class BaseService<TRepository, TBaseModel> :
+    GenericListService<TRepository, TBaseModel>,
+    IBaseService<TRepository, TBaseModel> where TRepository :
+    BaseRepository<TBaseModel> where TBaseModel :
+    BaseModel
+  {
+    #region Logic
+
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    public BaseService()
+    {
+      base._Repository = new BaseRepository<TBaseModel>();
+    }
+
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    public BaseService(BaseRepository<TBaseModel> repository)
+    {
+      base._Repository = repository;
+    }
+
+    #endregion
+  }
+}
---
+++ VACARM.Backend/VACARM.Infrastructure/Services/IBaseService.cs
@@ -0,0 +1,12 @@
+﻿using VACARM.Domain.Models;
+using VACARM.Infrastructure.Repositories;
+
+namespace VACARM.Application.Services
+{
+  public interface IBaseService<TRepository, TBaseModel> :
+    IGenericListService<TRepository, TBaseModel> where TRepository :
+    IBaseRepository<TBaseModel> where TBaseModel :
+    BaseModel
+  {
+  }
+}
