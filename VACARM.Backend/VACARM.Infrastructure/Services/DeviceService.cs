88e009aaee65660213f8f5fafc9adb5c3d575c96
portellam <amportell.13@gmail.com>
2025-02-12 15:44:35 -0500

    Added constructors, other services, and warnings.

--- VACARM.Backend/VACARM.Infrastructure/Services/DeviceService.cs
+++ VACARM.Backend/VACARM.Infrastructure/Services/DeviceService.cs
@@ -1,4 +1,7 @@
-﻿using VACARM.Domain.Models;
+﻿using NAudio.CoreAudioApi;
+#warning AudioSwitcher.AudioApi must be used after NAudio.CoreAudioApi
+using AudioSwitcher.AudioApi;
+using VACARM.Domain.Models;
 using VACARM.Infrastructure.Repositories;
 
 namespace VACARM.Application.Services
@@ -14,9 +17,60 @@ namespace VACARM.Application.Services
   {
     #region Parameters
 
+    private MMDeviceService<MMDeviceRepository<MMDevice>, MMDevice>
+      mMDeviceService
+    { get; set; }
+
+    public MMDeviceService<MMDeviceRepository<MMDevice>, MMDevice> MMDeviceService
+    {
+      get
+      {
+        return this.mMDeviceService;
+      }
+      private set
+      {
+        this.mMDeviceService = value;
+        base.OnPropertyChanged(nameof(MMDeviceService));
+      }
+    }
+
+    #warning AudioSwitcher.AudioApi must declare after NAudio.CoreAudioApi
+
+    private CoreAudioService<CoreAudioRepository<Device>, Device>
+      coreAudioService { get; set; }
+    
+    public CoreAudioService<CoreAudioRepository<Device>, Device> CoreAudioService
+    {
+      get
+      {
+        return this.coreAudioService;
+      }
+      private set
+      {
+        this.coreAudioService = value;
+        base.OnPropertyChanged(nameof(CoreAudioService));
+      }
+    }
+
     #endregion
 
     #region Logic
+
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    public DeviceService()
+    {
+      base._Repository = new DeviceRepository<TDeviceModel>();
+
+      this.MMDeviceService =
+        new MMDeviceService<MMDeviceRepository<MMDevice>, MMDevice>();
+
+      #warning AudioSwitcher.AudioApi must initialize after NAudio.CoreAudioApi
+
+      this.CoreAudioService = 
+        new CoreAudioService<CoreAudioRepository<Device>, Device>();
+    }
 
     #endregion
   }
