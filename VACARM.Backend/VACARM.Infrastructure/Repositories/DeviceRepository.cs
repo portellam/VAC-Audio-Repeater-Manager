6154f1e00c8e2b9bdd680b2f59932db030c7c5a2
portellam <amportell.13@gmail.com>
2025-02-12 06:58:40 -0500

    Update DeviceRepository.cs

--- VACARM.Backend/VACARM.Infrastructure/Repositories/DeviceRepository.cs
+++ VACARM.Backend/VACARM.Infrastructure/Repositories/DeviceRepository.cs
@@ -49,77 +49,107 @@ namespace VACARM.Infrastructure.Repositories
 
     public IEnumerable<TDeviceModel> GetAllAbsent()
     {
-      return GetAll().Where(x => !x.IsPresent);
+      return base
+        .GetAll()
+        .Where(x => !x.IsPresent);
     }
 
     public IEnumerable<TDeviceModel> GetAllAlphabetical()
     {
-      return GetAll().OrderBy(x => x.Name);
+      return base
+        .GetAll()
+        .OrderBy(x => x.Name);
     }
 
     public IEnumerable<TDeviceModel> GetAllAlphabeticalDescending()
     {
-      return GetAll().OrderByDescending(x => x.Name);
+      return base
+        .GetAll()
+        .OrderByDescending(x => x.Name);
     }
 
     public IEnumerable<TDeviceModel> GetAllCapture()
     {
-      return GetAll().Where(x => x.IsCapture);
+      return base
+        .GetAll()
+        .Where(x => x.IsCapture);
     }
 
     public IEnumerable<TDeviceModel> GetAllCommunications()
     {
-      return GetAll().Where(x => x.Role == "Communications");
+      return base
+        .GetAll()
+        .Where(x => x.Role == "Communications");
     }
 
     public IEnumerable<TDeviceModel> GetAllConsole()
     {
-      return GetAll().Where(x => x.Role == "Console");
+      return base
+        .GetAll()
+        .Where(x => x.Role == "Console");
     }
 
     public IEnumerable<TDeviceModel> GetAllDefault()
     {
-      return GetAll().Where(x => x.IsDefault);
+      return base
+        .GetAll()
+        .Where(x => x.IsDefault);
     }
 
     public IEnumerable<TDeviceModel> GetAllDisabled()
     {
-      return GetAll().Where(x => !x.IsEnabled);
+      return base
+        .GetAll()
+        .Where(x => !x.IsEnabled);
     }
 
     public IEnumerable<TDeviceModel> GetAllDuplex()
     {
-      return GetAll().Where(x => x.IsDuplex);
+      return base
+        .GetAll()
+        .Where(x => x.IsDuplex);
     }
 
     public IEnumerable<TDeviceModel> GetAllEnabled()
     {
-      return GetAll().Where(x => x.IsEnabled);
+      return base
+        .GetAll()
+        .Where(x => x.IsEnabled);
     }
 
     public IEnumerable<TDeviceModel> GetAllMultimedia()
     {
-      return GetAll().Where(x => x.Role == "Multimedia");
+      return base
+        .GetAll()
+        .Where(x => x.Role == "Multimedia");
     }
 
     public IEnumerable<TDeviceModel> GetAllMuted()
     {
-      return GetAll().Where(x => x.IsMuted);
+      return base
+        .GetAll()
+        .Where(x => x.IsMuted);
     }
 
     public IEnumerable<TDeviceModel> GetAllNotMuted()
     {
-      return GetAll().Where(x => !x.IsMuted);
+      return base
+        .GetAll()
+        .Where(x => !x.IsMuted);
     }
 
     public IEnumerable<TDeviceModel> GetAllRender()
     {
-      return GetAll().Where(x => x.IsRender);
+      return base
+        .GetAll()
+        .Where(x => x.IsRender);
     }
 
     public IEnumerable<TDeviceModel> GetAllPresent()
     {
-      return GetAll().Where(x => x.IsPresent);
+      return base
+        .GetAll()
+        .Where(x => x.IsPresent);
     }
 
     #endregion
