8a59eef7b45f87ce9b4df7b79c5b1e8fdb066f26
portellam <amportell.13@gmail.com>
2025-02-14 00:45:17 -0500

    Defined logic.

--- VACARM.Backend/VACARM.Infrastructure/Services/DeviceServiceAsync.cs
+++ VACARM.Backend/VACARM.Infrastructure/Services/DeviceServiceAsync.cs
@@ -1,5 +1,13 @@
-﻿using VACARM.Domain.Models;
+﻿using AudioSwitcher.AudioApi;
+using NAudio.CoreAudioApi;
+using VACARM.Domain.Models;
 using VACARM.Infrastructure.Repositories;
+
+/*
+ * TODO:
+ * - use DoWork and DoWorkAsync methods (this, range, all) to avoid repetitive code?
+ * 
+ */
 
 namespace VACARM.Application.Services
 {
@@ -11,144 +19,646 @@ namespace VACARM.Application.Services
   {
     #region Logic
 
-    public Task<DeviceModel?> GetDefaultCommunications(bool isInput, bool isOutput)
+    /// <summary>
+    /// Update the service.
+    /// </summary>
+    /// <returns>True/false result.</returns>
+    public async Task<bool> UpdateServiceAsync()
     {
-      throw new NotImplementedException();
+      if (this.MMDeviceService == null)
+      {
+        this.MMDeviceService =
+          new MMDeviceService<MMDeviceRepository<MMDevice>, MMDevice>();
+      }
+
+      else
+      {
+        this
+          .MMDeviceService
+          .UpdateAll();
+      }
+
+      if (this.CoreAudioService == null)
+      {
+        this.CoreAudioService =
+          new CoreAudioService<CoreAudioRepository<Device>, Device>();
+
+        return true;
+      }
+
+      return await this
+        .CoreAudioService
+        .UpdateAllAsync()
+        .ConfigureAwait(false);
     }
 
-    public Task<DeviceModel?> GetDefaultConsole(bool isInput, bool isOutput)
+    /// <summary>
+    /// Get the default communications <typeparamref name="TDeviceModel"/> item.
+    /// </summary>
+    /// <param name="isInput">True/false is an input</param>
+    /// <param name="isOutput">True/false is an output</param>
+    /// <returns>The item.</returns>
+    public async Task<TDeviceModel?> GetDefaultCommunicationsAsync
+    (
+      bool isInput,
+      bool isOutput
+    )
     {
-      throw new NotImplementedException();
+      Device? device = await this
+         .CoreAudioService
+         .GetDefaultCommunicationsAsync
+         (
+           isInput,
+           isOutput
+         ).ConfigureAwait(false);
+
+      if (device == null)
+      {
+        return null;
+      }
+
+      string actualId = device
+        .Id
+        .ToString();
+
+      return this
+        ._Repository
+        .GetByActualId(actualId);
     }
 
-    public Task<DeviceModel?> GetDefaultMultimedia(bool isInput, bool isOutput)
+    /// <summary>
+    /// Get the default console <typeparamref name="TDeviceModel"/> item.
+    /// </summary>
+    /// <param name="isInput">True/false is an input</param>
+    /// <param name="isOutput">True/false is an output</param>
+    /// <returns>The item.</returns>
+    public async Task<TDeviceModel?> GetDefaultConsole
+    (
+      bool isInput,
+      bool isOutput
+    )
     {
-      throw new NotImplementedException();
+      Device? device = await this
+        .CoreAudioService
+        .GetDefaultConsoleAsync
+        (
+          isInput,
+          isOutput
+        ).ConfigureAwait(false);
+
+      if (device == null)
+      {
+        return null;
+      }
+
+      string actualId = device
+        .Id
+        .ToString();
+
+      return this
+        ._Repository
+        .GetByActualId(actualId);
     }
 
-    public Task<bool> Mute(uint id)
+    /// <summary>
+    /// Get the default multimedia <typeparamref name="TDeviceModel"/> item.
+    /// </summary>
+    /// <param name="isInput">True/false is an input</param>
+    /// <param name="isOutput">True/false is an output</param>
+    /// <returns>The item.</returns>
+    public async Task<TDeviceModel?> GetDefaultMultimedia
+    (
+      bool isInput,
+      bool isOutput
+    )
     {
-      throw new NotImplementedException();
+      Device? device = await this
+        .CoreAudioService
+        .GetDefaultMultimediaAsync
+        (
+          isInput,
+          isOutput
+        ).ConfigureAwait(false);
+
+      if (device == null)
+      {
+        return null;
+      }
+
+      string actualId = device
+        .Id
+        .ToString();
+
+      return this
+        ._Repository
+        .GetByActualId(actualId);
     }
 
-    public Task<bool> MuteAll()
+    /// <summary>
+    /// Mute a <typeparamref name="TDeviceModel"/> item.
+    /// </summary>
+    /// <param name="id">The ID</param>
+    /// <returns>True/false result.</returns>
+    public async Task<bool> MuteAsync(uint id)
     {
-      throw new NotImplementedException();
+      TDeviceModel? model = this
+        ._Repository
+        .Get(id);
+
+      return await this
+        .CoreAudioService
+        .MuteAsync(model.ActualId)
+        .ConfigureAwait(false);
     }
 
-    public Task<bool> MuteRange(uint startId, uint endId)
+
+    /// <summary>
+    /// Mute an enumerable of all <typeparamref name="TDeviceModel"/> item(s).
+    /// </summary>
+    /// <returns>True/false result.</returns>
+    public async IAsyncEnumerable<bool> MuteAllAsync()
     {
-      throw new NotImplementedException();
+      IEnumerable<TDeviceModel> enumerable = this
+        ._Repository
+        .GetAll();
+
+      foreach (var item in enumerable)
+      {
+        yield return await this
+          .CoreAudioService
+          .MuteAsync(item.ActualId)
+          .ConfigureAwait(false);
+      }
     }
 
-    public Task<bool> Restart(uint id)
+    /// <summary>
+    /// Mute an enumerable of some <typeparamref name="TDeviceModel"/> item(s).
+    /// </summary>
+    /// <param name="idEnumerable">The enumerable of ID(s)</param>
+    /// <returns>True/false result.</returns>
+    public async IAsyncEnumerable<bool> MuteRangeAsync
+    (IEnumerable<uint> idEnumerable)
     {
-      throw new NotImplementedException();
+      IEnumerable<TDeviceModel> enumerable = this
+        ._Repository
+        .GetRange(idEnumerable);
+
+      foreach (var item in enumerable)
+      {
+        yield return await this
+          .CoreAudioService
+          .MuteAsync(item.ActualId)
+          .ConfigureAwait(false);
+      }
     }
 
-    public Task<bool> RestartAll()
+    /// <summary>
+    /// Mute an enumerable of some <typeparamref name="TDeviceModel"/> item(s).
+    /// </summary>
+    /// <param name="startId">The first ID</param>
+    /// <param name="endId">The last ID</param>
+    /// <returns>True/false result</returns>
+    public async IAsyncEnumerable<bool> MuteRangeAsync
+    (
+      uint startId,
+      uint endId
+    )
     {
-      throw new NotImplementedException();
+      IEnumerable<TDeviceModel> enumerable = this
+        ._Repository
+        .GetRange
+        (
+          startId,
+          endId
+        );
+
+      foreach (var item in enumerable)
+      {
+        yield return await this
+          .CoreAudioService
+          .MuteAsync(item.ActualId)
+          .ConfigureAwait(false);
+      }
     }
 
-    public Task<bool> RestartRange(uint startId, uint endId)
+    /// <summary>
+    /// Set the <typeparamref name="TDeviceModel"/> item as default.
+    /// </summary>
+    /// <param name="id">The ID</param>
+    /// <returns>The true/false result.</returns>
+    public async Task<bool> SetAsDefaultAsync(uint id)
     {
-      throw new NotImplementedException();
+      TDeviceModel? model = this
+        ._Repository
+        .Get(id);
+
+      return await this
+        .CoreAudioService
+        .SetAsDefaultAsync(model.ActualId)
+        .ConfigureAwait(false);
     }
 
-    public Task<bool> RestartRange(List<uint> idList)
+    /// <summary>
+    /// Set the <typeparamref name="TDeviceModel"/> item as default for
+    /// communications.
+    /// </summary>
+    /// <param name="id">The ID</param>
+    /// <returns>The true/false result.</returns>
+    public async Task<bool> SetAsDefaultCommunicationsAsync(uint id)
     {
-      throw new NotImplementedException();
+      TDeviceModel? model = this
+        ._Repository
+        .Get(id);
+
+      return await this
+        .CoreAudioService
+        .SetAsDefaultCommunicationsAsync(model.ActualId)
+        .ConfigureAwait(false);
     }
 
-    public Task<bool> SetAsDefault(uint id)
+    /// <summary>
+    /// Set the <typeparamref name="TDeviceModel"/> item volume.
+    /// </summary>
+    /// <param name="id">The ID</param>
+    /// <param name="volume">The audio volume</param>
+    /// <returns>The true/false result.</returns>
+    public async Task<bool> SetVolumeAsync
+    (
+      uint id,
+      double? volume
+    )
     {
-      throw new NotImplementedException();
+      TDeviceModel? model = this
+        ._Repository
+        .Get(id);
+
+      return await this
+        .CoreAudioService
+        .SetVolumeAsync
+        (
+          model.ActualId,
+          volume
+        ).ConfigureAwait(false);
     }
 
-    public Task<bool> SetAsDefaultCommunications(uint id)
+    /// <summary>
+    /// Start a <typeparamref name="TDeviceModel"/> item.
+    /// </summary>
+    /// <param name="id">The ID</param>
+    /// <returns>True/false result.</returns>
+    public async Task<bool> Start(uint id)
     {
-      throw new NotImplementedException();
+      TDeviceModel? model = this
+        ._Repository
+        .Get(id);
+
+      return await this
+        .MMDeviceService
+        .StartAsync(model.ActualId)
+        .ConfigureAwait(false);
     }
 
-    public Task<bool> SetVolume(string id, double? volume)
+    /// <summary>
+    /// Start an enumerable of all <typeparamref name="TDeviceModel"/> item(s).
+    /// </summary>
+    public void StartAll()
     {
-      throw new NotImplementedException();
+      this
+        .MMDeviceService
+        .StartAll();
     }
 
-    public Task<bool> Start(uint id)
+    /// <summary>
+    /// Start an enumerable of all <typeparamref name="TDeviceModel"/> item(s).
+    /// </summary>
+    /// <returns>True/false result.</returns>
+    public async IAsyncEnumerable<bool> StartAllAsync()
     {
-      throw new NotImplementedException();
+      IEnumerable<TDeviceModel> enumerable = this
+         ._Repository
+         .GetAll();
+
+      foreach (var item in enumerable)
+      {
+        yield return await this
+          .MMDeviceService
+          .StartAsync(item.ActualId)
+          .ConfigureAwait(false);
+      }
     }
 
-    public Task<bool> StartAll()
+    /// <summary>
+    /// Start an enumerable of some <typeparamref name="TDeviceModel"/> item(s).
+    /// </summary>
+    /// <param name="startId">The first ID</param>
+    /// <param name="endId">The last ID</param>
+    /// <returns>True/false result.</returns>
+    public async IAsyncEnumerable<bool> StartRange
+    (
+      uint startId,
+      uint endId
+    )
     {
-      throw new NotImplementedException();
+      IEnumerable<TDeviceModel> enumerable = this
+        ._Repository
+        .GetRange
+        (
+          startId,
+          endId
+        );
+
+      foreach (var item in enumerable)
+      {
+        yield return await this
+          .MMDeviceService
+          .StartAsync(item.ActualId)
+          .ConfigureAwait(false);
+      }
     }
 
-    public Task<bool> StartRange(uint startId, uint endId)
+    /// <summary>
+    /// Start an enumerable of some <typeparamref name="TDeviceModel"/> item(s).
+    /// </summary>
+    /// <param name="idEnumerable">The enumerable of ID(s)</param>
+    /// <returns>True/false result.</returns>
+    public async IAsyncEnumerable<bool> StartRangeAsync
+    (IEnumerable<uint> idEnumerable)
     {
-      throw new NotImplementedException();
+      IEnumerable<TDeviceModel> enumerable = this
+        ._Repository
+        .GetRange(idEnumerable);
+
+      foreach (var item in enumerable)
+      {
+        yield return await this
+          .MMDeviceService
+          .StartAsync(item.ActualId)
+          .ConfigureAwait(false);
+      }
     }
 
-    public Task<bool> StartRange(List<uint> idList)
+    /// <summary>
+    /// Stop a <typeparamref name="TDeviceModel"/> item.
+    /// </summary>
+    /// <param name="id">The ID</param>
+    /// <returns>True/false result.</returns>
+    public async Task<bool> StopAsync(uint id)
     {
-      throw new NotImplementedException();
+      TDeviceModel? model = this
+        ._Repository
+        .Get(id);
+
+      return await this
+        .MMDeviceService
+        .StopAsync(model.ActualId)
+        .ConfigureAwait(false);
     }
 
-    public Task<bool> Stop(uint id)
+    /// <summary>
+    /// Stop an enumerable of all <typeparamref name="TDeviceModel"/> item(s).
+    /// </summary>
+    public void StopAll()
     {
-      throw new NotImplementedException();
+      this
+        .MMDeviceService
+        .StopAll();
     }
 
-    public Task<bool> StopAll()
+    /// <summary>
+    /// Stop an enumerable of all <typeparamref name="TDeviceModel"/> item(s).
+    /// </summary>
+    /// <returns>True/false result.</returns>
+    public async IAsyncEnumerable<bool> StopAllAsync()
     {
-      throw new NotImplementedException();
+      IEnumerable<TDeviceModel> enumerable = this
+         ._Repository
+         .GetAll();
+
+      foreach (var item in enumerable)
+      {
+        yield return await this
+          .MMDeviceService
+          .StopAsync(item.ActualId)
+          .ConfigureAwait(false);
+      }
     }
 
-    public Task<bool> StopRange(uint startId, uint endId)
+    /// <summary>
+    /// Stop an enumerable of some <typeparamref name="TDeviceModel"/> item(s).
+    /// </summary>
+    /// <param name="startId">The first ID</param>
+    /// <param name="endId">The last ID</param>
+    /// <returns>True/false result.</returns>
+    public async IAsyncEnumerable<bool> StopRange
+    (
+      uint startId,
+      uint endId
+    )
     {
-      throw new NotImplementedException();
+      IEnumerable<TDeviceModel> enumerable = this
+        ._Repository
+        .GetRange
+        (
+          startId,
+          endId
+        );
+
+      foreach (var item in enumerable)
+      {
+        yield return await this
+          .MMDeviceService
+          .StopAsync(item.ActualId)
+          .ConfigureAwait(false);
+      }
     }
 
-    public Task<bool> StopRange(List<uint> idList)
+    /// <summary>
+    /// Stop an enumerable of some <typeparamref name="TDeviceModel"/> item(s).
+    /// </summary>
+    /// <param name="idEnumerable">The enumerable of ID(s)</param>
+    /// <returns>True/false result.</returns>
+    public async IAsyncEnumerable<bool> StopRangeAsync
+    (IEnumerable<uint> idEnumerable)
     {
-      throw new NotImplementedException();
+      IEnumerable<TDeviceModel> enumerable = this
+        ._Repository
+        .GetRange(idEnumerable);
+
+      foreach (var item in enumerable)
+      {
+        yield return await this
+          .MMDeviceService
+          .StopAsync(item.ActualId)
+          .ConfigureAwait(false);
+      }
     }
 
-    public Task<bool> Unmute(uint id)
+    /// <summary>
+    /// Unmute a <typeparamref name="TDeviceModel"/> item.
+    /// </summary>
+    /// <param name="id">The ID</param>
+    /// <returns>True/false result.</returns>
+    public async Task<bool> UnmuteAsync(uint id)
     {
-      throw new NotImplementedException();
+      TDeviceModel? model = this
+        ._Repository
+        .Get(id);
+
+      return await this
+        .CoreAudioService
+        .UnmuteAsync(model.ActualId)
+        .ConfigureAwait(false);
     }
 
-    public Task<bool> UnmuteAll()
+    /// <summary>
+    /// Unmute an enumerable of all <typeparamref name="TDeviceModel"/> item(s).
+    /// </summary>
+    /// <returns>True/false result.</returns>
+    public async IAsyncEnumerable<bool> UnmuteAllAsync()
     {
-      throw new NotImplementedException();
+      IEnumerable<TDeviceModel> enumerable = this
+        ._Repository
+        .GetAll();
+
+      foreach (var item in enumerable)
+      {
+        yield return await this
+          .CoreAudioService
+          .UnmuteAsync(item.ActualId)
+          .ConfigureAwait(false);
+      }
     }
 
-    public Task<bool> UnmuteRange(uint startId, uint endId)
+    /// <summary>
+    /// Unmute an enumerable of some <typeparamref name="TDeviceModel"/> item(s).
+    /// </summary>
+    /// <param name="idEnumerable">The enumerable of ID(s)</param>
+    /// <returns>True/false result.</returns>
+    public async IAsyncEnumerable<bool> UnmuteRangeAsync
+    (IEnumerable<uint> idEnumerable)
     {
-      throw new NotImplementedException();
+      IEnumerable<TDeviceModel> enumerable = this
+        ._Repository
+        .GetRange(idEnumerable);
+
+      foreach (var item in enumerable)
+      {
+        yield return await this
+          .CoreAudioService
+          .UnmuteAsync(item.ActualId)
+          .ConfigureAwait(false);
+      }
     }
 
-    public Task<bool> Update(uint id)
+    /// <summary>
+    /// Unmute an enumerable of some <typeparamref name="TDeviceModel"/> item(s).
+    /// </summary>
+    /// <param name="startId">The first ID</param>
+    /// <param name="endId">The last ID</param>
+    /// <returns>True/false result</returns>
+    public async IAsyncEnumerable<bool> UnmuteRangeAsync
+    (
+      uint startId,
+      uint endId
+    )
     {
-      throw new NotImplementedException();
+      IEnumerable<TDeviceModel> enumerable = this
+        ._Repository
+        .GetRange
+        (
+          startId,
+          endId
+        );
+
+      foreach (var item in enumerable)
+      {
+        yield return await this
+          .CoreAudioService
+          .UnmuteAsync(item.ActualId)
+          .ConfigureAwait(false);
+      }
     }
 
-    public Task<bool> UpdateAll()
+    /// <summary>
+    /// Update a <typeparamref name="TDeviceModel"/> item.
+    /// </summary>
+    /// <param name="id">The ID</param>
+    public void Update(uint id)
     {
-      throw new NotImplementedException();
+      TDeviceModel? model = this
+        ._Repository
+        .Get(id);
+
+      this
+         .MMDeviceService
+         .Update(model.ActualId);
     }
 
-    public Task<bool> UpdateRange(uint startId, uint endId)
+    /// <summary>
+    /// Update an enumerable of all <typeparamref name="TDeviceModel"/> item(s).
+    /// </summary>
+    public void UpdateAll()
     {
-      throw new NotImplementedException();
+      this
+        .MMDeviceService
+        .UpdateAll();
     }
 
-    public Task<bool> UpdateRange(List<uint> idList)
+    /// <summary>
+    /// Update an enumerable of all <typeparamref name="TDeviceModel"/> item(s).
+    /// </summary>
+    /// <returns>True/false result.</returns>
+    public async Task<bool> UpdateAllAsync()
     {
-      throw new NotImplementedException();
+      return await this
+        .CoreAudioService
+        .UpdateAllAsync()
+        .ConfigureAwait(false);
+    }
+
+    /// <summary>
+    /// Update an enumerable of some <typeparamref name="TDeviceModel"/> item(s).
+    /// </summary>
+    /// <param name="startId">The first ID</param>
+    /// <param name="endId">The last ID</param>
+    public void UpdateRange
+    (
+      uint startId,
+      uint endId
+    )
+    {
+      IEnumerable<TDeviceModel> enumerable = this
+        ._Repository
+        .GetRange
+        (
+          startId,
+          endId
+        );
+
+      foreach (var item in enumerable)
+      {
+        this
+          .MMDeviceService
+          .Update(item.ActualId);
+      }
+    }
+
+    /// <summary>
+    /// Update an enumerable of some <typeparamref name="TDeviceModel"/> item(s).
+    /// </summary>
+    /// <param name="idEnumerable">The enumerable of ID(s)</param>
+    public void UpdateRange(IEnumerable<uint> idEnumerable)
+    {
+      IEnumerable<TDeviceModel> enumerable = this
+        ._Repository
+        .GetRange(idEnumerable);
+
+      foreach (var item in enumerable)
+      {
+        this
+          .MMDeviceService
+          .Update(item.ActualId);
+      }
     }
 
     #endregion
