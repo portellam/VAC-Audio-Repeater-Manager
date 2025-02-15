c22e05136a62f782900b4ea869b3c6cb3d0cf0a0
portellam <amportell.13@gmail.com>
2025-02-15 02:32:57 -0500

    Added readonly repository (to be used with MMDevice and CoreAudioDevice).

---
+++ VACARM.Backend/VACARM.Infrastructure/Repositories/IReadonlyRepository.cs
@@ -0,0 +1,64 @@
+﻿using System.ComponentModel;
+
+namespace VACARM.Infrastructure.Repositories
+{
+  public interface IReadonlyRepository<TItem> where TItem :
+    class
+  {
+    #region Parameters
+
+    event PropertyChangedEventHandler PropertyChanged;
+
+    /// <summary>
+    /// The maximum count of <typeparamref name="TItem"/>(s).
+    /// </summary>
+    int MaxCount { get; set; }
+
+    #endregion
+
+    #region Logic
+
+    /// <summary>
+    /// True/false does the index exist.
+    /// </summary>
+    /// <param name="index">The index</param>
+    /// <returns>True/false</returns>
+    bool ContainsIndex(int index);
+
+    /// <summary>
+    /// Is enumerable of <typeparamref name="TItem"/>(s) null or empty.
+    /// </summary>
+    /// <param name="enumerable">The enumerable</param>
+    /// <returns>True/false</returns>
+    bool IsNullOrEmpty(IEnumerable<TItem> enumerable);
+
+    /// <summary>
+    /// True/false is the index valid.
+    /// </summary>
+    /// <param name="index">The index</param>
+    /// <returns>True/false</returns>
+    bool IsValidIndex(int index);
+
+    /// <summary>
+    /// Get a <typeparamref name="TItem"/>.
+    /// </summary>
+    /// <param name="func">The function</param>
+    /// <returns>The item.</returns>
+    TItem? Get(Func<TItem, bool> func);
+
+    /// <summary>
+    /// Get an enumerable of all <typeparamref name="TItem"/>(s).
+    /// </summary>
+    /// <returns>The enumerable of item(s).</returns>
+    IEnumerable<TItem> GetAll();
+
+    /// <summary>
+    /// Get an enumerable of some <typeparamref name="TItem"/>(s).
+    /// </summary>
+    /// <param name="func">The function</param>
+    /// <returns>The enumerable of item(s).</returns>
+    IEnumerable<TItem> GetRange(Func<TItem, bool> func);
+
+    #endregion
+  }
+}
---
+++ VACARM.Backend/VACARM.Infrastructure/Repositories/ReadonlyRepository.cs
@@ -0,0 +1,156 @@
+﻿using System.ComponentModel;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using VACARM.Infrastructure.Extensions;
+
+namespace VACARM.Infrastructure.Repositories
+{
+  /// <summary>
+  /// The readonly repository.
+  /// </summary>
+  public class ReadonlyRepository<TItem> :
+    IReadonlyRepository<TItem> where TItem :
+    class
+  {
+    #region Parameters
+
+    /// <summary>
+    /// The enumerable of all <typeparamref name="TItem"/>(s).
+    /// </summary>
+    protected IEnumerable<TItem> Enumerable
+    {
+      get
+      {
+        return this.enumerable;
+      }
+      set
+      {
+        this.enumerable = value;
+        OnPropertyChanged(nameof(Enumerable));
+      }
+    }
+
+    private IEnumerable<TItem> enumerable { get; set; }
+    private int maxCount { get; set; } = int.MaxValue;
+
+    public virtual event PropertyChangedEventHandler PropertyChanged;
+
+    public virtual int MaxCount
+    {
+      get
+      {
+        return this.maxCount;
+      }
+      set
+      {
+        this.maxCount = value;
+        OnPropertyChanged(nameof(MaxCount));
+      }
+    }
+
+    #endregion
+
+    #region Logic
+
+    /// <summary>
+    /// Logs event when property has changed.
+    /// </summary>
+    /// <param name="propertyName">The property name</param>
+    internal void OnPropertyChanged(string propertyName)
+    {
+      PropertyChanged?.Invoke
+      (
+        this,
+        new PropertyChangedEventArgs(propertyName)
+      );
+
+      Debug.WriteLine
+      (
+        string.Format
+        (
+          "PropertyChanged: {0}",
+          propertyName
+        )
+      );
+    }
+
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    [ExcludeFromCodeCoverage]
+    public ReadonlyRepository()
+    {
+      this.Enumerable = Array.Empty<TItem>();
+      this.MaxCount = int.MaxValue;
+    }
+
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    /// <param name="enumerable">The enumerable of item(s)</param>
+    /// <param name="maxCount">The max count of item(s)</param>
+    [ExcludeFromCodeCoverage]
+    public ReadonlyRepository
+    (
+      IEnumerable<TItem> enumerable,
+      int maxCount
+    )
+    {
+      this.Enumerable = enumerable;
+      this.MaxCount = maxCount;
+    }
+
+    public bool ContainsIndex(int index)
+    {
+      return index >= 0
+        && index <= this.Enumerable.Count();
+    }
+
+    public bool IsNullOrEmpty(IEnumerable<TItem> enumerable)
+    {
+      return IEnumerableExtension<TItem>.IsNullOrEmpty(enumerable);
+    }
+
+    public bool IsValidIndex(int index)
+    {
+      return index >= 0
+        && index <= this.MaxCount;
+    }
+
+    public TItem? Get(Func<TItem, bool> func)
+    {
+      if (this.IsNullOrEmpty(this.Enumerable))
+      {
+        return null;
+      }
+
+      return this.Enumerable.
+        FirstOrDefault(func);
+    }
+
+    public IEnumerable<TItem> GetAll()
+    {
+      if (IsNullOrEmpty(this.Enumerable))
+      {
+        return Array.Empty<TItem>();
+      }
+
+      return this.Enumerable
+        .AsEnumerable();
+    }
+
+    public IEnumerable<TItem> GetRange(Func<TItem, bool> func)
+    {
+      if (IsNullOrEmpty(this.Enumerable))
+      {
+        return Array.Empty<TItem>();
+      }
+
+      return this.Enumerable
+        .Where(x => func(x))
+        .AsEnumerable();
+    }
+
+    #endregion
+  }
+}
