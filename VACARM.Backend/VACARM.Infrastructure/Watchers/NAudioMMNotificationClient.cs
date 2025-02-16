a5b130646154694f895cd05325794ce6f698e9f1
portellam <amportell.13@gmail.com>
2025-02-16 08:09:00 -0500

    Added TOOD.

--- VACARM.Backend/VACARM.Infrastructure/Watchers/NAudioMMNotificationClient.cs
+++ VACARM.Backend/VACARM.Infrastructure/Watchers/NAudioMMNotificationClient.cs
@@ -1,6 +1,64 @@
 ﻿using NAudio.CoreAudioApi;
 using NAudio.CoreAudioApi.Interfaces;
 
+/*
+ * TODO:
+ * - determine location.
+ *  - Repository v. Service
+ *    - should a Repository only deal with CRUD?
+ *    - moved complex/extraneous CRUD logic outside of Repository to Service.
+ *    - business logic is in Service. This includes async logic.
+ *    - minimum CRUD logic is in Repository.
+ *  
+ *    - Q: Why Service? B/c this logic is async, and we should keep any complex
+ *    logic outside of the Repository.
+ *  
+ *  - MMDevice Service v. DeviceModel Service
+ *    - MMDevice business logic will reside in MMDeviceService.
+ *    - DeviceService may reference MMDeviceService. 
+ *    
+ *  - UI Controllers v. Services
+ *    - A Controller may call the business logic of one or more Service(s).
+ *    - A Service will care for the Repository/ies it handles.
+ *    - A Controller likely only needs Readonly access to the Repository, no complete CRUD.
+ *  
+ * - determine use case
+ *   - 1. update entire enumerable(s)
+ *      - if done individually (foreach item in an enumerable), this is slow.
+ *      - if done wholesale (entire enumerable), this is fast?
+ *   
+ *   - 2. update entire repository/ies
+ *      -  if done this way, the Repository should be 
+ *        - kept readonly, no complete CRUD.
+ *        - the enumerable changed wholesale on events.
+ *            
+ *  - conclusion:
+ *    - 1. Place this Watcher in MMDeviceService.
+ *    
+ *    - 2. Create an object of MMDeviceRepository. For the enumerable, use the 
+ *    collection from the property MMDeviceEnumerator.
+ *    
+ *    - 3. Determine what to do for changes: 
+ *      - a. Update the MMDeviceRepository wholesale.
+ *        - NOTE: For one Repository, this is fastest.
+ *        - WARNING: For two or more Services watching, this may cause slowdown.
+ *        - IDEA: 
+ *          - i. Prioritize MMDeviceService for UI presentation and logic. 
+ *          - ii. Forcedly have other Services/Repositories update slower.
+ *          - iii. For DeviceService, do its logic when it's appropriate. 
+ *          When updating, do so at file save or less periodically 
+ *          (every minute instead of every second).
+ *          - iv. For RepeaterService, when trying to start a repeater
+ *            - 1. Validate by MMDeviceService. If device ID is valid, and 
+ *            device is input or output.
+ *            - 2. Start a repeater. Do not enable or disable an audio device.
+ *      
+ *      - b. Optionally: run business logic in MMDeviceService.
+ *    
+ *    - 4. Determine how to watch for property changes to the 
+ *    MMDeviceService/MMDeviceRepository from other classes.
+ */
+
 namespace VACARM.Infrastructure.Watchers
 {
   /// <summary>
