517c6772dfbae12d967f206a7e99f56ce6b6380f
portellam <amportell.13@gmail.com>
2025-02-17 18:32:58 -0500

    Copied old "DeviceRepository" logic, updated base repo.

--- VACARM.Backend/VACARM.Infrastructure/Services/DeviceService.cs
+++ VACARM.Backend/VACARM.Infrastructure/Services/DeviceService.cs
@@ -5,6 +5,7 @@ using AudioSwitcher.AudioApi;
 using VACARM.Domain.Models;
 using VACARM.Infrastructure.Repositories;
 using System.Diagnostics.CodeAnalysis;
+using VACARM.Infrastructure.Functions;
 
 namespace VACARM.Application.Services
 {
@@ -12,9 +13,9 @@ namespace VACARM.Application.Services
   /// The service of the <typeparamref name="DeviceRepository"/>.
   /// </summary>
   public partial class DeviceService<TRepository, TDeviceModel> :
-    BaseService<DeviceRepository<TDeviceModel>, TDeviceModel>,
-    IDeviceService<DeviceRepository<TDeviceModel>, TDeviceModel> where TRepository :
-    DeviceRepository<TDeviceModel> where TDeviceModel :
+    BaseService<BaseRepository<TDeviceModel>, TDeviceModel>,
+    IDeviceService<BaseRepository<TDeviceModel>, TDeviceModel> where TRepository :
+    BaseRepository<TDeviceModel> where TDeviceModel :
     DeviceModel
   {
     #region Parameters
@@ -115,15 +116,69 @@ namespace VACARM.Application.Services
       this.CoreAudioService = coreAudioService;
     }
 
+    /// <summary>
+    /// Start an enumerable of all <typeparamref name="TDeviceModel"/>(s).
+    /// </summary>
+    public void Start(uint id)
+    {
+      this.MMDeviceService
+        .Start(id);
+
+      this.Update(id);
+    }
 
     /// <summary>
     /// Start an enumerable of all <typeparamref name="TDeviceModel"/>(s).
     /// </summary>
     public void StartAll()
     {
-      this
-        .MMDeviceService
+      this.MMDeviceService
         .StartAll();
+
+      this.UpdateAll();
+    }
+
+    /// <summary>
+    /// Start an enumerable of some <typeparamref name="TDeviceModel"/>(s).
+    /// </summary>
+    /// <param name="startId">The first ID</param>
+    /// <param name="endId">The last ID</param>
+    public void StartRange
+    (
+      uint startId,
+      uint endId
+    )
+    {
+      var func = BaseFunctions<TDeviceModel>.ContainsIdRange
+        (
+          startId,
+          endId
+        );
+
+      var enumerable = this.Repository
+        .GetRange(func);
+
+      foreach (var item in enumerable)
+      {
+        this.Start(item.Id);
+      }
+    }
+
+    /// <summary>
+    /// Start an enumerable of some <typeparamref name="TDeviceModel"/>(s).
+    /// </summary>
+    /// <param name="idEnumerable">The enumerable of ID(s)</param>
+    public void StartRange(IEnumerable<uint> idEnumerable)
+    {
+      var func = BaseFunctions<TDeviceModel>.ContainsIdEnumerable(idEnumerable);
+
+      var enumerable = this.Repository
+        .GetRange(func);
+
+      foreach (var item in enumerable)
+      {
+        this.Start(item.Id);
+      }
     }
 
     /// <summary>
@@ -131,42 +186,16 @@ namespace VACARM.Application.Services
     /// </summary>
     public void StopAll()
     {
-      this
-        .MMDeviceService
+      this.MMDeviceService
         .StopAll();
     }
 
     /// <summary>
-    /// Update a <typeparamref name="TDeviceModel"/>.
-    /// </summary>
-    /// <param name="id">The ID</param>
-    public void Update(uint id)
-    {
-      TDeviceModel? model = this
-        ._Repository
-        .Get(id);
-
-      this
-         .MMDeviceService
-         .Update(model.ActualId);
-    }
-
-    /// <summary>
-    /// Update an enumerable of all <typeparamref name="TDeviceModel"/>(s).
-    /// </summary>
-    public void UpdateAll()
-    {
-      this
-        .MMDeviceService
-        .UpdateAll();
-    }
-
-    /// <summary>
-    /// Update an enumerable of some <typeparamref name="TDeviceModel"/>(s).
+    /// Stop an enumerable of some <typeparamref name="TDeviceModel"/>(s).
     /// </summary>
     /// <param name="startId">The first ID</param>
     /// <param name="endId">The last ID</param>
-    public void UpdateRange
+    public void StopRange
     (
       uint startId,
       uint endId
@@ -182,8 +211,72 @@ namespace VACARM.Application.Services
 
       foreach (var item in enumerable)
       {
-        this
-          .MMDeviceService
+        this.MMDeviceService
+          .Stop(item.ActualId);
+      }
+    }
+
+    /// <summary>
+    /// Stop an enumerable of some <typeparamref name="TDeviceModel"/>(s).
+    /// </summary>
+    /// <param name="idEnumerable">The enumerable of ID(s)</param>
+    public void StopRange
+        (IEnumerable<uint> idEnumerable)
+    {
+      IEnumerable<TDeviceModel> enumerable = this
+        ._Repository
+        .GetRange(idEnumerable);
+
+      foreach (var item in enumerable)
+      {
+        this.MMDeviceService
+          .Stop(item.ActualId);
+      }
+    }
+
+    /// <summary>
+    /// Update a <typeparamref name="TDeviceModel"/>.
+    /// </summary>
+    /// <param name="id">The ID</param>
+    public void Update(uint id)
+    {
+      TDeviceModel? model = this._Repository
+        .Get(id);
+
+      this.MMDeviceService
+         .Update(model.ActualId);
+    }
+
+    /// <summary>
+    /// Update an enumerable of all <typeparamref name="TDeviceModel"/>(s).
+    /// </summary>
+    public void UpdateAll()
+    {
+      this.MMDeviceService
+        .UpdateAll();
+    }
+
+    /// <summary>
+    /// Update an enumerable of some <typeparamref name="TDeviceModel"/>(s).
+    /// </summary>
+    /// <param name="startId">The first ID</param>
+    /// <param name="endId">The last ID</param>
+    public void UpdateRange
+    (
+      uint startId,
+      uint endId
+    )
+    {
+      IEnumerable<TDeviceModel> enumerable = this._Repository
+        .GetRange
+        (
+          startId,
+          endId
+        );
+
+      foreach (var item in enumerable)
+      {
+        this.MMDeviceService
           .Update(item.ActualId);
       }
     }
@@ -194,16 +287,139 @@ namespace VACARM.Application.Services
     /// <param name="idEnumerable">The enumerable of ID(s)</param>
     public void UpdateRange(IEnumerable<uint> idEnumerable)
     {
-      IEnumerable<TDeviceModel> enumerable = this
-        ._Repository
+      IEnumerable<TDeviceModel> enumerable = this._Repository
         .GetRange(idEnumerable);
 
       foreach (var item in enumerable)
       {
-        this
-          .MMDeviceService
+        this.MMDeviceService
           .Update(item.ActualId);
       }
+    }
+
+    public TDeviceModel? GetByActualId(string actualId)
+    {
+      Func<TDeviceModel, bool> func = (TDeviceModel x) => x.ActualId == actualId;
+      return this.Get(func);
+    }
+
+    public TDeviceModel? GetDefaultCommunications()
+    {
+      return this
+        .GetAllCommunications()
+        .FirstOrDefault(x => x.IsDefault);
+    }
+
+    public TDeviceModel? GetDefaultConsole()
+    {
+      return this
+        .GetAllConsole()
+        .FirstOrDefault(x => x.IsDefault);
+    }
+
+    public TDeviceModel? GetDefaultMultimedia()
+    {
+      return this
+        .GetAllMultimedia()
+        .FirstOrDefault(x => x.IsDefault);
+    }
+
+    public IEnumerable<TDeviceModel> GetAllAbsent()
+    {
+      Func<TDeviceModel, bool> func = (TDeviceModel x) => !x.IsPresent;
+      return base.GetRange(func);
+    }
+
+    public IEnumerable<TDeviceModel> GetAllAlphabetical()
+    {
+      return base
+        .GetAll()
+        .OrderBy(x => x.Name);
+    }
+
+    public IEnumerable<TDeviceModel> GetAllAlphabeticalDescending()
+    {
+      return base
+        .GetAll()
+        .OrderByDescending(x => x.Name);
+    }
+
+    public IEnumerable<TDeviceModel> GetAllCapture()
+    {
+      Func<TDeviceModel, bool> func = (TDeviceModel x) => x.IsCapture;
+      return base.GetRange(func);
+    }
+
+    public IEnumerable<TDeviceModel> GetAllCommunications()
+    {
+      Func<TDeviceModel, bool> func = (TDeviceModel x) =>
+        x.Role == "Communications";
+
+      return base.GetRange(func);
+    }
+
+    public IEnumerable<TDeviceModel> GetAllConsole()
+    {
+      Func<TDeviceModel, bool> func = (TDeviceModel x) =>
+        x.Role == "Console";
+
+      return base.GetRange(func);
+    }
+
+    public IEnumerable<TDeviceModel> GetAllDefault()
+    {
+      Func<TDeviceModel, bool> func = (TDeviceModel x) => x.IsDefault;
+      return base.GetRange(func);
+    }
+
+    public IEnumerable<TDeviceModel> GetAllDisabled()
+    {
+      Func<TDeviceModel, bool> func = (TDeviceModel x) => !x.IsEnabled;
+      return base.GetRange(func);
+    }
+
+    public IEnumerable<TDeviceModel> GetAllDuplex()
+    {
+      Func<TDeviceModel, bool> func = (TDeviceModel x) => x.IsDuplex;
+      return base.GetRange(func);
+    }
+
+    public IEnumerable<TDeviceModel> GetAllEnabled()
+    {
+      Func<TDeviceModel, bool> func = (TDeviceModel x) => x.IsEnabled;
+      return base.GetRange(func);
+    }
+
+    public IEnumerable<TDeviceModel> GetAllMultimedia()
+    {
+      Func<TDeviceModel, bool> func = (TDeviceModel x) =>
+        x.Role == "Multimedia";
+
+      return base.GetRange(func);
+    }
+
+    public IEnumerable<TDeviceModel> GetAllMuted()
+    {
+      Func<TDeviceModel, bool> func = (TDeviceModel x) => x.IsMuted;
+      return base.GetRange(func);
+    }
+
+    public IEnumerable<TDeviceModel> GetAllNotMuted()
+    {
+      Func<TDeviceModel, bool> func = (TDeviceModel x) => !x.IsMuted;
+      return base.GetRange(func);
+    }
+
+    public IEnumerable<TDeviceModel> GetAllPresent()
+    {
+      Func<TDeviceModel, bool> func = (TDeviceModel x) => x.IsPresent;
+      return base.GetRange(func);
+    }
+
+    public IEnumerable<TDeviceModel> GetAllRender()
+    {
+      Func<TDeviceModel, bool> func = (TDeviceModel x) => x.IsRender;
+      return base.GetRange(func);
     }
 
     #endregion
--- VACARM.Backend/VACARM.Infrastructure/Services/IDeviceService.cs
+++ VACARM.Backend/VACARM.Infrastructure/Services/IDeviceService.cs
@@ -5,8 +5,132 @@ namespace VACARM.Application.Services
 {
   public partial interface IDeviceService<TRepository, TDeviceModel> where
     TRepository :
-    DeviceRepository<TDeviceModel> where TDeviceModel :
+    BaseRepository<TDeviceModel> where TDeviceModel :
     DeviceModel
   {
+    #region Logic
+
+    /// <summary>
+    /// Get a <typeparamref name="TDeviceModel"/>.
+    /// </summary>
+    /// <param name="actualId">The actual ID</param>
+    /// <returns>The item.</returns>
+    TDeviceModel? GetByActualId(string actualId);
+
+    /// <summary>
+    /// Get a default communications <typeparamref name="TDeviceModel"/>.
+    /// </summary>
+    /// <returns>The item.</returns>
+    TDeviceModel? GetDefaultCommunications();
+
+    /// <summary>
+    /// Get a default console <typeparamref name="TDeviceModel"/>.
+    /// </summary>
+    /// <returns>The item.</returns>
+    TDeviceModel? GetDefaultConsole();
+
+    /// <summary>
+    /// Get a default multimedia <typeparamref name="TDeviceModel"/>.
+    /// </summary>
+    /// <returns>The item.</returns>
+    TDeviceModel? GetDefaultMultimedia();
+
+    /// <summary>
+    /// Get an enumerable of all absent <typeparamref name="TDeviceModel"/>(s).
+    /// </summary>
+    /// <returns>The enumerable of item(s).</returns>
+    IEnumerable<TDeviceModel> GetAllAbsent();
+
+    /// <summary>
+    /// Get an enumerable of all <typeparamref name="TDeviceModel"/>(s) in
+    /// alphebetical order.
+    /// <typeparamref name="TDeviceModel"/>(s).
+    /// </summary>
+    /// <returns>The enumerable of item(s).</returns>
+    IEnumerable<TDeviceModel> GetAllAlphabetical();
+
+    /// <summary>
+    /// Get an enumerable of all <typeparamref name="TDeviceModel"/>(s) in
+    /// alphebetical descending order.
+    /// </summary>
+    /// <returns>The enumerable of item(s).</returns>
+    IEnumerable<TDeviceModel> GetAllAlphabeticalDescending();
+
+    /// <summary>
+    /// Get an enumerable of all capture <typeparamref name="TDeviceModel"/>(s).
+    /// </summary>
+    /// <returns>The enumerable of item(s).</returns>
+    IEnumerable<TDeviceModel> GetAllCapture();
+
+    /// <summary>
+    /// Get an enumerable of all communications <typeparamref name="TDeviceModel"/> 
+    ///(s).
+    /// </summary>
+    /// <returns>The enumerable of item(s).</returns>
+    IEnumerable<TDeviceModel> GetAllCommunications();
+
+    /// <summary>
+    /// Get an enumerable of all console <typeparamref name="TDeviceModel"/>(s).
+    /// </summary>
+    /// <returns>The enumerable of item(s).</returns>
+    IEnumerable<TDeviceModel> GetAllConsole();
+
+    /// <summary>
+    /// Get an enumerable of all default <typeparamref name="TDeviceModel"/>(s).
+    /// </summary>
+    /// <returns>The enumerable of item(s).</returns>
+    IEnumerable<TDeviceModel> GetAllDefault();
+
+    /// <summary>
+    /// Get an enumerable of all disabled <typeparamref name="TDeviceModel"/>(s).
+    /// </summary>
+    /// <returns>The enumerable of item(s).</returns>
+    IEnumerable<TDeviceModel> GetAllDisabled();
+
+    /// <summary>
+    /// Get an enumerable of all duplex <typeparamref name="TDeviceModel"/>(s).
+    /// </summary>
+    /// <returns>The enumerable of item(s).</returns>
+    IEnumerable<TDeviceModel> GetAllDuplex();
+
+    /// <summary>
+    /// Get an enumerable of all enabled <typeparamref name="TDeviceModel"/>(s).
+    /// </summary>
+    /// <returns>The enumerable of item(s).</returns>
+    IEnumerable<TDeviceModel> GetAllEnabled();
+
+    /// <summary>
+    /// Get an enumerable of all multimedia <typeparamref name="TDeviceModel"/>
+    ///(s).
+    /// </summary>
+    /// <returns>The enumerable of item(s).</returns>
+    IEnumerable<TDeviceModel> GetAllMultimedia();
+
+    /// <summary>
+    /// Get an enumerable of all muted <typeparamref name="TDeviceModel"/>(s).
+    /// </summary>
+    /// <returns>The enumerable of item(s).</returns>
+    IEnumerable<TDeviceModel> GetAllMuted();
+
+    /// <summary>
+    /// Get an enumerable of all not muted
+    /// <typeparamref name="TDeviceModel"/>(s).
+    /// </summary>
+    /// <returns>The enumerable of item(s).</returns>
+    IEnumerable<TDeviceModel> GetAllNotMuted();
+
+    /// <summary>
+    /// Get an enumerable of all present <typeparamref name="TDeviceModel"/>(s).
+    /// </summary>
+    /// <returns>The enumerable of item(s).</returns>
+    IEnumerable<TDeviceModel> GetAllPresent();
+
+    /// <summary>
+    /// Get an enumerable of all render <typeparamref name="TDeviceModel"/>(s).
+    /// </summary>
+    /// <returns>The enumerable of item(s).</returns>
+    IEnumerable<TDeviceModel> GetAllRender();
+
+    #endregion
   }
 }
