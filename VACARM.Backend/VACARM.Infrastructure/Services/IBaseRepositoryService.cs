9e16c3a43c9224b466adf793930c6ddafbf89a14
portellam <amportell.13@gmail.com>
2025-02-18 04:53:38 -0500

    Create IBaseRepositoryService.cs

---
+++ VACARM.Backend/VACARM.Infrastructure/Services/IBaseRepositoryService.cs
@@ -0,0 +1,20 @@
+﻿using VACARM.Domain.Models;
+using VACARM.Infrastructure.Repositories;
+
+namespace VACARM.Application.Services
+{
+  public interface IBaseRepositoryService
+    <
+      TService,
+      TRepository,
+      TBaseModel
+    >
+    where TService :
+    BaseService<TRepository, TBaseModel>
+    where TRepository :
+    BaseRepository<TBaseModel>
+    where TBaseModel :
+    BaseModel
+  {
+  }
+}
