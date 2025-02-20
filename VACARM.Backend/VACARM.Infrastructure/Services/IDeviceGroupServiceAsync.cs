0b5feacf0cbd6fc23d77c2c794d4e66c2ec762b9
portellam <amportell.13@gmail.com>
2025-02-20 04:25:27 -0500

    Update IDeviceGroupServiceAsync.cs

--- VACARM.Backend/VACARM.Infrastructure/Services/IDeviceGroupServiceAsync.cs
+++ VACARM.Backend/VACARM.Infrastructure/Services/IDeviceGroupServiceAsync.cs
@@ -11,11 +11,148 @@
     #region Logic
 
     /// <summary>
+    /// Update an enumerable of some <typeparamref name="TDeviceModel"/>(s).
+    /// </summary>
+    /// <param name="startId">The first ID</param>
+    /// <param name="endId">The last ID</param>
+    /// <returns>True/false result.</returns>
+    IAsyncEnumerable<bool> UpdateRangeAsync
+    (
+      uint startId,
+      uint endId
+    );
+
+    /// <summary>
+    /// Update an enumerable of some <typeparamref name="TDeviceModel"/>(s).
+    /// </summary>
+    /// <param name="idList">The enumerable of ID(s)</param>
+    /// <returns>True/false result.</returns>
+    IAsyncEnumerable<bool> UpdateRangeAsync(List<uint> idList);
+
+    /// <summary>
+    /// Mute the enumerable of all <typeparamref name="TDeviceModel"/>(s).
+    /// </summary>
+    /// <returns>True/false result.</returns>
+    IAsyncEnumerable<bool> MuteAllAsync();
+
+    /// <summary>
+    /// Mute an enumerable of some <typeparamref name="TDeviceModel"/>(s).
+    /// </summary>
+    /// <param name="idEnumerable">The enumerable of ID(s)</param>
+    /// <returns>True/false result.</returns>
+    IAsyncEnumerable<bool> MuteRangeAsync
+    (IEnumerable<uint> idEnumerable);
+
+    /// <summary>
+    /// Mute an enumerable of some <typeparamref name="TDeviceModel"/>(s).
+    /// </summary>
+    /// <param name="startId">The first ID</param>
+    /// <param name="endId">The last ID</param>
+    /// <returns>True/false result</returns>
+    IAsyncEnumerable<bool> MuteRangeAsync
+    (
+      uint startId,
+      uint endId
+    );
+
+    /// <summary>
+    /// Unmute the enumerable of all <typeparamref name="TDeviceModel"/>(s).
+    /// </summary>
+    /// <returns>True/false result.</returns>
+    IAsyncEnumerable<bool> UnmuteAllAsync();
+
+    /// <summary>
+    /// Unmute an enumerable of some <typeparamref name="TDeviceModel"/>(s).
+    /// </summary>
+    /// <param name="idEnumerable">The enumerable of ID(s)</param>
+    /// <returns>True/false result.</returns>
+    IAsyncEnumerable<bool> UnmuteRangeAsync
+    (IEnumerable<uint> idEnumerable);
+
+    /// <summary>
+    /// Unmute an enumerable of some <typeparamref name="TDeviceModel"/>(s).
+    /// </summary>
+    /// <param name="startId">The first ID</param>
+    /// <param name="endId">The last ID</param>
+    /// <returns>True/false result</returns>
+    IAsyncEnumerable<bool> UnmuteRangeAsync
+    (
+      uint startId,
+      uint endId
+    );
+
+    /// <summary>
+    /// Mute a <typeparamref name="TDeviceModel"/>.
+    /// </summary>
+    /// <param name="id">The ID</param>
+    /// <returns>True/false result.</returns>
+    Task<bool> MuteAsync(uint id);
+
+    /// <summary>
+    /// Set the audio device volume.
+    /// </summary>
+    /// <param name="id">The ID</param>
+    /// <param name="volume">The audio volume</param>
+    /// <returns>True/false result.</returns>
+    /// <returns>True/false result.</returns>
+    Task<bool> SetVolumeAsync
+    (
+      string id,
+      double? volume
+    );
+
+    /// <summary>
+    /// Set the <typeparamref name="TDeviceModel"/> as default.
+    /// </summary>
+    /// <param name="id">The ID</param>
+    /// <returns>True/false result.</returns>
+    Task<bool> SetAsDefaultAsync(uint id);
+
+    /// <summary>
+    /// Set the <typeparamref name="TDeviceModel"/> as default for
+    /// communications.
+    /// </summary>
+    /// <param name="id">The ID</param>
+    /// <returns>True/false result.</returns>
+    Task<bool> SetAsDefaultCommunicationsAsync(uint id);
+
+    /// <summary>
+    /// Set the <typeparamref name="TDeviceModel"/> volume.
+    /// </summary>
+    /// <param name="id">The ID</param>
+    /// <param name="volume">The audio volume</param>
+    /// <returns>True/false result.</returns>
+    Task<bool> SetVolumeAsync
+    (
+      uint id,
+      double? volume
+    );
+
+    /// <summary>
+    /// Unmute a <typeparamref name="TDeviceModel"/>.
+    /// </summary>
+    /// <param name="id">The ID</param>
+    /// <returns>True/false result.</returns>
+    Task<bool> UnmuteAsync(uint id);
+
+    /// <summary>
+    /// Update the enumerable of all <typeparamref name="TDeviceModel"/>(s).
+    /// </summary>
+    /// <returns>True/false result.</returns>
+    Task<bool> UpdateAllAsync();
+
+    /// <summary>
+    /// Update the service.
+    /// </summary>
+    /// <returns>True/false result.</returns>
+    Task<bool> UpdateServiceAsync();
+
+    /// <summary>
     /// Get the default communications <typeparamref name="TDeviceModel"/>.
     /// </summary>
     /// <param name="isInput">True/false is an input</param>
     /// <param name="isOutput">True/false is an output</param>
-    /// <returns>The audio device.</returns>
+    /// <returns>The item.</returns>
     Task<TDeviceModel?> GetDefaultCommunicationsAsync
     (
       bool isInput,
@@ -27,8 +164,8 @@
     /// </summary>
     /// <param name="isInput">True/false is an input</param>
     /// <param name="isOutput">True/false is an output</param>
-    /// <returns>The audio device.</returns>
-    Task<TDeviceModel?> GetDefaultConsole
+    /// <returns>The item.</returns>
+    Task<TDeviceModel?> GetDefaultConsoleAsync
     (
       bool isInput,
       bool isOutput
@@ -39,222 +176,12 @@
     /// </summary>
     /// <param name="isInput">True/false is an input</param>
     /// <param name="isOutput">True/false is an output</param>
-    /// <returns>The audio device.</returns>
-    Task<TDeviceModel?> GetDefaultMultimedia
+    /// <returns>The item.</returns>
+    Task<TDeviceModel?> GetDefaultMultimediaAsync
     (
       bool isInput,
       bool isOutput
     );
-
-    /// <summary>
-    /// Mute a <typeparamref name="TDeviceModel"/>.
-    /// </summary>
-    /// <param name="id">The ID</param>
-    /// <returns>True/false result.</returns>
-    Task<bool> Mute(uint id);
-
-    /// <summary>
-    /// Mute the enumerable of all <typeparamref name="TDeviceModel"/>(s).
-    /// </summary>
-    /// <returns>True/false result.</returns>
-    IAsyncEnumerable<bool> MuteAll();
-
-    /// <summary>
-    /// Mute an enumerable of some <typeparamref name="TDeviceModel"/>(s).
-    /// </summary>
-    /// <param name="startId">The first ID</param>
-    /// <param name="endId">The last ID</param>
-    /// <returns>True/false result.</returns>
-    IAsyncEnumerable<bool> MuteRange
-    (
-      uint startId,
-      uint endId
-    );
-
-    /// <summary>
-    /// Restart a <typeparamref name="TDeviceModel"/>.
-    /// </summary>
-    /// <param name="id">The ID</param>
-    /// <returns>True/false result.</returns>
-    Task<bool> Restart(uint id);
-
-    /// <summary>
-    /// Restart the enumerable of all <typeparamref name="TDeviceModel"/>(s).
-    /// </summary>
-    /// <returns>True/false result.</returns>
-    IAsyncEnumerable<bool> RestartAll();
-
-    /// <summary>
-    /// Restart an enumerable of some <typeparamref name="TDeviceModel"/>(s).
-    /// </summary>
-    /// <param name="startId">The first ID</param>
-    /// <param name="endId">The last ID</param>
-    /// <returns>True/false result.</returns>
-    IAsyncEnumerable<bool> RestartRange
-    (
-      uint startId,
-      uint endId
-    );
-
-    /// <summary>
-    /// Restart some <typeparamref name="TDeviceModel"/>(s).
-    /// </summary>
-    /// <param name="idList">The enumerable of ID(s)</param>
-    /// <returns>True/false result.</returns>
-    IAsyncEnumerable<bool> RestartRange(List<uint> idList);
-
-    /// <summary>
-    /// Set a <typeparamref name="TDeviceModel"/> as the default.
-    /// </summary>
-    /// <param name="id">The ID</param>
-    /// <returns>True/false result.</returns>
-    Task<bool> SetAsDefaultAsync(uint id);
-
-    /// <summary>
-    /// Set a <typeparamref name="TDeviceModel"/> as the default communications.
-    /// </summary>
-    /// <param name="id">The ID</param>
-    /// <returns>True/false result.</returns>
-    Task<bool> SetAsDefaultCommunicationsAsync(uint id);
-
-    /// <summary>
-    /// Set the audio device volume.
-    /// </summary>
-    /// <param name="id">The ID</param>
-    /// <param name="volume">The audio volume</param>
-    /// <returns>True/false result.</returns>
-    /// <returns>True/false result.</returns>
-    Task<bool> SetVolume
-    (
-      string id,
-      double? volume
-    );
-
-    /// <summary>
-    /// Restart a <typeparamref name="TDeviceModel"/>.
-    /// </summary>
-    /// <param name="id">The ID</param>
-    /// <returns>True/false result.</returns>
-    Task<bool> Start(uint id);
-
-    /// <summary>
-    /// Start the enumerable of all <typeparamref name="TDeviceModel"/>(s).
-    /// </summary>
-    /// <returns>True/false result.</returns>
-    IAsyncEnumerable<bool> StartAll();
-
-    /// <summary>
-    /// Start an enumerable of some <typeparamref name="TDeviceModel"/>(s).
-    /// </summary>
-    /// <param name="startId">The first ID</param>
-    /// <param name="endId">The last ID</param>
-    /// <returns>True/false result.</returns>
-    IAsyncEnumerable<bool> StartRange
-    (
-      uint startId,
-      uint endId
-    );
-
-    /// <summary>
-    /// Start an enumerable of some <typeparamref name="TDeviceModel"/>(s).
-    /// </summary>
-    /// <param name="idList">The enumerable of ID(s)</param>
-    IAsyncEnumerable<bool> StartRange(List<uint> idList);
-
-    /// <summary>
-    /// Stop a <typeparamref name="TDeviceModel"/>.
-    /// </summary>
-    /// <param name="id">The ID</param>
-    /// <returns>True/false result.</returns>
-    Task<bool> StopAsync(uint id);
-
-    /// <summary>
-    /// Stop the enumerable of all <typeparamref name="TDeviceModel"/>(s).
-    /// </summary>
-    /// <returns>True/false result.</returns>
-    Task<bool> StopAll();
-
-    /// <summary>
-    /// Stop an enumerable of some <typeparamref name="TDeviceModel"/>(s).
-    /// </summary>
-    /// <param name="startId">The first ID</param>
-    /// <param name="endId">The last ID</param>
-    /// <returns>True/false result.</returns>
-    IAsyncEnumerable<bool> StopRange
-    (
-      uint startId,
-      uint endId
-    );
-
-    /// <summary>
-    /// Stop an enumerable of some <typeparamref name="TDeviceModel"/>(s).
-    /// </summary>
-    /// <param name="idList">The enumerable of ID(s)</param>
-    /// <returns>True/false result.</returns>
-    IAsyncEnumerable<bool> StopRange(List<uint> idList);
-
-    /// <summary>
-    /// Unmute a <typeparamref name="TDeviceModel"/>.
-    /// </summary>
-    /// <param name="id">The ID</param>
-    /// <returns>True/false result.</returns>
-    Task<bool> UnmuteAsync(uint id);
-
-    /// <summary>
-    /// Unmute the enumerable of all <typeparamref name="TDeviceModel"/>(s).
-    /// </summary>
-    /// <returns>True/false result.</returns>
-    Task<bool> UnmuteAll();
-
-    /// <summary>
-    /// Unmute some <typeparamref name="TDeviceModel"/>(s).
-    /// </summary>
-    /// <param name="startId">The first ID</param>
-    /// <param name="endId">The last ID</param>
-    /// <returns>True/false result.</returns>
-    Task<bool> UnmuteRange
-    (
-      uint startId,
-      uint endId
-    );
-
-    /// <summary>
-    /// Update a <typeparamref name="TDeviceModel"/>.
-    /// </summary>
-    /// <param name="id">The ID</param>
-    /// <returns>True/false result.</returns>
-    Task<bool> Update(uint id);
-
-    /// <summary>
-    /// Update the enumerable of all <typeparamref name="TDeviceModel"/>(s).
-    /// </summary>
-    /// <returns>True/false result.</returns>
-    Task<bool> UpdateAll();
-
-    /// <summary>
-    /// Update an enumerable of some <typeparamref name="TDeviceModel"/>(s).
-    /// </summary>
-    /// <param name="startId">The first ID</param>
-    /// <param name="endId">The last ID</param>
-    /// <returns>True/false result.</returns>
-    IAsyncEnumerable<bool> UpdateRange
-    (
-      uint startId,
-      uint endId
-    );
-
-    /// <summary>
-    /// Update an enumerable of some <typeparamref name="TDeviceModel"/>(s).
-    /// </summary>
-    /// <param name="idList">The enumerable of ID(s)</param>
-    /// <returns>True/false result.</returns>
-    IAsyncEnumerable<bool> UpdateRange(List<uint> idList);
-
-    /// <summary>
-    /// Update the service.
-    /// </summary>
-    /// <returns>True/false result.</returns>
-    Task<bool> UpdateServiceAsync();
 
     #endregion
   }
