3fa01fcce9094669606dd60724a4b3f0c271a82b
portellam <amportell.13@gmail.com>
2025-02-20 02:00:14 -0500

    Will carry over logic from DeviceService to DeviceGroupService.

--- VACARM.Backend/VACARM.Infrastructure/Services/DeviceGroupService.cs
+++ VACARM.Backend/VACARM.Infrastructure/Services/DeviceGroupService.cs
@@ -1,10 +1,11 @@
 ﻿#warning AudioSwitcher.AudioApi must initialize after NAudio.CoreAudioApi
 
-using NAudio.CoreAudioApi;
 using AudioSwitcher.AudioApi;
+using NAudio.CoreAudioApi;
+using System.Diagnostics.CodeAnalysis;
 using VACARM.Domain.Models;
 using VACARM.Infrastructure.Repositories;
-using System.Diagnostics.CodeAnalysis;
+using VACARM.Infrastructure.Services;
 
 namespace VACARM.Application.Services
 {
@@ -18,29 +19,22 @@ namespace VACARM.Application.Services
   /// </summary>
   public partial class DeviceGroupService
     <
-      TParentRepository,
-      TRepository,
+      TServiceRepository,
+      TBaseService,
+      TBaseRepository,
       TDeviceModel
     > :
-    IDeviceGroupService
+    BaseGroupService
     <
-      GroupService
+      ReadonlyRepository
       <
-        DeviceService
+        BaseService
         <
           BaseRepository<TDeviceModel>,
           TDeviceModel
-        >,
-        BaseRepository<TDeviceModel>,
-        TDeviceModel
+        >
       >,
-      BaseRepository<TDeviceModel>,
-      TDeviceModel
-    >
-    where TParentRepository :
-    GroupService
-    <
-      DeviceService
+      BaseService
       <
         BaseRepository<TDeviceModel>,
         TDeviceModel
@@ -48,58 +42,33 @@ namespace VACARM.Application.Services
       BaseRepository<TDeviceModel>,
       TDeviceModel
     >
-    where TRepository :
-    DeviceService
+    where TServiceRepository :
+    ReadonlyRepository
+    <
+      BaseService
+      <
+        BaseRepository<TDeviceModel>,
+        TDeviceModel
+      >
+    >
+    where TBaseService :
+    BaseService
     <
       BaseRepository<TDeviceModel>,
       TDeviceModel
     >
+    where TBaseRepository :
+    BaseRepository<TDeviceModel>
     where TDeviceModel :
     DeviceModel
   {
     #region Parameters
-
-    internal readonly int SafeMaxRepositoryCount = 3;                                   // TODO: test, and change me!
-    internal uint SelectedId { get; set; } = uint.MinValue;
-
-    private ReadonlyRepository<TDeviceModel> repositoryRepository
-    { get; set; }
 
     private CoreAudioService<ReadonlyRepository<Device>, Device> coreAudioService
     { get; set; }
 
     private MMDeviceService<ReadonlyRepository<MMDevice>, MMDevice> mMDeviceService
     { get; set; }
-
-    private Func<BaseRepository<DeviceModel>, bool> IsSelectedId
-    {
-      get
-      {
-        return (BaseRepository<DeviceModel> x) => x. == this.SelectedId;
-      }
-    }
-
-    protected ReadonlyRepository<TDeviceModel> RepositoryRepository
-    {
-      get
-      {
-        return base.Repository;
-      }
-      private set
-      {
-        base.Repository = value;
-        this.OnPropertyChanged(nameof(RepositoryRepository));
-      }
-    }
-
-    protected new BaseRepository<DeviceModel> SelectedRepository
-    {
-      get
-      {
-        return this.RepositoryRepository
-          .Get(IsSelectedId);
-      }
-    }
 
     /// <summary>
     /// <typeparamref name="AudioSwitcher.AudioApi"/> must declare after
@@ -148,17 +117,8 @@ namespace VACARM.Application.Services
     public DeviceGroupService() :
       base()
     {
-      this.
-
-      Dictionary<int, BaseRepository<DeviceModel>> Dictionary =
-        new Dictionary<int, BaseRepository<DeviceModel>>();
-
-      Dictionary.Add(0, new BaseRepository<DeviceModel>());
-
-      this.RepositoryRepository = new ReadonlyRepository<TDeviceModel>
-        (
-          Dictionary
-        );
+      this.BaseServiceList =
+        new List<BaseService<BaseRepository<TDeviceModel>, TDeviceModel>>();
 
       this.MMDeviceService =
         new MMDeviceService<ReadonlyRepository<MMDevice>, MMDevice>();
@@ -170,18 +130,24 @@ namespace VACARM.Application.Services
     /// <summary>
     /// Constructor
     /// </summary>
-    /// <param name="repositoryRepository">The repository of repositories</param>
+    /// <param name="baseServiceList">The list of service(s)</param>
+    /// <param name="maxCount">The maximum count of service(s)</param>
     /// <param name="mMDeviceService">The MMDevice service</param>
     /// <param name="coreAudioService">The Core Audio service</param>
     public DeviceGroupService
     (
-      BaseRepository<TDeviceModel> repositoryRepository,
+      List<BaseService<BaseRepository<TDeviceModel>, TDeviceModel>> baseServiceList,
+      int maxCount,
       MMDeviceService<ReadonlyRepository<MMDevice>, MMDevice> mMDeviceService,
       CoreAudioService<ReadonlyRepository<Device>, Device> coreAudioService
     ) :
-      base(repositoryRepository)
+      base
+      (
+        baseServiceList,
+        maxCount
+      )
     {
-      this.Repository = repositoryRepository;
+      this.BaseServiceList = baseServiceList;
       this.MMDeviceService = mMDeviceService;
       this.CoreAudioService = coreAudioService;
     }
@@ -195,8 +161,7 @@ namespace VACARM.Application.Services
 
       if (isDisposed)
       {
-        this.Repository
-          .Dispose();
+        this.Dispose();
 
         this.CoreAudioService
           .Dispose();
