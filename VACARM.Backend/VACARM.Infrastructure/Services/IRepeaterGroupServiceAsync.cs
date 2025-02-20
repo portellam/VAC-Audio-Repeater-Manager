a0c5bac9c6848bf6d9cdf6e5647a128ed8380baf
portellam <amportell.13@gmail.com>
2025-02-20 05:29:36 -0500

    Sorted logic.

--- VACARM.Backend/VACARM.Infrastructure/Services/IRepeaterGroupServiceAsync.cs
+++ VACARM.Backend/VACARM.Infrastructure/Services/IRepeaterGroupServiceAsync.cs
@@ -23,9 +23,27 @@
     Task<int?> RestartAsync(uint? id);
 
     /// <summary>
+    /// Restart a <typeparamref name="TRepeaterModel"/>.
+    /// </summary>
+    /// <param name="id">The ID</param>
+    Task<int?> StartAsync(uint? id);
+
+    /// <summary>
+    /// Stop a <typeparamref name="TRepeaterModel"/>.
+    /// </summary>
+    /// <param name="id">The ID</param>
+    Task<int?> StopAsync(uint? id);
+
+    /// <summary>
     /// Restart all <typeparamref name="TRepeaterModel"/>(s).
     /// </summary>
     IAsyncEnumerable<int?> RestartAllAsync();
+
+    /// <summary>
+    /// Restart some <typeparamref name="TRepeaterModel"/>(s).
+    /// </summary>
+    /// <param name="idEnumerable">The enumerable of ID(s)</param>
+    IAsyncEnumerable<int?> RestartRangeAsync(IEnumerable<uint> idEnumerable);
 
     /// <summary>
     /// Restart some <typeparamref name="TRepeaterModel"/>(s).
@@ -39,21 +57,16 @@
     );
 
     /// <summary>
-    /// Restart some <typeparamref name="TRepeaterModel"/>(s).
-    /// </summary>
-    /// <param name="idEnumerable">The enumerable of ID(s)</param>
-    IAsyncEnumerable<int?> RestartRangeAsync(IEnumerable<uint> idEnumerable);
-
-    /// <summary>
-    /// Restart a <typeparamref name="TRepeaterModel"/>.
-    /// </summary>
-    /// <param name="id">The ID</param>
-    Task<int?> StartAsync(uint? id);
-
-    /// <summary>
     /// Start all <typeparamref name="TRepeaterModel"/>(s).
     /// </summary>
     IAsyncEnumerable<int?> StartAllAsync();
+
+    /// <summary>
+    /// Start some <typeparamref name="TRepeaterModel"/>(s).
+    /// </summary>
+    /// <param name="idEnumerable">The enumerable of ID(s)</param>
+    IAsyncEnumerable<int?> StartRangeAsync
+    (IEnumerable<uint> idEnumerable);
 
     /// <summary>
     /// Start some <typeparamref name="TRepeaterModel"/>(s).
@@ -67,22 +80,15 @@
     );
 
     /// <summary>
-    /// Start some <typeparamref name="TRepeaterModel"/>(s).
-    /// </summary>
-    /// <param name="idEnumerable">The enumerable of ID(s)</param>
-    IAsyncEnumerable<int?> StartRangeAsync
-    (IEnumerable<uint> idEnumerable);
-
-    /// <summary>
-    /// Stop a <typeparamref name="TRepeaterModel"/>.
-    /// </summary>
-    /// <param name="id">The ID</param>
-    Task<int?> StopAsync(uint? id);
-
-    /// <summary>
     /// Stop all <typeparamref name="TRepeaterModel"/>(s).
     /// </summary>
     IAsyncEnumerable<int?> StopAllAsync();
+
+    /// <summary>
+    /// Stop some <typeparamref name="TRepeaterModel"/>(s).
+    /// </summary>
+    /// <param name="idEnumerable">The enumerable of ID(s)</param>
+    IAsyncEnumerable<int?> StopRangeAsync(IEnumerable<uint> idEnumerable);
 
     /// <summary>
     /// Stop some <typeparamref name="TRepeaterModel"/>(s).
@@ -94,12 +100,6 @@
       uint startId,
       uint endId
     );
-
-    /// <summary>
-    /// Stop some <typeparamref name="TRepeaterModel"/>(s).
-    /// </summary>
-    /// <param name="idEnumerable">The enumerable of ID(s)</param>
-    IAsyncEnumerable<int?> StopRangeAsync(IEnumerable<uint> idEnumerable);
 
     #endregion
   }
