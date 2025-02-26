36b666b5329d619680aba425e67c4e0cafd38817
portellam <amportell.13@gmail.com>
2025-02-26 13:44:58 -0500

    Will not predefine company in shared assembly file.

--- VACARM.Assembly/Assembly.cs
+++ VACARM.Assembly/Assembly.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Reflection;
 
-[assembly: AssemblyCompany("Alexander Portell &lt;github.com/portellam&gt;")]
 [assembly: AssemblyCopyright("\"Audio Repeater Manager\" Copyleft 2025.")]
 [assembly: AssemblyMetadataAttribute("RepositoryUrl", "https://github.com/portellam/VAC-Audio-Repeater-Manager")]
