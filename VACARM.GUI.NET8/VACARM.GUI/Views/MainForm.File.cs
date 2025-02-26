f060ec7f7750782f71c27951bf02935d8604b047
portellam <amportell.13@gmail.com>
2025-02-26 02:15:24 -0500

    Moved file logic.

--- VACARM.GUI.NET8/VACARM.GUI/Views/MainForm.File.cs
+++ VACARM.GUI.NET8/VACARM.GUI/Views/MainForm.File.cs
@@ -16,7 +16,126 @@
 
     #region Interaction Logic
 
+    private void fileCloseAllToolStripMenuItem_Click
+    (
+      object sender,
+      EventArgs eventArgs
+    )
+    {
 
+    }
+
+    private void fileCloseMultipleToolStripMenuItem_Click
+    (
+      object sender,
+      EventArgs eventArgs
+    )
+    {
+
+    }
+
+    private void fileCloseToolStripMenuItem_Click
+    (
+      object sender,
+      EventArgs eventArgs
+    )
+    {
+
+    }
+
+    private void fileExitToolStripMenuItem_Click
+    (
+      object sender,
+      EventArgs eventArgs
+    )
+    {
+      // check if it is safe to exit.
+
+      Environment.Exit(0);
+    }
+
+    private void fileNewToolStripMenuItem_Click
+    (
+      object sender,
+      EventArgs eventArgs
+    )
+    {
+
+    }
+
+    private void fileOpenToolStripMenuItem_Click
+    (
+      object sender,
+      EventArgs eventArgs
+    )
+    {
+      OpenFileDialog openFileDialog = new OpenFileDialog()
+      {
+        AddExtension = true,
+        DefaultExt = ".vacarm",
+        CheckFileExists = true,
+        CheckPathExists = true,
+        InitialDirectory = "C:\\",
+        Multiselect = true,
+        OkRequiresInteraction = true,
+        ShowPreview = true,
+
+      };
+
+      openFileDialog.ShowDialog();
+
+      string fileName = openFileDialog.FileName;
+      //send to FileController?
+      //file controller populates an instance of the repositories?
+
+      openFileDialog.AddToRecent = true;
+      openFileDialog.ShowPreview = true;
+    }
+
+    private void fileOpenContainingFolderToolStripMenuItem_Click
+    (
+      object sender,
+      EventArgs eventArgs
+    )
+    {
+
+    }
+
+    private void fileSaveToolStripMenuItem_Click
+    (
+      object sender,
+      EventArgs eventArgs
+    )
+    {
+
+    }
+
+    private void fileSaveAsToolStripMenuItem_Click
+    (
+      object sender,
+      EventArgs eventArgs
+    )
+    {
+
+    }
+
+    private void fileSaveACopyAsToolStripMenuItem_Click
+    (
+      object sender,
+      EventArgs eventArgs
+    )
+    {
+
+    }
+
+    private void fileSaveAllToolStripMenuItem_Click
+    (
+      object sender,
+      EventArgs eventArgs
+    )
+    {
+
+    }
 
     #endregion
   }
