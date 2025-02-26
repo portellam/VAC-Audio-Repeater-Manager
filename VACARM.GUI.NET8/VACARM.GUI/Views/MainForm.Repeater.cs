e6d28abfb899f367465216dd07e980141091ad36
portellam <amportell.13@gmail.com>
2025-02-26 02:16:45 -0500

    Moved repeater and view logic.

--- VACARM.GUI.NET8/VACARM.GUI/Views/MainForm.Repeater.cs
+++ VACARM.GUI.NET8/VACARM.GUI/Views/MainForm.Repeater.cs
@@ -16,7 +16,185 @@
 
     #region Interaction Logic
 
+    private void repeaterExportToClipboardToolStripMenuItem_Click
+    (
+      object sender,
+      EventArgs eventArgs
+    )
+    {
 
+    }
+
+    private void repeaterExportToScriptToolStripMenuItem_Click
+    (
+      object sender,
+      EventArgs eventArgs
+    )
+    {
+
+    }
+
+    private void repeaterExportToXMLToolStripMenuItem_Click
+    (
+      object sender,
+      EventArgs eventArgs
+    )
+    {
+
+    }
+
+    private void repeaterImportFromClipboardToolStripMenuItem_Click
+    (
+      object sender,
+      EventArgs eventArgs
+    )
+    {
+
+    }
+
+    private void repeaterImportFromScriptToolStripMenuItem_Click
+    (
+      object sender,
+      EventArgs eventArgs
+    )
+    {
+
+    }
+
+    private void repeaterImportFromXMLToolStripMenuItem_Click
+    (
+      object sender,
+      EventArgs eventArgs
+    )
+    {
+
+    }
+
+    private void repeaterRedoToolStripMenuItem_Click
+    (
+      object sender,
+      EventArgs eventArgs
+    )
+    {
+
+    }
+
+    private void repeaterSelectToolStripMenuItem_Click
+    (
+      object sender,
+      EventArgs eventArgs
+    )
+    {
+
+    }
+
+    private void repeaterSelectAllToolStripMenuItem_Click
+    (
+      object sender,
+      EventArgs eventArgs
+    )
+    {
+
+    }
+
+    private void repeaterUndoToolStripMenuItem_Click
+    (
+      object sender,
+      EventArgs eventArgs
+    )
+    {
+
+    }
+
+    private void repeaterRestartToolStripMenuItem_Click
+    (
+      object sender,
+      EventArgs eventArgs
+    )
+    {
+
+    }
+
+    private void repeaterSelectAllWithAbsentDevicesToolStripMenuItem_Click
+    (
+      object sender,
+      EventArgs eventArgs
+    )
+    {
+
+    }
+
+    private void selectAllWithDisabledDevicesToolStripMenuItem_Click
+    (
+      object sender,
+      EventArgs eventArgs
+    )
+    {
+
+    }
+
+    private void selectAllWithEnabledDevicesToolStripMenuItem_Click
+    (
+      object sender,
+      EventArgs eventArgs
+    )
+    {
+
+    }
+
+    private void repeaterSelectAllWithPresentDevicesToolStripMenuItem_Click
+    (
+      object sender,
+      EventArgs eventArgs
+    )
+    {
+
+    }
+
+    private void repeaterSelectDevicesToolStripMenuItem_Click
+    (
+      object sender,
+      EventArgs eventArgs
+    )
+    {
+
+    }
+
+    private void repeaterSelectInputDeviceToolStripMenuItem_Click
+    (
+      object sender,
+      EventArgs eventArgs
+    )
+    {
+
+    }
+
+    private void repeaterSelectOutputDeviceToolStripMenuItem_Click
+    (
+      object sender,
+      EventArgs eventArgs
+    )
+    {
+
+    }
+
+    private void repeaterStartToolStripMenuItem_Click
+    (
+      object sender,
+      EventArgs eventArgs
+    )
+    {
+
+    }
+
+    private void repeaterStopToolStripMenuItem_Click
+    (
+      object sender,
+      EventArgs eventArgs
+    )
+    {
+
+    }
 
     #endregion
   }
--- VACARM.GUI.NET8/VACARM.GUI/Views/MainForm.View.cs
+++ VACARM.GUI.NET8/VACARM.GUI/Views/MainForm.View.cs
@@ -16,7 +16,50 @@
 
     #region Interaction Logic
 
+    private void viewAlwaysOnTopToolStripMenuItem_Click
+    (
+      object sender,
+      EventArgs eventArgs
+    )
+    {
 
+    }
+    private void viewPreferSystemThemeToolStripMenuItem_Click
+    (
+      object sender,
+      EventArgs eventArgs
+    )
+    {
+      if (sender is null)
+      {
+        return;
+      }
+
+      if (Environment.OSVersion.Version.Major < 6)
+      {
+        viewPreferSystemThemeToolStripMenuItem.Enabled = false;
+      }
+
+      preferSystemTheme = true;
+    }
+
+    private void viewPreferDarkThemeToolStripMenuItem_Click
+    (
+      object sender,
+      EventArgs eventArgs
+    )
+    {
+
+    }
+
+    private void viewToggleFullScreenModeToolStripMenuItem_Click
+    (
+      object sender,
+      EventArgs eventArgs
+    )
+    {
+
+    }
 
     #endregion
   }
