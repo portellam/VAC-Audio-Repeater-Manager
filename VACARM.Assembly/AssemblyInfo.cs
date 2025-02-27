3f095d3e73d340c3a69317dc5508c9b172c0ec37
portellam <amportell.13@gmail.com>
2025-02-27 03:59:45 -0500

    Rename.

--- VACARM.Assembly/AssemblyInfo.cs
+++ VACARM.Assembly/AssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System;
 using System.Reflection;
 
-[assembly: AssemblyCopyright("\"Audio Repeater Manager\" Copyleft <#=DateTime.Now.Year#>.")]
+[assembly: AssemblyCopyright("\"Audio Repeater Manager\" Copyleft 2025.")]
 [assembly: AssemblyMetadataAttribute("RepositoryUrl", "https://github.com/portellam/VAC-Audio-Repeater-Manager")]
