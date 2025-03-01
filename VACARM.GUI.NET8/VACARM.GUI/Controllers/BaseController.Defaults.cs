dcb1dc490a9dc460a96196c64b7e4e3bf1ca134c
portellam <amportell.13@gmail.com>
2025-03-01 07:08:04 -0500

    Update BaseController.Defaults.cs

--- VACARM.GUI.NET8/VACARM.GUI/Controllers/BaseController.Defaults.cs
+++ VACARM.GUI.NET8/VACARM.GUI/Controllers/BaseController.Defaults.cs
@@ -11,34 +11,33 @@
     internal static ToolStripMenuItem DefaultToolStripMenuItem { get; set; } =
       new ToolStripMenuItem()
       {
+        AutoToolTip = false,
         CheckOnClick = true,
         DisplayStyle = ToolStripItemDisplayStyle.Text,
+        Size = DefaultSize
       };
 
     internal static ToolStripMenuItem DefaultSelectToolStripMenuItem
     { get; set; } = new ToolStripMenuItem()
     {
-      CheckOnClick = true,
-      DisplayStyle = ToolStripItemDisplayStyle.Text,
-      Name = SelectString
+      AutoToolTip = DefaultToolStripMenuItem.AutoToolTip,
+      CheckOnClick = DefaultToolStripMenuItem.CheckOnClick,
+      DisplayStyle = DefaultToolStripMenuItem.DisplayStyle,
+      Name = SelectString,
+      Size = DefaultToolStripMenuItem.Size
     };
 
     internal static ToolStripMenuItem DefaultSelectRangeToolStripMenuItem
     { get; set; } = new ToolStripMenuItem()
     {
-      CheckOnClick = true,
-      DisplayStyle = ToolStripItemDisplayStyle.Text,
-      Name = SelectRangeString
+      AutoToolTip = DefaultToolStripMenuItem.AutoToolTip,
+      CheckOnClick = DefaultToolStripMenuItem.CheckOnClick,
+      DisplayStyle = DefaultToolStripMenuItem.DisplayStyle,
+      Name = SelectRangeString,
+      Size = DefaultToolStripMenuItem.Size
     };
 
-    private readonly static bool DefaultIsChecked = false;
-    private readonly static string SelectString = "Select";
-    private readonly static string SelectRangeString = string.Format
-      (
-        "{0} {1}",
-        SelectString,
-        "All"
-      );
+    internal static Size DefaultSize { get; set; } = new Size(0, 0);
 
     internal virtual string DefaultName
     {
@@ -48,22 +47,40 @@
       }
     }
 
+    private readonly static string SelectString = "Select";
+    private readonly static string SelectRangeString = string.Format
+      (
+        "{0} {1}",
+        SelectString,
+        "All"
+      );
+
     #endregion
 
     #region Logic
 
     private void SetDefaultToolStripMenuItems()
     {
-      DefaultToolStripMenuItem.CheckedChanged += this.CheckedChangedEventHandler;
-      DefaultSelectToolStripMenuItem.CheckedChanged += this.CheckedChangedEventHandler;
+      DefaultToolStripMenuItem.CheckedChanged +=
+        this.CheckedChangedEventHandler;
+
+      DefaultSelectToolStripMenuItem.CheckedChanged +=
+        this.CheckedChangedEventHandler;
 
       DefaultSelectRangeToolStripMenuItem.CheckedChanged +=
         this.RangeCheckedChangedEventHandler
         (
           this.GroupService
-            .SelectedService
-            .GetAllId(),
-          DefaultIsChecked
+            .SelectedRepository
+            .DeselectedIdEnumerable,
+          false
+        )
+        + this.RangeCheckedChangedEventHandler
+        (
+          this.GroupService
+            .SelectedRepository
+            .SelectedIdHashSet,
+          true
         );
     }
 
