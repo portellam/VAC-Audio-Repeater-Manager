cff21dfa19246d4ddfc677994f78ba5e7fdbc2a9
portellam <amportell.13@gmail.com>
2025-02-28 22:53:12 -0500

    Began work on Repeater Controller.

---
+++ VACARM.GUI.NET8/VACARM.GUI/Controllers/RepeaterController.cs
@@ -0,0 +1,92 @@
+﻿using VACARM.Domain.Models;
+using VACARM.Infrastructure.Repositories;
+using VACARM.Infrastructure.Services;
+
+namespace VACARM.GUI.Controllers
+{
+  internal partial class RepeaterController
+  {
+    #region Parameters
+
+    internal RepeaterGroupService
+    <
+      ReadonlyRepository
+      <
+        BaseService
+        <
+          BaseRepository<RepeaterModel>,
+          RepeaterModel
+        >
+      >,
+      BaseService
+      <
+        BaseRepository<RepeaterModel>,
+        RepeaterModel
+      >,
+      BaseRepository<RepeaterModel>,
+      RepeaterModel
+    > GroupService
+    { get; set; }
+
+    #endregion
+
+    #region Logic
+
+    internal EventHandler? SelectAllPropertyCheckedChangedEventHandler
+    (
+      ref IEnumerable<uint> propertyIdEnumerable,
+      bool isChecked
+    )
+    {
+      if (propertyIdEnumerable == null)
+      {
+        return null;
+      }
+
+      var enumerable = this.GetToolStripMenuItemEnumerable
+        (
+          this.ToolStrip,
+          propertyIdEnumerable
+        );
+
+      return
+        (
+          sender,
+          eventArgs
+        ) =>
+        {
+          foreach (var item in enumerable)
+          {
+            item.Checked = isChecked;
+          }
+        };
+    }
+
+    
+
+    internal ToolStripItemCollection GetToolStripItemCollection
+    (
+      ref ToolStripMenuItem propertyToolStripMenuItem,
+      IEnumerable<uint> propertyIdEnumerable
+    )
+    {
+
+    }
+
+
+    internal void SetToolStripItemCollectionProperties
+    (
+      ref ToolStripItemCollection toolStripItemCollection
+    )
+    {
+      if (toolStripItemCollection == null)
+      {
+        yield break;
+      }
+
+      var enumerable = this.Get
+    }
+
+    #endregion
+  }
+}
