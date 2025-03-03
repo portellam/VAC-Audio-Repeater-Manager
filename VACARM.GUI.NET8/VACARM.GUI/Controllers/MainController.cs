ce7ca6168f14c3c5f9a5fc5aae13ec67889949f9
portellam <amportell.13@gmail.com>
2025-03-02 22:00:46 -0500

    Began work on MainController. Added arguments.

---
+++ VACARM.GUI.NET8/VACARM.GUI/Controllers/MainController.cs
@@ -0,0 +1,75 @@
+﻿using VACARM.Domain.Models;
+using VACARM.GUI.Views;
+using VACARM.Infrastructure.Repositories;
+using VACARM.Infrastructure.Services;
+
+namespace VACARM.GUI.Controllers
+{
+  internal partial class MainController :
+    IDisposable
+  {
+    #region Parameters
+
+    internal DeviceController
+      <
+        DeviceGroupService
+        <
+          ReadonlyRepository
+          <
+            BaseService
+            <
+              BaseRepository<DeviceModel>,
+              DeviceModel
+            >
+          >,
+          BaseService
+          <
+            BaseRepository<DeviceModel>,
+            DeviceModel
+          >,
+          BaseRepository<DeviceModel>,
+          DeviceModel
+        >,
+        DeviceModel
+      >
+    DeviceController
+    { get; set; }
+
+    internal RepeaterController
+      <
+        RepeaterGroupService
+        <
+          ReadonlyRepository
+          <
+            BaseService
+            <
+              BaseRepository<RepeaterModel>,
+              RepeaterModel
+            >
+          >,
+          BaseService
+          <
+            BaseRepository<RepeaterModel>,
+            RepeaterModel
+          >,
+          BaseRepository<RepeaterModel>,
+          RepeaterModel
+        >,
+        RepeaterModel
+      >
+    RepeaterController
+    { get; set; }
+
+    internal AboutForm AboutForm { get; set; }
+    internal DeviceFindForm DeviceFindForm { get; set; }
+    internal MainForm MainForm { get; set; }
+
+    #endregion
+
+    #region Logic
+
+    internal MainController(string[] arguments)
+
+    #endregion
+  }
+}
