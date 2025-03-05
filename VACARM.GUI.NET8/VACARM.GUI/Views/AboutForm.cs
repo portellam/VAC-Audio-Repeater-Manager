beed50e99e76023ed9516045268ce0f5c5df72a0
portellam <amportell.13@gmail.com>
2025-03-05 05:29:11 -0500

    Refactor and formatting.

--- VACARM.GUI.NET8/VACARM.GUI/Views/AboutForm.cs
+++ VACARM.GUI.NET8/VACARM.GUI/Views/AboutForm.cs
@@ -1,5 +1,4 @@
 ﻿using VACARM.GUI.Accessors;
-using VACARM.Common;
 using System.Diagnostics.CodeAnalysis;
 
 namespace VACARM.GUI.Views
@@ -9,50 +8,58 @@ namespace VACARM.GUI.Views
   {
     #region Logic
 
-    /// <summary>
-    /// Constructor
-    /// </summary>
-    [ExcludeFromCodeCoverage]
-    public AboutForm()
-    {
-      InitializeComponent();
-      SetComponentsNameProperties();
-      SetComponentsTextProperties();
-    }
-
     private void SetComponentsNameProperties()
     {
-      labelCompanyName.Name = nameof(this.labelCompanyName);
-      labelCopyright.Name = nameof(this.labelCopyright);
-      labelProductName.Name = nameof(this.labelProductName);
-      labelVersion.Name = nameof(this.labelVersion);
-      tableLayoutPanel.Name = nameof(this.tableLayoutPanel);
-      logoPictureBox.Name = nameof(this.logoPictureBox);
-      textBoxDescription.Name = nameof(this.textBoxDescription);
-      okButton.Name = nameof(this.okButton);
+      this.labelCompanyName.Name = nameof(this.labelCompanyName);
+      this.labelCopyright.Name = nameof(this.labelCopyright);
+      this.labelProductName.Name = nameof(this.labelProductName);
+      this.labelVersion.Name = nameof(this.labelVersion);
+      this.tableLayoutPanel.Name = nameof(this.tableLayoutPanel);
+      this.logoPictureBox.Name = nameof(this.logoPictureBox);
+      this.textBoxDescription.Name = nameof(this.textBoxDescription);
+      this.okButton.Name = nameof(this.okButton);
     }
 
     private void SetComponentsTextProperties()
     {
-      this.labelCopyright.Text = AssemblyInformationAccessor.AssemblyCopyright;
-      this.labelCompanyName.Text = AssemblyInformationAccessor.AssemblyCompany;
-      this.labelProductName.Text = AssemblyInformationAccessor.AssemblyProduct;
+      this.labelCopyright
+        .Text = AssemblyInformationAccessor.AssemblyCopyright;
 
-      this.labelVersion.Text = string.Format
+      this.labelCompanyName
+        .Text = AssemblyInformationAccessor.AssemblyCompany;
+
+      this.labelProductName
+        .Text = AssemblyInformationAccessor.AssemblyProduct;
+
+      this.labelVersion
+        .Text = string.Format
         (
-          "Version {0}",
+          this.labelVersion
+            .Text,
           AssemblyInformationAccessor.AssemblyVersion
         );
 
       this.Text = string
         .Format
         (
-          "About {0}",
-          Common.Info.ApplicationPartialAbbreviatedName
+          this.Text,
+          Common.Info
+            .ApplicationPartialAbbreviatedName
         );
 
-      this.textBoxDescription.Text =
-        AssemblyInformationAccessor.AssemblyDescription;
+      this.textBoxDescription
+        .Text = AssemblyInformationAccessor.AssemblyDescription;
+    }
+
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    [ExcludeFromCodeCoverage]
+    public AboutForm()
+    {
+      this.InitializeComponent();
+      this.SetComponentsNameProperties();
+      this.SetComponentsTextProperties();
     }
 
     #endregion
