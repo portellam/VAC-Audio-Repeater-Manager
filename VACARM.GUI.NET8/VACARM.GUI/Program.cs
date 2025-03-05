0fda1d951176794e2bbeec2c801f8097c9f0d110
portellam <amportell.13@gmail.com>
2025-03-05 02:44:47 -0500

    Added arguments.

--- VACARM.GUI.NET8/VACARM.GUI/Program.cs
+++ VACARM.GUI.NET8/VACARM.GUI/Program.cs
@@ -1,3 +1,4 @@
+using VACARM.GUI.Controllers;
 using VACARM.GUI.Views;
 
 namespace VACARM.GUI
@@ -9,12 +10,15 @@ namespace VACARM.GUI
     /// <summary>
     ///  The main entry point for the application.
     /// </summary>
+    /// <param name="argumentEnumerable">The enumerable of argument(s)</param>
     [STAThread]
-    static void Main()
+    static void Main(IEnumerable<string> argumentEnumerable)
     {
       // To customize application configuration such as set high DPI settings or default font,
       // see https://aka.ms/applicationconfiguration.
       ApplicationConfiguration.Initialize();
+      var argumentController = new ArgumentController(argumentEnumerable);
+      argumentController.Dispose();
       System.Windows.Forms.Application.Run(new MainForm());
     }
 
