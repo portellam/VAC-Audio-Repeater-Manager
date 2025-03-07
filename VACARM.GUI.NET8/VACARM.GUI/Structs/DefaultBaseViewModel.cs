98373b61ee62f406696bfb55980a2639320d409f
portellam <amportell.13@gmail.com>
2025-03-07 04:43:44 -0500

    Create DefaultBaseViewModel.cs

---
+++ VACARM.GUI.NET8/VACARM.GUI/Structs/DefaultBaseViewModel.cs
@@ -0,0 +1,58 @@
+﻿namespace VACARM.GUI.Structs
+{
+  internal struct DefaultBaseViewModel
+  {
+    #region Parameters
+
+    internal readonly static Size Size = new Size
+      (
+        209,
+        22
+      );
+
+    internal readonly static ToolStripMenuItem SelectAllToolStripMenuItem = 
+      new ToolStripMenuItem()
+      {
+        AutoToolTip = ToolStripMenuItem.AutoToolTip,
+        CheckOnClick = ToolStripMenuItem.CheckOnClick,
+        DisplayStyle = ToolStripMenuItem.DisplayStyle,
+        Enabled = ToolStripMenuItem.Enabled,
+        Name = "Select All",
+        Size = ToolStripMenuItem.Size
+      };
+
+    internal readonly static ToolStripMenuItem SelectRangeToolStripMenuItem =
+      new ToolStripMenuItem()
+      {
+      AutoToolTip = ToolStripMenuItem.AutoToolTip,
+      CheckOnClick = ToolStripMenuItem.CheckOnClick,
+      DisplayStyle = ToolStripMenuItem.DisplayStyle,
+      Enabled = ToolStripMenuItem.Enabled,
+      Name = "Select All...",
+      Size = ToolStripMenuItem.Size
+    };
+
+    internal readonly static ToolStripMenuItem SelectToolStripMenuItem =
+      new ToolStripMenuItem()
+      {
+      AutoToolTip = ToolStripMenuItem.AutoToolTip,
+      CheckOnClick = ToolStripMenuItem.CheckOnClick,
+      DisplayStyle = ToolStripMenuItem.DisplayStyle,
+      Enabled = ToolStripMenuItem.Enabled,
+      Name = "Select...",
+      Size = ToolStripMenuItem.Size
+    };
+
+    internal readonly static ToolStripMenuItem ToolStripMenuItem =
+      new ToolStripMenuItem()
+      {
+        AutoToolTip = false,
+        CheckOnClick = true,
+        DisplayStyle = ToolStripItemDisplayStyle.Text,
+        Enabled = true,
+        Size = Size
+      };
+
+    #endregion
+  }
+}
