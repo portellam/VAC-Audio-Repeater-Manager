e3d75eaa1247d1e8cca3d8c6242a1c1a09f182ef
portellam <amportell.13@gmail.com>
2025-03-09 08:18:16 -0400

    Separated logic for C# 8.0 + only.

---
+++ VACARM.Backend.NET8/VACARM.Infrastructure/Services/IRepeaterGroupService.AsyncEnumerable.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections.Generic;
+namespace VACARM.Infrastructure.Services
+{
+  public partial interface IRepeaterGroupService
+    <
+      TGroupReadonlyRepository,
+      TBaseService,
+      TBaseRepository,
+      TRepeaterModel
+    >
+  {
+    #region Logic
+
+    /// <summary>
+    /// Restart all <typeparamref name="TRepeaterModel"/>(s).
+    /// </summary>
+    IAsyncEnumerable<int?> RestartAllAsync();
+
+    /// <summary>
+    /// Restart some <typeparamref name="TRepeaterModel"/>(s).
+    /// </summary>
+    /// <param name="idEnumerable">The enumerable of ID(s)</param>
+    IAsyncEnumerable<int?> RestartRangeAsync(IEnumerable<uint> idEnumerable);
+
+    /// <summary>
+    /// Restart some <typeparamref name="TRepeaterModel"/>(s).
+    /// </summary>
+    /// <param name="startId">The first ID</param>
+    /// <param name="endId">The last ID</param>
+    IAsyncEnumerable<int?> RestartRangeAsync
+    (
+      uint startId,
+      uint endId
+    );
+
+    /// <summary>
+    /// Start all <typeparamref name="TRepeaterModel"/>(s).
+    /// </summary>
+    IAsyncEnumerable<int?> StartAllAsync();
+
+    /// <summary>
+    /// Start some <typeparamref name="TRepeaterModel"/>(s).
+    /// </summary>
+    /// <param name="idEnumerable">The enumerable of ID(s)</param>
+    IAsyncEnumerable<int?> StartRangeAsync
+    (IEnumerable<uint> idEnumerable);
+
+    /// <summary>
+    /// Start some <typeparamref name="TRepeaterModel"/>(s).
+    /// </summary>
+    /// <param name="startId">The first ID</param>
+    /// <param name="endId">The last ID</param>
+    IAsyncEnumerable<int?> StartRangeAsync
+    (
+      uint startId,
+      uint endId
+    );
+
+    /// <summary>
+    /// Stop all <typeparamref name="TRepeaterModel"/>(s).
+    /// </summary>
+    IAsyncEnumerable<int?> StopAllAsync();
+
+    /// <summary>
+    /// Stop some <typeparamref name="TRepeaterModel"/>(s).
+    /// </summary>
+    /// <param name="idEnumerable">The enumerable of ID(s)</param>
+    IAsyncEnumerable<int?> StopRangeAsync(IEnumerable<uint> idEnumerable);
+
+    /// <summary>
+    /// Stop some <typeparamref name="TRepeaterModel"/>(s).
+    /// </summary>
+    /// <param name="startId">The first ID</param>
+    /// <param name="endId">The last ID</param>
+    IAsyncEnumerable<int?> StopRangeAsync
+    (
+      uint startId,
+      uint endId
+    );
+
+    #endregion
+  }
+}
---
+++ VACARM.Backend.NET8/VACARM.Infrastructure/Services/RepeaterGroupService.AsyncEnumerable.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections.Generic;
+using VACARM.Application.Commands;
+using VACARM.Infrastructure.Functions;
+
+namespace VACARM.Infrastructure.Services
+{
+  public partial class RepeaterGroupService
+    <
+      TGroupReadonlyRepository,
+      TBaseService,
+      TBaseRepository,
+      TRepeaterModel
+    >
+  {
+    #region Logic
+
+    public IAsyncEnumerable<int?> RestartAllAsync()
+    {
+      return this.SelectedService
+        .DoActionAllAsync(this.RestartAsync);
+    }
+
+    public IAsyncEnumerable<int?> RestartRangeAsync
+    (
+      uint startId,
+      uint endId
+    )
+    {
+      var func = BaseFunctions<TRepeaterModel>.ContainsIdRange
+        (
+          startId,
+          endId
+        );
+
+      return this.SelectedService
+        .DoActionRangeAsync
+        (
+          this.RestartAsync,
+          func
+        );
+    }
+
+    public IAsyncEnumerable<int?> RestartRangeAsync(IEnumerable<uint> idEnumerable)
+    {
+      var func = BaseFunctions<TRepeaterModel>.ContainsIdEnumerable(idEnumerable);
+
+      return this.SelectedService
+        .DoActionRangeAsync
+        (
+          this.RestartAsync,
+          func
+        );
+    }
+
+    public IAsyncEnumerable<int?> StartAllAsync()
+    {
+      return this.SelectedService
+        .DoActionAllAsync(this.StartAsync);
+    }
+
+    public IAsyncEnumerable<int?> StartRangeAsync
+    (
+      uint startId,
+      uint endId
+    )
+    {
+      var func = BaseFunctions<TRepeaterModel>.ContainsIdRange
+        (
+          startId,
+          endId
+        );
+
+      return this.SelectedService
+        .DoActionRangeAsync
+        (
+          this.StartAsync,
+          func
+        );
+    }
+
+    public IAsyncEnumerable<int?> StartRangeAsync(IEnumerable<uint> idEnumerable)
+    {
+      var func = BaseFunctions<TRepeaterModel>.ContainsIdEnumerable(idEnumerable);
+
+      return this.SelectedService
+        .DoActionRangeAsync
+        (
+          this.StartAsync,
+          func
+        );
+    }
+
+    public IAsyncEnumerable<int?> StopAllAsync()
+    {
+      return this.SelectedService
+        .DoActionAllAsync(this.StopAsync);
+    }
+
+    public IAsyncEnumerable<int?> StopRangeAsync
+    (
+      uint startId,
+      uint endId
+    )
+    {
+      var func = BaseFunctions<TRepeaterModel>.ContainsIdRange
+        (
+          startId,
+          endId
+        );
+
+      return this.SelectedService
+        .DoActionRangeAsync
+        (
+          this.StopAsync,
+          func
+        );
+    }
+
+    public IAsyncEnumerable<int?> StopRangeAsync(IEnumerable<uint> idEnumerable)
+    {
+      var func = BaseFunctions<TRepeaterModel>.ContainsIdEnumerable(idEnumerable);
+
+      return this.SelectedService
+        .DoActionRangeAsync
+        (
+          this.StopAsync,
+          func
+        );
+    }
+
+    #endregion
+  }
+}
