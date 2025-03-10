4b823dd64c9d1f6fc1f2b70721c22f69dca373d8
portellam <amportell.13@gmail.com>
2025-03-10 02:48:26 -0400

    Create AsyncEnumerableExtension.cs

---
+++ VACARM.Backend.NET4_8/VACARM.Infrastructure/Extensions/AsyncEnumerableExtension.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VACARM.Infrastructure.Extensions
+{
+  public class AsyncEnumerableExtension<T> : 
+    IEnumerable<T>
+  {
+    #region Parameters
+
+    private readonly Func<Task<IEnumerable<T>>> TaskFactory;
+
+    #endregion
+
+    #region Logic
+
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    /// <param name="taskFactory">The task factory</param>
+    public AsyncEnumerableExtension(Func<Task<IEnumerable<T>>> taskFactory)
+    {
+      this.TaskFactory = taskFactory;
+    }
+
+
+    public IEnumerator<T> GetEnumerator()
+    {
+      this.TaskFactory()
+        .Wait();
+      
+      return this.TaskFactory()
+        .Result
+        .GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+      return this.GetEnumerator();
+    }
+
+    #endregion
+  }
+}
