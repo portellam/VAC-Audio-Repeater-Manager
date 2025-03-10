14f8efd75227050a67f2fb0d13f1f320af11dd5e
portellam <amportell.13@gmail.com>
2025-03-10 08:18:22 -0400

    Added backwards compat logic.

---
+++ VACARM.Backend.NET8/VACARM.Infrastructure/Services/ReadonlyService/IReadonlyService.Actions.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace VACARM.Infrastructure.Services
+{
+  public partial interface IReadonlyService
+    <
+      TRepository, 
+      TItem
+    >
+  {
+    #region Logic
+
+    /// <summary>
+    /// Do an action for the enumerable of all <typeparamref name="TItem"/>(s).
+    /// </summary>
+    /// <param name="actionFunc">The action function</param>
+    /// <returns>The enumerable of result code(s).</returns>
+    Task<IEnumerable<int?>> DoActionAll(Func<TItem, Task<int?>> actionFunc);
+
+    /// <summary>
+    /// Do an action for an enumerable of some <typeparamref name="TItem"/>(s).
+    /// </summary>
+    /// <param name="actionFunc">The action function</param>
+    /// <param name="enumerable">The enumerable of item(s)</param>
+    /// <returns>The enumerable of result code(s).</returns>
+    Task<IEnumerable<int?>> DoActionRange
+    (
+      Func<TItem, Task<int?>> actionFunc,
+      IEnumerable<TItem> enumerable
+    );
+
+    /// <summary>
+    /// Do an action for an enumerable of some <typeparamref name="TItem"/>(s).
+    /// </summary>
+    /// <param name="actionFunc">The action function</param>
+    /// <param name="matchFunc">The match function</param>
+    /// <returns>The enumerable of result code(s).</returns>
+    Task<IEnumerable<int?>> DoActionRange
+    (
+      Func<TItem, Task<int?>> actionFunc,
+      Func<TItem, bool> matchFunc
+    );
+
+
+    #endregion
+  }
+}
