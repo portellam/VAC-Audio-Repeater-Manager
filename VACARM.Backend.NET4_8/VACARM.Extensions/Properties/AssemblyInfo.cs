feef5e707aa7d58d8b5a2820909ce51461b94aaf
portellam <amportell.13@gmail.com>
2025-03-10 14:22:37 -0400

    Added extensions.

---
+++ VACARM.Backend.NET4_8/VACARM.Extensions/Properties/AssemblyInfo.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyCompany("Alexander Portell <github.com/portellam>")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyDescription("A library package for \"Virtual Audio Cable Audio" +
+  " Repeater Manager\". Easily multiplex multiple audio sources.Create, manage," +
+  " and automate instances of Virtual Audio Cable(VAC) audio repeaters.")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyTitle("VACARM.Extensions")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6420a186-e50d-49d0-b04c-ad38914b3e99")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- VACARM.Backend.NET4_8/VACARM.Infrastructure/Services/ReadonlyService/ReadonlyService.Actions.IEnumerable.cs
+++ VACARM.Backend.NET4_8/VACARM.Infrastructure/Services/ReadonlyService/ReadonlyService.Actions.IEnumerable.cs
@@ -4,7 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using VACARM.Infrastructure.Extensions;
+using VACARM.Extensions;
 
 namespace VACARM.Infrastructure.Services
 {
