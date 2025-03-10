7204d42159958df46f8f3051188e50bd1fbe9ab8
portellam <amportell.13@gmail.com>
2025-03-10 13:03:21 -0400

    Made NET 2 and NET 4.0 solutions.

---
+++ VACARM.Backend.NET2/VACARM.Infrastructure/Services/BaseFileService.cs
@@ -0,0 +1,126 @@
+﻿#warning Differs from project of later NET revision (above v4.6).
+
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using VACARM.Domain.Models;
+using VACARM.Infrastructure.Extensions;
+
+namespace VACARM.Infrastructure.Services
+{
+  /// <summary>
+  /// Read/write <typeparamref name="TBaseModel"/>(s) to/from a file.
+  /// </summary>
+  public class BaseFileService<TBaseModel>
+    where TBaseModel :
+    BaseModel
+  {
+    #region Parameters
+
+    private const string Extension = ".json";
+
+    #endregion
+
+    #region Logic
+
+    /// <summary>
+    /// Get the file path name with the extension.
+    /// </summary>
+    /// <param name="filePathName">The file path name</param>
+    /// <returns>The modified file path name</returns>
+    private static string GetModifiedFilePathName(string filePathName)
+    {
+      var diff = filePathName.Length - Extension.Length;
+
+      var result = filePathName
+        .Substring
+        (
+          diff
+        ) == Extension;
+
+      if (!result)
+      {
+        filePathName += Extension;
+      }
+
+      return filePathName;
+    }
+
+    /// <summary>
+    /// Write enumerable of <typeparamref name="TBaseModel"/>(s) to a JSON file.
+    /// </summary>
+    /// <param name="enumerable">The enumerable of item(s)</param>
+    /// <param name="filePathName">The file path name</param>
+    /// <returns>True/false result.</returns>
+    public async static Task WriteJsonFileAsync
+    (
+      IEnumerable<TBaseModel> enumerable,
+      string filePathName
+    )
+    {
+      if (IEnumerableExtension<TBaseModel>.IsNullOrEmpty(enumerable))
+      {
+        return;
+      }
+
+      if (StringExtension.IsNullOrEmptyOrWhitespace(filePathName))
+      {
+        return;
+      }
+
+      filePathName = GetModifiedFilePathName(filePathName);
+
+      var fileStream = await Task.Run
+        (
+          () => File.Create(filePathName)
+        );
+
+      await JsonSerializerExtension<IEnumerable<TBaseModel>>.SerializeAsync
+        (
+          fileStream,
+          enumerable
+        );
+
+      fileStream.Dispose();
+    }
+
+    /// <summary>
+    /// Get an enumerable of <typeparamref name="TBaseModel"/>(s) from a JSON file.
+    /// </summary>
+    /// <param name="filePathName">The file path name</param>
+    /// <returns>The enumerable of item(s)</returns>
+    public async static Task<IEnumerable<TBaseModel>> ReadJsonFileAsync
+    (string filePathName)
+    {
+      IEnumerable<TBaseModel> enumerable = Array.Empty<TBaseModel>();
+
+      if (StringExtension.IsNullOrEmptyOrWhitespace(filePathName))
+      {
+        return enumerable;
+      }
+
+      filePathName = GetModifiedFilePathName(filePathName);
+      var fileStream = File.OpenRead(filePathName);
+
+      try
+      {
+        enumerable = await
+          JsonSerializerExtension<IEnumerable<TBaseModel>>
+          .DeserializeAsync<IEnumerable<TBaseModel>>(fileStream)
+          .ConfigureAwait(false);
+      }
+
+      catch
+      {
+        enumerable = Array.Empty<TBaseModel>();
+      }
+
+      fileStream.Dispose();
+      return enumerable;
+    }
+
+    #endregion
+  }
+}
---
+++ VACARM.Backend.NET4_0/VACARM.Infrastructure/Properties/AssemblyInfo.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyCompany("Alexander Portell <github.com/portellam>")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyDescription("A library package for \"Virtual Audio Cable Audio" +
+  " Repeater Manager\". Easily multiplex multiple audio sources.Create, manage," +
+  " and automate instances of Virtual Audio Cable(VAC) audio repeaters.")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyTitle("VACARM.Infrastructure")]
+[assembly: AssemblyTrademark("")]
+
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("bb666493-ed06-4346-9475-32e22098c331")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
