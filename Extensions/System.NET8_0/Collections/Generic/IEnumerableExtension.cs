99380b7812c2d85a1f7baea6721cf2516bbf2260
portellam <amportell.13@gmail.com>
2025-03-13 11:17:18 -0400

    Update IEnumerableExtension.cs

--- Extensions/System.NET8_0/Collections/Generic/IEnumerableExtension.cs
+++ Extensions/System.NET8_0/Collections/Generic/IEnumerableExtension.cs
@@ -10,10 +10,10 @@ namespace System.Collections.Generic
     /// Is the sequence null or empty.
     /// </summary>
     /// <typeparam name="TSource"></typeparam>
-    /// <param name="source">The sequence.</param>
+    /// <param name="source">The sequence to test.</param>
     /// <returns><see langword="true"/> if the
-    /// <typeparamref name="enumerable"/> parameter is null or empty; otherwise,
-    /// <see langword="false"/>.</returns>
+    /// <typeparamref name="enumerable"/> parameter is <see langword="null"/> or
+    /// empty; otherwise, <see langword="false"/>.</returns>
     public static bool IsNullOrEmpty<TSource>(this IEnumerable<TSource> source)
     {
       if (source == null)
