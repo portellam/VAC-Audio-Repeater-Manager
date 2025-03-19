bb79cf4f0ae131bfeb698927bde62fd8ba4b04d9
portellam <amportell.13@gmail.com>
2025-03-19 11:55:36 -0400

    Added function.

---
+++ VACARM.Backend.NET8/VACARM.Infrastructure/Services/BaseGroupService/BaseGroupService.Functions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace VACARM.Infrastructure.Services
+{
+  public partial class BaseGroupService<TBaseModel>
+  {
+    #region Parameters
+
+    public new Func<BaseService<TBaseModel>, bool> IsValidIndex
+    {
+      get
+      {
+        return (BaseService<TBaseModel> x) => x.Model
+          .Id == this.SelectedId;
+      }
+    }
+
+    #endregion
+  }
+}
