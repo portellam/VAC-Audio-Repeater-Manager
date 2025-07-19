52487eaaf084cace6923148329bfce102ccfa115
portellam <amportell.13@gmail.com>
2025-07-19 13:12:13 -0400

    Removed on property logic. Added date time records. Added deconstructor.

--- VACARM.Backend.NET8/VACARM.Domain/Models/BaseModel.cs
+++ VACARM.Backend.NET8/VACARM.Domain/Models/BaseModel.cs
@@ -12,49 +12,13 @@ namespace VACARM.Domain.Models
   {
     #region Parameters
 
-    private uint id { get; set; }
-
-    public virtual uint Id
-    {
-      get
-      {
-        return id;
-      }
-      set
-      {
-        id = value;
-        OnPropertyChanged(nameof(this.Id));
-      }
-    }
-
-    public virtual event PropertyChangedEventHandler? PropertyChanged;
+    public virtual uint Id { get; set; }
+    public virtual DateTime CreatedDateTime { get; private set; }
+    public virtual DateTime ModifiedDateTime { get; set; }
 
     #endregion
 
     #region Logic
-
-    /// <summary>
-    /// Logs event when property has changed.
-    /// </summary>
-    /// <param name="propertyName">The property name</param>
-    internal void OnPropertyChanged(string propertyName)
-    {
-      PropertyChanged?
-        .Invoke
-        (
-          this,
-          new PropertyChangedEventArgs(propertyName)
-        );
-
-      Debug.WriteLine
-      (
-        string.Format
-        (
-          "PropertyChanged: {0}",
-          propertyName
-        )
-      );
-    }
 
     /// <summary>
     /// Constructor
@@ -64,7 +28,41 @@ namespace VACARM.Domain.Models
     [ExcludeFromCodeCoverage]
     public BaseModel(uint id)
     {
-      Id = id;
+      this.Id = id;
+      this.CreatedDateTime = DateTime.UtcNow;
+      this.ModifiedDateTime = CreatedDateTime;
+    }
+
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    /// <param name="id">The ID</param>
+    /// <param name="createdDateTime">The construct date-time</param>
+    /// <param name="modifiedDateTime">The last date-time a change occurred</param>
+    [ExcludeFromCodeCoverage]
+    public BaseModel
+    (
+      uint id,
+      DateTime createdDateTime,
+      DateTime modifiedDateTime
+    )
+    {
+      this.Id = id;
+      this.CreatedDateTime = createdDateTime;
+      this.ModifiedDateTime = modifiedDateTime;
+    }
+
+    [ExcludeFromCodeCoverage]
+    public virtual void Deconstruct
+    (
+      out uint id,
+      out DateTime createdDateTime,
+      out DateTime modifiedDateTime
+    )
+    {
+      id = this.Id;
+      createdDateTime = this.CreatedDateTime;
+      modifiedDateTime = this.ModifiedDateTime;
     }
 
     #endregion
--- VACARM.Backend.NET8/VACARM.Domain/Models/IBaseModel.cs
+++ VACARM.Backend.NET8/VACARM.Domain/Models/IBaseModel.cs
@@ -13,7 +13,34 @@ namespace VACARM.Domain.Models
     [Required]
     uint Id { get; set; }
 
-    event PropertyChangedEventHandler PropertyChanged;
+    /// <summary>
+    /// The construct date-time.
+    /// </summary>
+    [Required]
+    DateTime CreatedDateTime { get; }
+
+    /// <summary>
+    /// The last date-time a change occurred.
+    /// </summary>
+    [Required]
+    DateTime ModifiedDateTime { get; set; }
+
+    #endregion
+
+    #region Logic
+
+    /// <summary>
+    /// Deconstructor
+    /// </summary>
+    /// <param name="id">The ID</param>
+    /// <param name="createdDateTime">The construct date-time</param>
+    /// <param name="modifiedDateTime">The last date-time a change occurred</param>
+    void Deconstruct
+    (
+      out uint id,
+      out DateTime createdDateTime,
+      out DateTime modifiedDateTime
+    );
 
     #endregion
   }
