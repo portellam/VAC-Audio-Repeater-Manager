af18e4be0795bd9c9d46cb0f18a8f10f0a51f1db
portellam <amportell.13@gmail.com>
2025-07-19 13:13:02 -0400

    Began working on link model.

---
+++ VACARM.Backend.NET8/VACARM.Domain/Models/IRepeaterDeviceLinkModel.cs
@@ -0,0 +1,47 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace VACARM.Domain.Models
+{
+  public interface IRepeaterDeviceLinkModel
+  {
+    #region Parameters
+
+    RepeaterModel RepeaterModel { get; set; }
+
+    /// <summary>
+    /// The input <typeparamref name="DeviceModel"/>.
+    /// </summary>
+    DeviceModel InputDeviceModel { get; set; }
+
+    /// <summary>
+    /// The output <typeparamref name="DeviceModel"/>.
+    /// </summary>    
+    DeviceModel OutputDeviceModel { get; set; }
+
+    #endregion
+
+    #region Logic
+
+    /// <summary>
+    /// Deconstructor
+    /// </summary>
+    /// <param name="id">The ID</param>
+    /// <param name="inputDeviceModel">
+    /// The input<typeparamref name="DeviceModel"/>
+    /// </param>
+    /// <param name="outputDeviceModel">
+    /// The output <typeparamref name="DeviceModel"/>
+    /// </param>
+    /// <param name="repeaterModel">The repeater</param>
+    void Deconstruct
+    (
+      out uint id,
+      out DeviceModel inputDeviceModel,
+      out DeviceModel outputDeviceModel,
+      out RepeaterModel repeaterModel
+    );
+
+    #endregion
+  }
+}
---
+++ VACARM.Backend.NET8/VACARM.Domain/Models/RepeaterDeviceLinkModel.cs
@@ -0,0 +1,66 @@
+﻿using System.ComponentModel;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+
+namespace VACARM.Domain.Models
+{
+  /// <summary>
+  /// The link between <typeparamref name="DeviceModel"/> and
+  /// <typeparamref name="RepeaterModel"/>.
+  /// </summary>
+  public class RepeaterDeviceLinkModel :
+    BaseModel,
+    IRepeaterDeviceLinkModel
+  {
+    #region Parameters
+
+    public virtual DeviceModel InputDeviceModel { get; set; }
+    public virtual DeviceModel OutputDeviceModel { get; set; }
+    public virtual RepeaterModel RepeaterModel { get; set; }
+
+    #endregion
+
+    #region Logic
+
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    /// <param name="id">The ID</param>
+    /// <param name="inputDeviceModel">
+    /// The input<typeparamref name="DeviceModel"/>
+    /// </param>
+    /// <param name="outputDeviceModel">
+    /// The output <typeparamref name="DeviceModel"/>
+    /// </param>
+    /// <param name="repeaterModel">The repeater</param>
+    [ExcludeFromCodeCoverage]
+    public RepeaterDeviceLinkModel
+    (
+      uint id,
+      DeviceModel inputDeviceModel,
+      DeviceModel outputDeviceModel,
+      RepeaterModel repeaterModel
+    ) :
+      base(id)
+    {
+      this.Id = id;
+      this.InputDeviceModel = inputDeviceModel;
+      this.OutputDeviceModel = outputDeviceModel;
+      this.RepeaterModel = repeaterModel;
+    }
+
+    [ExcludeFromCodeCoverage]
+    public void Deconstruct
+    (
+      out uint id,
+      out DeviceModel inputDeviceModel,
+      out DeviceModel outputDeviceModel,
+      out RepeaterModel repeaterModel
+    )
+    {
+
+    }
+
+    #endregion
+  }
+}
