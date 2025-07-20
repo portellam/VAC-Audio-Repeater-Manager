a90b4e80ff87aed7e4865d0dee2803408c348aee
portellam <amportell.13@gmail.com>
2025-07-19 20:05:21 -0400

    Started initializer.

---
+++ VACARM.Backend.NET8/VACARM.Infrastructure/Initializer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VACARM.Infrastructure.Contexts;
+
+namespace VACARM.Infrastructure
+{
+  public class Initializer : DropCreateDatabaseIfModelChanges<Context>
+  {
+  }
+}
