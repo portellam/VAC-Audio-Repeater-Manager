b63a67069fdd5482936d63470d777e92d439ed72
portellam <amportell.13@gmail.com>
2025-07-21 14:17:34 -0400

    Consolidated DbContext logic, using only EFCore.

--- VACARM.Backend.NET8/VACARM.Infrastructure/Contexts/Context.cs
+++ VACARM.Backend.NET8/VACARM.Infrastructure/Contexts/Context.cs
@@ -1,20 +1,53 @@
-﻿using System.Data.Entity;
-using System.Data.Entity.ModelConfiguration.Conventions;
+﻿using Microsoft.EntityFrameworkCore;
+using VACARM.Common;
+using VACARM.Domain.Models;
 
 namespace VACARM.Infrastructure.Contexts
 {
-  public partial class Context :
+  public class Context :
     DbContext,
     IContext
   {
+    #region Parameters
+
+    public DbSet<DeviceModel> DeviceDbSet { get; set; }
+    public DbSet<RepeaterModel> RepeaterDbSet { get; set; }
+    public DbSet<RepeaterDeviceLinkModel> LinkDbSet { get; set; }
+    private readonly static string FileExtension = ".db";
+
+    public string FileName { get; set; } =
+      Info.ReferencedApplicationAbbreviatedName + FileExtension;
+
+    public string Path { get; }
+
+    #endregion
+
     #region Logic
 
     /// <summary>
     /// Constructor
     /// </summary>
-    public Context() :
-      base(nameof(Context))
+    public Context()
     {
+      var folder = Environment.SpecialFolder
+        .LocalApplicationData;
+
+      var path = Environment.GetFolderPath(folder);
+
+      this.Path = System.IO
+        .Path
+        .Join
+        (
+          path,
+          FileName
+        );
+    }
+
+    protected override void OnConfiguring
+    (DbContextOptionsBuilder dbContextOptionsBuilder)
+    {
+      string connectionString = $"Data Source={this.Path}";
+      dbContextOptionsBuilder.UseSqlite(connectionString);
     }
 
     #endregion
