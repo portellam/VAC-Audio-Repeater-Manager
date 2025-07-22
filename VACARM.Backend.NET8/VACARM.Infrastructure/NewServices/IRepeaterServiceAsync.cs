c0d80b376a9db8c156d6a21c5de125bd4a239550
portellam <amportell.13@gmail.com>
2025-07-22 14:40:59 -0400

    Began work on Repeater logic.

---
+++ VACARM.Backend.NET8/VACARM.Infrastructure/NewServices/IRepeaterServiceAsync.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VACARM.Domain.Models;
+
+namespace VACARM.Infrastructure.Services
+{
+  public partial interface IService
+  {
+    #region Logic
+
+    /// <summary>
+    /// Restart a <typeparamref name="RepeaterModel"/>.
+    /// </summary>
+    /// <param name="id">The ID</param>
+    Task<int?> RestartAsync(uint? id);
+
+    /// <summary>
+    /// Restart a <typeparamref name="RepeaterModel"/>.
+    /// </summary>
+    /// <param name="id">The ID</param>
+    Task<int?> StartAsync(uint? id);
+
+    /// <summary>
+    /// Stop a <typeparamref name="RepeaterModel"/>.
+    /// </summary>
+    /// <param name="id">The ID</param>
+    Task<int?> StopAsync(uint? id);
+
+    /// <summary>
+    /// Restart all <typeparamref name="RepeaterModel"/>(s).
+    /// </summary>
+    IAsyncEnumerable<int?> RestartAllAsync();
+
+    /// <summary>
+    /// Restart some <typeparamref name="RepeaterModel"/>(s).
+    /// </summary>
+    /// <param name="idEnumerable">The enumerable of ID(s)</param>
+    IAsyncEnumerable<int?> RestartRangeAsync(IEnumerable<uint> idEnumerable);
+
+    /// <summary>
+    /// Restart some <typeparamref name="RepeaterModel"/>(s).
+    /// </summary>
+    /// <param name="startId">The first ID</param>
+    /// <param name="endId">The last ID</param>
+    IAsyncEnumerable<int?> RestartRangeAsync
+    (
+      uint startId,
+      uint endId
+    );
+
+    /// <summary>
+    /// Start all <typeparamref name="RepeaterModel"/>(s).
+    /// </summary>
+    IAsyncEnumerable<int?> StartAllAsync();
+
+    /// <summary>
+    /// Start some <typeparamref name="RepeaterModel"/>(s).
+    /// </summary>
+    /// <param name="idEnumerable">The enumerable of ID(s)</param>
+    IAsyncEnumerable<int?> StartRangeAsync
+    (IEnumerable<uint> idEnumerable);
+
+    /// <summary>
+    /// Start some <typeparamref name="RepeaterModel"/>(s).
+    /// </summary>
+    /// <param name="startId">The first ID</param>
+    /// <param name="endId">The last ID</param>
+    IAsyncEnumerable<int?> StartRangeAsync
+    (
+      uint startId,
+      uint endId
+    );
+
+    /// <summary>
+    /// Stop all <typeparamref name="RepeaterModel"/>(s).
+    /// </summary>
+    IAsyncEnumerable<int?> StopAllAsync();
+
+    /// <summary>
+    /// Stop some <typeparamref name="RepeaterModel"/>(s).
+    /// </summary>
+    /// <param name="idEnumerable">The enumerable of ID(s)</param>
+    IAsyncEnumerable<int?> StopRangeAsync(IEnumerable<uint> idEnumerable);
+
+    /// <summary>
+    /// Stop some <typeparamref name="RepeaterModel"/>(s).
+    /// </summary>
+    /// <param name="startId">The first ID</param>
+    /// <param name="endId">The last ID</param>
+    IAsyncEnumerable<int?> StopRangeAsync
+    (
+      uint startId,
+      uint endId
+    );
+
+    #endregion
+  }
+}
