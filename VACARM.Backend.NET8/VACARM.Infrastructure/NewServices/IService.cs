d34f5d156648046fb06506b38272535f051abdde
portellam <amportell.13@gmail.com>
2025-07-22 14:02:14 -0400

    Began work on main Service.

---
+++ VACARM.Backend.NET8/VACARM.Infrastructure/NewServices/IService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VACARM.Domain.Models;
+
+namespace VACARM.Infrastructure.NewServices
+{
+  public interface IService
+  {
+    #region Parameters
+
+    #endregion
+
+    #region Logic
+
+    Task<IBaseModel> CreateAsync();
+    Task<IBaseModel> RemoveAsync();
+    Task<IBaseModel> ValidateAsync();
+
+    #endregion
+  }
+}
---
+++ VACARM.Backend.NET8/VACARM.Infrastructure/NewServices/Service.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VACARM.Domain.Models;
+using VACARM.Infrastructure.Contexts;
+
+namespace VACARM.Infrastructure.NewServices
+{
+  public class Service :
+    IService
+  {
+    #region Parameters
+
+    private Context Context { get; set; }
+
+    #endregion
+
+    #region Logic
+
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    /// <param name="context">The context</param>
+    public Service(Context context)
+    {
+      this.Context = context;
+    }
+
+    public async Task<IBaseModel> CreateAsync()
+    {
+      // Validate input/output devices
+      // Create RepeaterDeviceLink
+      // Save changes
+      throw new NotImplementedException();
+    }
+
+    public async Task<IBaseModel> RemoveAsync()
+    {
+      throw new NotImplementedException();
+    }
+
+    public async Task<IBaseModel> ValidateAsync()
+    {
+      throw new NotImplementedException();
+    }
+
+    #endregion
+  }
+}
