5bf748a52f6e224d5b5672619d4f7b8e4c4b51df
portellam <amportell.13@gmail.com>
2025-07-24 23:18:38 -0400

    Finalized Service.Base.Async methods. Added CRUD and iterative methods.

--- VACARM.Backend.NET8/VACARM.Infrastructure/Services/Service/IService.Base.Async.cs
+++ VACARM.Backend.NET8/VACARM.Infrastructure/Services/Service/IService.Base.Async.cs
@@ -20,11 +20,67 @@ namespace VACARM.Infrastructure.Services
     Task<bool> ExportAsync(int id);
 
     /// <summary>
+    /// Export an enumerable of <typeparamref name="IBaseModel"/>(s).
+    /// </summary>
+    /// <returns>True/false result</returns>
+    IAsyncEnumerable<bool> ExportRangeAsync(Func<IBaseModel, bool> matchFunc);
+
+    /// <summary>
+    /// Export an enumerable of <typeparamref name="IBaseModel"/>(s).
+    /// </summary>
+    /// <returns>True/false result</returns>
+    IAsyncEnumerable<bool> ExportRangeAsync(IEnumerable<int> idEnumerable);
+
+    /// <summary>
+    /// Export an enumerable of <typeparamref name="IBaseModel"/>(s).
+    /// </summary>
+    /// <returns>True/false result</returns>
+    IAsyncEnumerable<bool> ExportRangeAsync
+    (
+      int startId,
+      int endId
+    );
+
+    /// <summary>
+    /// Export an enumerable of all <typeparamref name="IBaseModel"/>(s).
+    /// </summary>
+    /// <returns>True/false result</returns>
+    IAsyncEnumerable<bool> ExportAllAsync();
+
+    /// <summary>
     /// Remove a <typeparamref name="IBaseModel"/>.
     /// </summary>
     /// <param name="id">The ID</param>
     /// <returns>True/false result</returns>
     Task<bool> RemoveAsync(int id);
+
+    /// <summary>
+    /// Remove an enumerable of <typeparamref name="IBaseModel"/>(s).
+    /// </summary>
+    /// <returns>True/false result</returns>
+    IAsyncEnumerable<bool> RemoveRangeAsync(Func<IBaseModel, bool> matchFunc);
+
+    /// <summary>
+    /// Remove an enumerable of <typeparamref name="IBaseModel"/>(s).
+    /// </summary>
+    /// <returns>True/false result</returns>
+    IAsyncEnumerable<bool> RemoveRangeAsync(IEnumerable<int> idEnumerable);
+
+    /// <summary>
+    /// Remove an enumerable of <typeparamref name="IBaseModel"/>(s).
+    /// </summary>
+    /// <returns>True/false result</returns>
+    IAsyncEnumerable<bool> RemoveRangeAsync
+    (
+      int startId,
+      int endId
+    );
+
+    /// <summary>
+    /// Remove an enumerable of all <typeparamref name="IBaseModel"/>(s).
+    /// </summary>
+    /// <returns>True/false result</returns>
+    IAsyncEnumerable<bool> RemoveAllAsync();
 
     /// <summary>
     /// Save a <typeparamref name="IBaseModel"/>.
@@ -34,11 +90,23 @@ namespace VACARM.Infrastructure.Services
     Task<bool> SaveAsync(IBaseModel model);
 
     /// <summary>
+    /// Save an enumerable of <typeparamref name="IBaseModel"/>(s).
+    /// </summary>
+    /// <returns>True/false result</returns>
+    IAsyncEnumerable<bool> SaveRangeAsync(IEnumerable<IBaseModel> enumerable);
+
+    /// <summary>
     /// Update a <typeparamref name="IBaseModel"/>.
     /// </summary>
     /// <param name="model">The model</param>
     /// <returns>True/false result</returns>
     Task<bool> UpdateAsync(IBaseModel model);
+
+    /// <summary>
+    /// Update an enumerable of <typeparamref name="IBaseModel"/>(s).
+    /// </summary>
+    /// <returns>True/false result</returns>
+    IAsyncEnumerable<bool> UpdateRangeAsync(IEnumerable<IBaseModel> enumerable);
 
     /// <summary>
     /// Validate a <typeparamref name="IBaseModel"/>.
@@ -48,6 +116,41 @@ namespace VACARM.Infrastructure.Services
     Task<bool> ValidateAsync(int id);
 
     /// <summary>
+    /// Validate a <typeparamref name="IBaseModel"/>.
+    /// </summary>
+    /// <param name="matchFunc">The match function</param>
+    /// <returns>True/false result</returns>
+    Task<bool> ValidateAsync(Func<IBaseModel, bool> matchFunc);
+
+    /// <summary>
+    /// Validate an enumerable of <typeparamref name="IBaseModel"/>(s).
+    /// </summary>
+    /// <returns>True/false result</returns>
+    IAsyncEnumerable<bool> ValidateRangeAsync(Func<IBaseModel, bool> matchFunc);
+
+    /// <summary>
+    /// Validate an enumerable of <typeparamref name="IBaseModel"/>(s).
+    /// </summary>
+    /// <returns>True/false result</returns>
+    IAsyncEnumerable<bool> ValidateRangeAsync(IEnumerable<int> idEnumerable);
+
+    /// <summary>
+    /// Validate an enumerable of <typeparamref name="IBaseModel"/>(s).
+    /// </summary>
+    /// <returns>True/false result</returns>
+    IAsyncEnumerable<bool> ValidateRangeAsync
+    (
+      int startId,
+      int endId
+    );
+
+    /// <summary>
+    /// Validate an enumerable of all <typeparamref name="IBaseModel"/>(s).
+    /// </summary>
+    /// <returns>True/false result</returns>
+    IAsyncEnumerable<bool> ValidateAllAsync();
+
+    /// <summary>
     /// Create a <typeparamref name="IBaseModel"/>.
     /// </summary>
     /// <param name="model">The model</param>
@@ -55,18 +158,11 @@ namespace VACARM.Infrastructure.Services
     Task<IBaseModel> CreateAsync(IBaseModel model);
 
     /// <summary>
-    /// Import a <typeparamref name="IBaseModel"/>.
+    /// Create an enumerable of <typeparamref name="IBaseModel"/>(s).
     /// </summary>
-    /// <param name="id">The ID</param>
-    /// <returns>The model</returns>
-    Task<IBaseModel> ImportAsync(int id);
-
-    /// <summary>
-    /// Validate a <typeparamref name="IBaseModel"/>.
-    /// </summary>
-    /// <param name="matchFunc">The match function</param>
-    /// <returns>The model</returns>
-    Task<IBaseModel> ValidateAsync(Func<IBaseModel, bool> matchFunc);
+    /// <returns>True/false result</returns>
+    IAsyncEnumerable<IBaseModel> CreateRangeAsync
+    (IEnumerable<IBaseModel> enumerable);
 
     /// <summary>
     /// Get a <typeparamref name="IBaseModel"/>.
@@ -83,23 +179,21 @@ namespace VACARM.Infrastructure.Services
     Task<IBaseModel?> GetAsync(Func<IBaseModel, bool> matchFunc);
 
     /// <summary>
-    /// Get an enumerable of all <typeparamref name="IBaseModel"/>(s).
+    /// Get an enumerable of <typeparamref name="IBaseModel"/>(s).
     /// </summary>
     /// <param name="matchFunc">The match function</param>
     /// <returns>The enumerable</returns>
-    IAsyncEnumerable<IBaseModel> GetRangeAsync
-    (Func<IBaseModel, bool> matchFunc);
+    IAsyncEnumerable<IBaseModel> GetRangeAsync(Func<IBaseModel, bool> matchFunc);
 
     /// <summary>
-    /// Get an enumerable of all <typeparamref name="IBaseModel"/>(s).
+    /// Get an enumerable of <typeparamref name="IBaseModel"/>(s).
     /// </summary>
     /// <param name="idEnumerable">The ID enumerable</param>
     /// <returns>The enumerable</returns>
-    IAsyncEnumerable<IBaseModel> GetRangeAsync
-    (IEnumerable<int> idEnumerable);
+    IAsyncEnumerable<IBaseModel> GetRangeAsync(IEnumerable<int> idEnumerable);
 
     /// <summary>
-    /// Get an enumerable of all <typeparamref name="IBaseModel"/>(s).
+    /// Get an enumerable of <typeparamref name="IBaseModel"/>(s).
     /// </summary>
     /// <param name="startId">The first ID</param>
     /// <param name="endId">The last ID</param>
@@ -115,6 +209,41 @@ namespace VACARM.Infrastructure.Services
     /// </summary>
     /// <returns>The enumerable</returns>
     IAsyncEnumerable<IBaseModel> GetAllAsync();
+
+    /// <summary>
+    /// Import a <typeparamref name="IBaseModel"/>.
+    /// </summary>
+    /// <param name="id">The ID</param>
+    /// <returns>The model</returns>
+    Task<IBaseModel> ImportAsync(int id);
+
+    /// <summary>
+    /// Import an enumerable of <typeparamref name="IBaseModel"/>(s).
+    /// </summary>
+    /// <returns>True/false result</returns>
+    IAsyncEnumerable<IBaseModel> ImportRangeAsync(Func<IBaseModel, bool> matchFunc);
+
+    /// <summary>
+    /// Import an enumerable of <typeparamref name="IBaseModel"/>(s).
+    /// </summary>
+    /// <returns>True/false result</returns>
+    IAsyncEnumerable<IBaseModel> ImportRangeAsync(IEnumerable<int> idEnumerable);
+
+    /// <summary>
+    /// Import an enumerable of <typeparamref name="IBaseModel"/>(s).
+    /// </summary>
+    /// <returns>True/false result</returns>
+    IAsyncEnumerable<IBaseModel> ImportRangeAsync
+    (
+      int startId,
+      int endId
+    );
+
+    /// <summary>
+    /// Import an enumerable of all <typeparamref name="IBaseModel"/>(s).
+    /// </summary>
+    /// <returns>True/false result</returns>
+    IAsyncEnumerable<IBaseModel> ImportAllAsync();
 
     /// <summary>
     /// Do an action for a <typeparamref name="IBaseModel"/>.
